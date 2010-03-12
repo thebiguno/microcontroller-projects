9415daef2b9663e7a56ada78cdc5d3e5334a1af5
Wyatt <wyatt@digitalcave.ca>
2010-03-11 22:07:09 -0700

    Updated documentation on shift.h

--- shift/shift.h
+++ shift/shift.h
@@ -12,6 +12,8 @@
 /*
  * Initializes the library to use the specified ports and pins for data out, clock, and latch.  You can
  * call this function again to change to different values.
+ * 
+ * You need to pass the ports by reference - e.g. shift_init(&PORTC, PORTC0, &PORTC, PORTC1, &PORTC, PORTC2).
  */
 void shift_init(volatile uint8_t *data_port, uint8_t data_pin, volatile uint8_t *clock_port, uint8_t clock_pin, volatile uint8_t *latch_port, uint8_t latch_pin);
 
