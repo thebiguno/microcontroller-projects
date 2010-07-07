64fb57c0fa8770012ad760fee48e65a8b33afede
Wyatt <wyatt@digitalcave.ca>
2010-07-07 00:31:33 -0600

    Updated documentation for PSX library

--- lib/psx/psx.c
+++ lib/psx/psx.c
@@ -1,21 +1,22 @@
 /******************************************************************
 *  PS2 controller library for AVR
 *
-*    Original code by Shutter on Arduino Forums
+*	 Original code by Shutter on Arduino Forums
 *
-*    Revamped, added analog stick support and made into Arduino library by:
-*              Bill Porter
-*              http://www.billporter.info/?p=219
+*	 Revamped, added analog stick support and made into Arduino library by:
+*			   Bill Porter
+*			   http://www.billporter.info/?p=219
 *
-*    Converted to generic AVR library without C++ or Arduino references, and 
-*              the ability to use any pins for input / output by:
-*              Wyatt Olson
-*              http://digitalcave.ca
+*	 Converted to generic AVR library without C++ or Arduino references, and 
+*			   the ability to use any pins for input / output by:
+*			   Wyatt Olson
+*			   http://digitalcave.ca
 *
 *  Lib version history
-*    ????		0.1		Bill Porter		-Made into library, added analog stick support
-*    2010-07	0.2		Wyatt Olson		-Converted to generic AVR-GCC library
-*                       				-Added port / pin initializers (psx_init())
+*	 ????		0.1		Bill Porter		-Made into Arduino library
+*										-Added analog stick support
+*	 2010-07	0.2		Wyatt Olson		-Converted to generic AVR-GCC library
+*										-Added port / pin initializers (psx_init())
 *										-Changed to use uintx_t data types instead of char
 *
 *
@@ -23,13 +24,13 @@
 *  Connect PSX controller wires to AVR:
 *		Yellow: Attention
 *		Orange: Command
-*		Brown:  Data
+*		Brown:	Data
 *		Blue:	Clock
 *  Call psx_init() function with the port / pins which you are using
 *  Call psx_button() function with PSB_X argument to read the value of the given button (see
-*              psx.h for possible buttons, e.g. PSB_SELECT, PSB_L3, PSB_PAD_UP, etc)
+*			   psx.h for possible buttons, e.g. PSB_SELECT, PSB_L3, PSB_PAD_UP, etc)
 *  Call psx_stick() function with PSS_X argument to read the analog stick value (possible
-*              values are PSS_RX, PSS_RY, PSS_LX, PSS_LY for right / left and X / Y)
+*			   values are PSS_RX, PSS_RY, PSS_LX, PSS_LY for right / left and X / Y)
 * 
 ******************************************************************/
 
@@ -48,7 +49,7 @@ uint8_t _psx_dat_pin = 0;
 uint8_t _psx_data[9];
 
 void psx_init(volatile uint8_t *psx_port, volatile uint8_t *psx_ddr, 
-				uint8_t clk_pin, 	
+				uint8_t clk_pin,	
 				uint8_t cmd_pin, 
 				uint8_t att_pin, 
 				uint8_t dat_pin){
@@ -65,62 +66,62 @@ void psx_init(volatile uint8_t *psx_port, volatile uint8_t *psx_ddr,
 	
 	//Set clock, attention, and command pins to output mode
 	*_psx_ddr |= _BV(_psx_clk_pin) | _BV(_psx_att_pin) | _BV(_psx_cmd_pin);
-    //Set data pin to input mode, and set pullup resistor
-    *_psx_ddr &= ~(_BV(_psx_dat_pin));
-    *_psx_port |= _BV(_psx_dat_pin);
+	//Set data pin to input mode, and set pullup resistor
+	*_psx_ddr &= ~(_BV(_psx_dat_pin));
+	*_psx_port |= _BV(_psx_dat_pin);
 
-  	//Initialize game pad
+	//Initialize game pad
 	*_psx_port |= _BV(_psx_cmd_pin);
 	*_psx_port |= _BV(_psx_clk_pin);
 	*_psx_port &= ~_BV(_psx_att_pin); // low enable joystick
-    _psx_gamepad_shiftout(0x01);
-    _psx_gamepad_shiftout(0x43);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0x01);
-    _psx_gamepad_shiftout(0x00);
-    // Lock to Analog Mode on Stick
-    _psx_gamepad_shiftout(0x01);
-    _psx_gamepad_shiftout(0x44);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0x01);
-    _psx_gamepad_shiftout(0x03);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0x00);
-    // Vibration
-    /*
-    _gamepad_shiftout(0x01);
-    _gamepad_shiftout(0x4D);
-    _gamepad_shiftout(0x00);
-    _gamepad_shiftout(0x00);
-    _gamepad_shiftout(0x01);
-    */
-    _psx_gamepad_shiftout(0x01);
-    _psx_gamepad_shiftout(0x4F);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0xFF);
-    _psx_gamepad_shiftout(0xFF);
-    _psx_gamepad_shiftout(0x03);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0x01);
-    _psx_gamepad_shiftout(0x43);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0x00);
-    _psx_gamepad_shiftout(0x5A);
-    _psx_gamepad_shiftout(0x5A);
-    _psx_gamepad_shiftout(0x5A);
-    _psx_gamepad_shiftout(0x5A);
-    _psx_gamepad_shiftout(0x5A);
+	_psx_gamepad_shiftout(0x01);
+	_psx_gamepad_shiftout(0x43);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0x01);
+	_psx_gamepad_shiftout(0x00);
+	// Lock to Analog Mode on Stick
+	_psx_gamepad_shiftout(0x01);
+	_psx_gamepad_shiftout(0x44);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0x01);
+	_psx_gamepad_shiftout(0x03);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0x00);
+	// Vibration
+	/*
+	_gamepad_shiftout(0x01);
+	_gamepad_shiftout(0x4D);
+	_gamepad_shiftout(0x00);
+	_gamepad_shiftout(0x00);
+	_gamepad_shiftout(0x01);
+	*/
+	_psx_gamepad_shiftout(0x01);
+	_psx_gamepad_shiftout(0x4F);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0xFF);
+	_psx_gamepad_shiftout(0xFF);
+	_psx_gamepad_shiftout(0x03);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0x01);
+	_psx_gamepad_shiftout(0x43);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0x00);
+	_psx_gamepad_shiftout(0x5A);
+	_psx_gamepad_shiftout(0x5A);
+	_psx_gamepad_shiftout(0x5A);
+	_psx_gamepad_shiftout(0x5A);
+	_psx_gamepad_shiftout(0x5A);
 	*_psx_port |= _BV(_psx_att_pin);
 }
 
 
 uint8_t psx_button(uint16_t button) {
-    uint16_t buttons = *(uint16_t*)(_psx_dat_pin+3);
-    return ((~buttons & button) > 0);
+	uint16_t buttons = *(uint16_t*)(_psx_dat_pin+3);
+	return ((~buttons & button) > 0);
 }
 
 uint8_t psx_stick(unsigned int stick) {
@@ -179,24 +180,24 @@ uint8_t _psx_gamepad_shiftin() {
 	return tmp;
 }
 void psx_read_gamepad() {
-    *_psx_port |= _BV(_psx_cmd_pin);
-    *_psx_port |= _BV(_psx_clk_pin);
-    *_psx_port &= ~_BV(_psx_att_pin); // low enable joystick
-    _delay_us(CTRL_BYTE_DELAY);
-    
-    //Send the command to send button and joystick data;
-    char dword[9] = {0x01,0x42,0,0,0,0,0,0,0};
+	*_psx_port |= _BV(_psx_cmd_pin);
+	*_psx_port |= _BV(_psx_clk_pin);
+	*_psx_port &= ~_BV(_psx_att_pin); // low enable joystick
+	_delay_us(CTRL_BYTE_DELAY);
+	
+	//Send the command to send button and joystick data;
+	char dword[9] = {0x01,0x42,0,0,0,0,0,0,0};
 	for (uint8_t i = 0; i<9; i++) {
 		_psx_data[i] = _psx_gamepad_shiftinout(dword[i]);
-    }
-    *_psx_port |= _BV(_psx_att_pin); // HI disable joystick
+	}
+	*_psx_port |= _BV(_psx_att_pin); // HI disable joystick
 }
 uint8_t _psx_get_gamepad_mode() {
-    *_psx_port |= _BV(_psx_cmd_pin);
-    *_psx_port |= _BV(_psx_clk_pin);
-    *_psx_port &= ~_BV(_psx_att_pin); // low enable joystick
-    _psx_gamepad_shiftout(0x01);
-    unsigned char x = _psx_gamepad_shiftin();
-    *_psx_port |= _BV(_psx_att_pin); // HI disable joystick
-    return x;
+	*_psx_port |= _BV(_psx_cmd_pin);
+	*_psx_port |= _BV(_psx_clk_pin);
+	*_psx_port &= ~_BV(_psx_att_pin); // low enable joystick
+	_psx_gamepad_shiftout(0x01);
+	unsigned char x = _psx_gamepad_shiftin();
+	*_psx_port |= _BV(_psx_att_pin); // HI disable joystick
+	return x;
 }
