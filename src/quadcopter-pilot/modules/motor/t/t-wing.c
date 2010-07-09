fd1ff16bb71308d7158aa873051470a853d3a83b
Warren Janssens <warren.janssens@gmail.com>
2010-07-09 15:23:16 -0600

    ensure that no overflow and underflow occurs

--- src/quadcopter-pilot/modules/motor/t/t-wing.c
+++ src/quadcopter-pilot/modules/motor/t/t-wing.c
@@ -5,9 +5,19 @@
 
 uint16_t[] motor_cmd(uint16_t throttle_cmd, uint16_t[] axis_cmd) {
     uint16_t[] motor_cmd = {0,0,0,0};
-    motor_cmd[MOTOR_FRONT] = constrain(throttle_cmd + axis_cmd[PITCH] - axis_command[YAW], MOTOR_MINCOMMAND, MOTOR_MAXCOMMAND);
-    motor_cmd[MOTOR_REAR] = constrain(throttle_cmd - axis_cmd[PITCH] - axis_command[YAW], MOTOR_MINCOMMAND, MOTOR_MAXCOMMAND);
-    motor_cmd[MOTOR_RIGHT] = constrain(throttle_cmd + axis_cmd[ROLL] + axis_command[YAW], MOTOR_MINCOMMAND, MOTOR_MAXCOMMAND);
-    motor_cmd[MOTOR_LEFT] = constrain(throttle_cmd - axis_cmd[ROLL] + axis_command[YAW], MOTOR_MINCOMMAND, MOTOR_MAXCOMMAND);
+    motor_cmd[MOTOR_FRONT] = chkadd(throttle_cmd, chksub(axis_cmd[PITCH], axis_command[YAW]));
+    motor_cmd[MOTOR_REAR] = chksub(throttle_cmd, chksub(axis_cmd[PITCH], axis_command[YAW]));
+    motor_cmd[MOTOR_RIGHT] = chkadd(throttle_cmd, chkadd(axis_cmd[ROLL], axis_command[YAW]));
+    motor_cmd[MOTOR_LEFT] = chksub(throttle_cmd, chkadd(axis_cmd[ROLL], axis_command[YAW]));
     return motor_cmd;
+}
+
+uint16_t chkadd(uint16_t a, uint16_t b) {
+    if (0xFFFF - b < a) return 0xFFFF;
+    return a + b;
+}
+
+uint16_t chksub(uint16_t a, uint16_t b) {
+    if (b > a) return 0x0000;
+    return a - b;
 }
--- src/quadcopter-pilot/modules/motor/x/x-wing.c
+++ src/quadcopter-pilot/modules/motor/x/x-wing.c
@@ -5,9 +5,20 @@
 
 uint16_t[] motor_cmd(uint16_t throttle_cmd, uint16_t[] axis_cmd) {
     uint16_t[] motor_cmd = {0,0,0,0};
-    motor_cmd[MOTOR_FRONT_LEFT] = constrain(throttle_cmd + axis_cmd[PITCH] + axis_command[YAW], MOTOR_MINCOMMAND, MOTOR_MAXCOMMAND);
-    motor_cmd[MOTOR_REAR_RIGHT] = constrain(throttle_cmd - axis_cmd[PITCH] + axis_command[YAW], MOTOR_MINCOMMAND, MOTOR_MAXCOMMAND);
-    motor_cmd[MOTOR_FRONT_RIGHT] = constrain(throttle_cmd - axis_cmd[ROLL] - axis_command[YAW], MOTOR_MINCOMMAND, MOTOR_MAXCOMMAND);
-    motor_cmd[MOTOR_REAR_LEFT] = constrain(throttle_cmd + axis_cmd[ROLL] - axis_command[YAW], MOTOR_MINCOMMAND, MOTOR_MAXCOMMAND);
+    
+    motor_cmd[MOTOR_FRONT_LEFT] = chkadd(throttle_cmd, chkadd(axis_cmd[PITCH], axis_command[YAW]));
+    motor_cmd[MOTOR_REAR_RIGHT] = chksub(throttle_cmd, chkadd(axis_cmd[PITCH], axis_command[YAW]));
+    motor_cmd[MOTOR_FRONT_RIGHT] = chksub(throttle_cmd, chksub(axis_cmd[ROLL], axis_command[YAW]));
+    motor_cmd[MOTOR_REAR_LEFT] = chkadd(throttle_cmd, chksub(axis_cmd[ROLL], axis_command[YAW]));
     return motor_cmd;
+}
+
+uint16_t chkadd(uint16_t a, uint16_t b) {
+    if (0xFFFF - b < a) return 0xFFFF;
+    return a + b;
+}
+
+uint16_t chksub(uint16_t a, uint16_t b) {
+    if (b > a) return 0x0000;
+    return a - b;
 }
