30a53b7a547674d5fc2ea3d661c95a046f51764d
Warren Janssens <warren.janssens@gmail.com>
2010-07-09 13:29:47 -0600

    changing types - probably still not right

--- src/quadcopter-pilot/modules/pid/pid.c
+++ src/quadcopter-pilot/modules/pid/pid.c
@@ -1,13 +1,13 @@
-double[] kp = {1,1,1,1,1,1};
-double[] ki = {1,1,1,1,1,1};
-double[] kd = {1,1,1,1,1,1};
+double[] kp = {1,1,1};
+double[] ki = {1,1,1};
+double[] kd = {1,1,1};
 
-uint16_t[] ei = {0,0,0,0,0,0};
-uint16_t[] ed = {0,0,0,0,0,0};
+int16_t[] ei = {0,0,0};
+int16_t[] ed = {0,0,0};
 
-int16_t[] mv(uint16_t[] sp, uint16_t[] pv) {
-    int16_t[] mv = {0,0,0,0,0,0};
-    for (int i = 0; i < 6; i++) {
+uint16_t[] mv(uint16_t[] sp, uint16_t[] pv) {
+    int16_t[] mv = {0,0,0};
+    for (int i = 0; i < 3; i++) {
         e = sp[i] - pv[i];
         
         p = kp[i] * e[i];
