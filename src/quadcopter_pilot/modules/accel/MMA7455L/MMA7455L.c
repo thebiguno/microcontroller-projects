9204b45473cdda741e0ac872678192f6fb84e96b
Warren Janssens <warren.janssens@gmail.com>
2010-07-19 15:17:28 -0600

    use macros instead of methods for the scale to radians

--- src/quadcopter_pilot/modules/accel/MMA7455L/MMA7455L.c
+++ src/quadcopter_pilot/modules/accel/MMA7455L/MMA7455L.c
@@ -7,13 +7,10 @@
 #define M 0.062992125984252
 #define B 0
 
-// scale accel output (-127 .. 127) to (-8 .. 8)
-double scale(int16_t x) {
-    // y = mx + b 
-    // m = (y2 - y1) / (x2 - x1) = (8 - -8) / (127 - -127) = 0.062992125984252
-    // b = y1 - m * x1 = -8 - 0.062992125984252 * -127 = 0
-    return M * x + B;
-}
+// y = mx + b 
+// m = (y2 - y1) / (x2 - x1) = (8 - -8) / (127 - -127) = 0.062992125984252
+// b = y1 - m * x1 = -8 - 0.062992125984252 * -127 = 0
+#define SCALE_TO_RAD(x) (M * (x) + B)
 
 void accel_init(){
 	//TODO
@@ -22,8 +19,8 @@ void accel_init(){
 vector_t accel_get() {
     vector_t result;
 	//TODO
-    result.x = scale(0);
-    result.y = scale(0);
-    result.z = scale(0);
+    result.x = SCALE_TO_RAD(0);
+    result.y = SCALE_TO_RAD(0);
+    result.z = SCALE_TO_RAD(0);
     return result;
 }
--- src/quadcopter_pilot/modules/gyro/LYPR530/LYPR530.c
+++ src/quadcopter_pilot/modules/gyro/LYPR530/LYPR530.c
@@ -7,25 +7,22 @@
 #define M 0.006141921121388
 #define B M_PI * -1
 
-// scale gyro output (0 .. 1023) to (-π .. π)
-double scale(int8_t x) {
-    // TODO this may need to be a non-linear scale dependending on the measured output of the gyro
-    // If AREF = 3.3V, then A/D is 931 at 3V and 465 = 1.5V
-    // y = mx + b 
-    // m = (y2 - y1) / (x2 - x1) = (π - -π) / (1023 - 0) = 0.006141921121388
-    // b = y1 - m * x1 = -π - 0.006141921121388 * 0 = -π
-    return M * x + B;
-}
+// TODO this may need to be a non-linear scale dependending on the measured output of the gyro
+// If AREF = 3.3V, then A/D is 931 at 3V and 465 = 1.5V
+// y = mx + b 
+// m = (y2 - y1) / (x2 - x1) = (π - -π) / (1023 - 0) = 0.006141921121388
+// b = y1 - m * x1 = -π - 0.006141921121388 * 0 = -π
+#define SCALE_TO_RAD(x) (M * (x) + B)
 
 void gyro_init(){
-
+    ;
 }
 
 vector_t gyro_get() {
     vector_t result;
 	//TODO
-    result.x = scale(0x7F);
-    result.y = scale(0x7F);
-    result.z = scale(0x7F)
+    result.x = SCALE_TO_RAD(0x7F);
+    result.y = SCALE_TO_RAD(0x7F);
+    result.z = SCALE_TO_RAD(0x7F)
     return result;
 }
