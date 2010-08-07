fffbab1a52124b057a919353ce3e45651b1f857b
Warren Janssens <janssensw@richer.ca>
2010-08-07 16:12:15 -0600

    fixed path

--- control/main.h
+++ control/main.h
@@ -4,7 +4,7 @@
 #include <avr/io.h>
 #include <math.h>
 
-typedef struct control_t {
+typedef struct control {
     double pitch;
     double roll;
     double yaw;
@@ -13,6 +13,6 @@ typedef struct control_t {
 } control_t;
 
 #include "modules/control/control.h"
-#include "modules/communication/communication.h"
+#include "modules/comm/communication.h"
 
 #endif
