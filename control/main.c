5476e8dafda08be75d72fb83ed28dfbd5a923eba
Warren Janssens <janssensw@richer.ca>
2010-11-10 20:12:06 -0700

    removed debouncing on mode buttons since it shouldn't be required fixed some compile warnings

--- control/main.c
+++ control/main.c
@@ -20,51 +20,39 @@ int main (void){
         uint16_t buttons = get_buttons();
         
         flags = 0x00;
-        if ((debounce & POWER_ON == 0) && buttons & POWER_ON) {
+        if (((debounce & POWER_ON) == 0) && buttons & POWER_ON) {
             debounce |= POWER_ON;
             flags &= POWER_ON;
         } else if (!(buttons & POWER_ON)) {
             debounce &= ~POWER_ON;
         }
         
-        if ((debounce & MODE_AEROBATIC == 0) && buttons & MODE_AEROBATIC) {
-            debounce |= MODE_AEROBATIC;
+        if (buttons & MODE_AEROBATIC) {
             mode = MODE_AEROBATIC;
-        } else if (!(buttons & MODE_AEROBATIC)) {
-            debounce &= ~MODE_AEROBATIC;
         }
         
-        if ((debounce & MODE_STABLE == 0) && buttons & MODE_STABLE) {
-            debounce |= MODE_STABLE;
+        if (buttons & MODE_STABLE) {
             mode = MODE_STABLE;
-        } else if (!(buttons & MODE_STABLE)) {
-            debounce &= ~MODE_STABLE;
         }
         
-        if ((debounce & MODE_HOVER == 0) && buttons & MODE_HOVER) {
-            debounce |= MODE_HOVER;
+        if (buttons & MODE_HOVER) {
             mode = MODE_HOVER;
-        } else if (!(buttons & MODE_HOVER)) {
-            debounce &= ~MODE_HOVER;
         }
         
-        if ((debounce & MODE_STABLE == 0) && buttons & MODE_STABLE) {
-            debounce |= MODE_STABLE;
+        if (buttons & MODE_STABLE) {
             mode = MODE_STABLE;
-        } else if (!(buttons & MODE_STABLE)) {
-            debounce &= ~MODE_STABLE;
         }
         
         // TODO implement calibrate and reset flags
         
         // TODO should there be limits on throttle???
-        if (mode & MODE_AEROBATIC == MODE_AEROBATIC) {
+        if ((mode & MODE_AEROBATIC) == MODE_AEROBATIC) {
             // aerobatic / 3d mode (no limits on pitch and roll)
-        } else if (mode & MODE_STABLE == MODE_SPORT) {
+        } else if ((mode & MODE_STABLE) == MODE_SPORT) {
             // sport mode (roll limited to 30 deg, pitch limited to 45 deg)
             control.roll *= 0.3333;
             control.roll *= 0.5;
-        } else if (mode & MODE_HOVER == MODE_HOVER) {
+        } else if ((mode & MODE_HOVER) == MODE_HOVER) {
             // TODO this mode is pretty useless
             // hover mode (roll, pitch, yaw = 0)
             control.pitch = 0;
