579b16fe4cb67467afd46a6d602be74e069bc44d
Warren Janssens <janssensw@richer.ca>
2010-11-18 21:22:55 -0700

    adding parameterization interface

--- pilot/modules/attitude/attitude.h
+++ pilot/modules/attitude/attitude.h
@@ -14,3 +14,9 @@ void attitude_reset();
  * Given gyro_data in rad/s, accel_data in m/s/s, and delta t in seconds, return an attitude in radians (relative to the power on state)
  */
 vector_t attitude(vector_t gyro, vector_t accel, uint16_t dt);
+
+uint8_t attitude_get_id();
+
+void attitude_get_params(double params[]);
+
+void attitude_set_params(double params[]);
