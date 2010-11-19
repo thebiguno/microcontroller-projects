5b9ed4fb717c41d59787fad08846d637e537197e
Warren Janssens <janssensw@richer.ca>
2010-11-18 21:25:28 -0700

    adapting to api changes and changing some variable names to make things more clear

--- pilot/main.c
+++ pilot/main.c
@@ -22,13 +22,13 @@ int main(){
     double throttle;
     vector_t sp;
     double motor[4];
-    uint8_t cmd_flags = 0x00;
-    uint16_t motor_flags = 0x0000;
+    uint8_t attitude_flags = 0x00;
+    uint16_t command_flags = 0x0000;
 
     //Main program loop
     while (1) {
-        comm_rx_ctrl(&throttle, &sp, &cmd_flags);
-        // comm_rx_motor(motor, &motor_flags);
+        comm_rx_attitude(&throttle, &sp, &attitude_flags);
+        // comm_rx_motor(motor, &command_flags);
         // uint8_t tune_pid = comm_rx_pid(&p, &i, &d);
         // if (tune_pid) {
         //     pid_tune(p, i, d);
@@ -38,44 +38,49 @@ int main(){
         //     
         // }
         
-        if (cmd_flags & 0x04 || motor_flags & 0x04) {
+        if (attitude_flags & 0x04 || command_flags & 0x0004) {
             gyro_calibrate();
         }
-        if (cmd_flags & 0x08 || motor_flags & 0x08) {
+        if (attitude_flags & 0x08 || command_flags & 0x0008) {
             accel_calibrate();
         }
-        if (cmd_flags & 0x10 || motor_flags & 0x10) {
+        if (attitude_flags & 0x10 || command_flags & 0x0010) {
             esc_calibrate();
         }
         
         vector_t g = gyro_get();
         vector_t a = accel_get();
 
-        if (cmd_flags & 0x02 || motor_flags & 0x02) {
+        if (attitude_flags & 0x02 || command_flags & 0x02) {
             attitude_reset(&g, &a);
         }
 
         vector_t pv = attitude(g, a, dt);
         vector_t mv = pid_mv(sp, pv);
 
-        if (cmd_flags & 0x01) {
+        if (attitude_flags & 0x01) {
             // armed by controller
             motor_percent(throttle, mv, motor);
             esc_set(1, motor);
-        } else if (motor_flags & 0x0001) {
+        } else if (command_flags & 0x0001) {
             // armed by configuration
             esc_set(1, motor);
         } else {
             esc_set(0, motor);
         }
         
-        if (cmd_flags & 0x0100) {
-            // comm_tx_pid(pid); // TODO expose pid module values
+        if (command_flags & 0x0100) {
+            comm_tx_telemetry(pv, motor, 0x00); // TODO flags
         }
-        if (cmd_flags & 0x0200) {
-            comm_tx_tm(pv, motor, 0x00); // TODO flags
+        if (command_flags & 0x0200) {
+//            comm_tx_pid_tuning(pid);
         }
-        if (cmd_flags & 0x0300) {
+        if (command_flags & 0x0400) {
+        	double params[] = { 0.0 };
+        	attitude_get_params(params);
+        	comm_tx_attitude_tuning(params, attitude_get_id());
+        }
+        if (command_flags & 0x0800) {
             //comm_tx_raw(&g, &a);
         }
 	}
