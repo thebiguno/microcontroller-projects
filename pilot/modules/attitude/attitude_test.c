dddf11ffcdba5293afe5ff6c540204b119ebff84
Warren Janssens <janssensw@richer.ca>
2010-11-29 22:36:47 -0700

    first working version

--- pilot/modules/attitude/attitude_test.c
+++ pilot/modules/attitude/attitude_test.c
@@ -7,7 +7,15 @@
 #include "../../../lib/serial/serial.h"
 
 int main(){
-    char temp[32];
+    char temp[35];
+    
+    serial_init(9600, 8, 0, 1);
+    
+    gyro_init();
+    gyro_calibrate();
+    
+    accel_init();
+    accel_calibrate();
     
     //Main program loop
     while (1) {
@@ -15,7 +23,11 @@ int main(){
         vector_t a = accel_get();
         vector_t pv = attitude(g, a);
         
-        sprintf(temp, "x=%+1.2f, y=%+1.2f, z=%+1.2f\n\r", pv.x, pv.y, pv.z);
+        sprintf(temp, "gx=%+1.2f, gy=%+1.2f, gz=%+1.2f\n\r", g.x, g.y, g.z);
+        serial_write_s(temp);
+        sprintf(temp, "ax=%+1.2f, ay=%+1.2f, az=%+1.2f\n\r", a.x, a.y, a.z);
+        serial_write_s(temp);
+        sprintf(temp, " x=%+1.2f,  y=%+1.2f,  z=%+1.2f\n\r", pv.x, pv.y, pv.z);
         serial_write_s(temp);
     }
 }
