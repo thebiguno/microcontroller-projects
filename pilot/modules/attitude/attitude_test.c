628c59c8b2185a2ba58ad4175b8b0c0052bd336b
Warren Janssens <janssensw@richer.ca>
2010-11-29 23:16:49 -0700

    don't print raw values

--- pilot/modules/attitude/attitude_test.c
+++ pilot/modules/attitude/attitude_test.c
@@ -23,10 +23,10 @@ int main(){
         vector_t a = accel_get();
         vector_t pv = attitude(g, a);
         
-        sprintf(temp, "gx=%+1.2f, gy=%+1.2f, gz=%+1.2f\n\r", g.x, g.y, g.z);
-        serial_write_s(temp);
-        sprintf(temp, "ax=%+1.2f, ay=%+1.2f, az=%+1.2f\n\r", a.x, a.y, a.z);
-        serial_write_s(temp);
+//        sprintf(temp, "gx=%+1.2f, gy=%+1.2f, gz=%+1.2f\n\r", g.x, g.y, g.z);
+//        serial_write_s(temp);
+//        sprintf(temp, "ax=%+1.2f, ay=%+1.2f, az=%+1.2f\n\r", a.x, a.y, a.z);
+//        serial_write_s(temp);
         sprintf(temp, " x=%+1.2f,  y=%+1.2f,  z=%+1.2f\n\r", pv.x, pv.y, pv.z);
         serial_write_s(temp);
     }
