85ec34dca479695c86f326f107e27a5644abc646
Warren Janssens <janssensw@richer.ca>
2010-12-01 07:53:00 -0700

    overhauled the implementation

--- pilot/modules/attitude/kalman/attitude_kalman.c
+++ pilot/modules/attitude/kalman/attitude_kalman.c
@@ -5,48 +5,59 @@
 
 #define ATTITUDE_ALGORITHM_ID 0x01
 
-static vector_t angle;
-static vector_t bias;
-static vector_t p00;
-static vector_t p01;
-static vector_t p10;
-static vector_t p11;
+typedef struct kalman_state {
+    double angle;
+    double bias;
+    double p00;
+    double p01;
+    double p10;
+    double p11;
+} kalman_state_t;
 
-static vector_t q_gyro;
-static vector_t q_angle;
-static vector_t r_angle;
+typedef struct kalman_tuning {
+    double q_gyro;
+    double q_angle;
+    double r_angle;
+} kalman_tuning_t;
+    
+static vector_t angle;
+static kalman_state_t state_x;
+static kalman_state_t state_y;
+static kalman_tuning_t tuning_x;
+static kalman_tuning_t tuning_y;
+
 static uint64_t millis;
 
 void attitude_init(vector_t gyro, vector_t accel) {
     millis = timer_millis();
 
     // TODO read this from EEPROM
-    q_angle.x = 0.001;
-    q_angle.y = 0.001;
-    q_gyro.x = 0.003;
-    q_gyro.y = 0.003;
-    r_angle.x = 0.03;
-    r_angle.y = 0.03;
+    tuning_x.q_angle = 0.001;
+    tuning_y.q_angle = 0.001;
+    tuning_x.q_gyro = 0.003;
+    tuning_y.q_gyro = 0.003;
+    tuning_x.r_angle = 0.03;
+    tuning_y.r_angle = 0.03;
 }
 
-void _attitude (double gyro, double accel, double *angle, double *bias, double *p00, double *p01, double *p10, double *p11, double q_gyro, double q_angle, double r_angle, double dt) {
-    *angle += dt * (gyro - *bias);
-    *p00 +=  - dt * (*p10 + *p01) + q_angle * dt;
-    *p01 +=  - dt * *p11;
-    *p10 +=  - dt * *p11;
-    *p11 +=  + q_gyro * dt;
+void _attitude (double gyro, double accel, kalman_state_t *state, kalman_tuning_t tuning, double dt) {
+    state->angle += dt * (gyro - state->bias);
+    state->p00 +=  - dt * (state->p10 + state->p01) + tuning.q_angle * dt;
+    state->p01 +=  - dt * state->p11;
+    state->p10 +=  - dt * state->p11;
+    state->p11 +=  + tuning.q_gyro * dt;
 
-    double y = accel - *angle;
-    double s = *p00 + r_angle;
-    double k0 = *p00 / s;
-    double k1 = *p10 / s;
+    double y = accel - state->angle;
+    double s = state->p00 + tuning.r_angle;
+    double k0 = state->p00 / s;
+    double k1 = state->p10 / s;
 
-    *angle +=  k0 * y;
-    *bias +=  k1 * y;
-    *p00 -= k0 * *p00;
-    *p01 -= k0 * *p01;
-    *p10 -= k1 * *p00;
-    *p11 -= k1 * *p01;
+    state->angle +=  k0 * y;
+    state->bias +=  k1 * y;
+    state->p00 -= k0 * state->p00;
+    state->p01 -= k0 * state->p01;
+    state->p10 -= k1 * state->p00;
+    state->p11 -= k1 * state->p01;
 }
 
 vector_t attitude(vector_t gyro, vector_t accel) {
@@ -54,8 +65,11 @@ vector_t attitude(vector_t gyro, vector_t accel) {
     double dt = (curr_millis - millis) * 0.001;
     millis = curr_millis;
 
-    _attitude(gyro.x, accel.x, &angle.x, &bias.x, &p00.x, &p01.x, &p10.x, &p11.x, q_gyro.x, q_angle.x, r_angle.x, dt);
-    _attitude(gyro.y, accel.y, &angle.y, &bias.y, &p00.y, &p01.y, &p10.y, &p11.y, q_gyro.y, q_angle.y, r_angle.y, dt);
+    _attitude(gyro.x, accel.x, &state_x, tuning_x, dt);
+    _attitude(gyro.y, accel.y, &state_y, tuning_y, dt);
+    angle.x = state_x.angle;
+    angle.y = state_y.angle;
+    angle.z = gyro.z;
     return angle;
 }
 
@@ -66,24 +80,23 @@ uint8_t attitude_get_id() {
 void attitude_reset() {
 	angle.x = 0.0;
 	angle.y = 0.0;
-	angle.z = 0.0;
 }
 
 void attitude_get_params(double params[]) {
-    params[0] = q_angle.x;
-    params[1] = q_angle.y;
-    params[2] = q_gyro.x;
-    params[3] = q_gyro.y;
-    params[4] = r_angle.x;
-    params[5] = r_angle.y;
+    params[0] = tuning_x.q_angle;
+    params[1] = tuning_y.q_angle;
+    params[2] = tuning_x.q_gyro;
+    params[3] = tuning_y.q_gyro;
+    params[4] = tuning_x.r_angle;
+    params[5] = tuning_y.r_angle;
 }
 
 void attitude_set_params(double params[]) {
-    q_angle.x = params[0];
-    q_angle.y = params[1];
-    q_gyro.x = params[2];
-    q_gyro.y = params[3];
-    r_angle.x = params[4];
-    r_angle.y = params[5];
+    tuning_x.q_angle = params[0];
+    tuning_y.q_angle = params[1];
+    tuning_x.q_gyro = params[2];
+    tuning_y.q_gyro = params[3];
+    tuning_x.r_angle = params[4];
+    tuning_y.r_angle = params[5];
 }
 
