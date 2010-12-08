db266c75e33e10ad62113352458c1efa6340e262
Wyatt <wyatt@digitalcave.ca>
2010-12-08 00:13:54 -0700

    Playing with serial tests...

--- atmega168/serial/main.c
+++ atmega168/serial/main.c
@@ -12,24 +12,12 @@
 int main (void){
 	serial_init(57600, 8, 0, 1);
 		
-	char temp[32];
+	char c;
 
 	//Main program loop
 	while (1){
-		int r = serial_read_s(temp, 32);
-		if (r){
-			serial_write_s(temp);
-			serial_write_s(itoa(r, temp, 10));
-		}
-		//serial_write_c(serial_read_c());
-		serial_write_s("\n\rTesting...\n\r");
-		serial_write_c('c');
-		serial_write_c('h');
-		serial_write_c('a');
-		serial_write_c('r');
-		serial_write_c('\n');
-		serial_write_c('\r');
-		_delay_ms(1000);
-		
+		if (serial_read_c(&c)){
+			serial_write_c(c);
+		}		
 	}
 }
