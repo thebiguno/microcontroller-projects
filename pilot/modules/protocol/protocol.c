fc475381a6eac657b6d2250f0f232180e1f38956
Warren Janssens <janssensw@richer.ca>
2010-12-07 22:34:52 -0700

    fixed linker errors

--- pilot/modules/protocol/protocol.c
+++ pilot/modules/protocol/protocol.c
@@ -190,4 +190,32 @@ void protocol_send_telemetry(vector_t vector, double motor[]) {
 	}
 }
 
+double protocol_byte_to_radian(uint8_t x) {
+	double r = 0.024639942381096 * x;
+	return (r > M_PI) ? 2.0 * M_PI - r : r; // convert quadrants III & IV into negative values
+}
+uint8_t protocol_radian_to_byte(float x) {
+	double r = x < 0 ? 2.0 * M_PI - x : x; // convert quadrants III & IV into positive values
+	return 40.584510488433314 * r;
+}
+double protocol_byte_to_percent(uint8_t x) {
+	return 0.392156862745098 * x;
+}
+uint8_t protocol_percent_to_byte(float x) {
+	return 2.55 * x;
+}
+void protocol_double_to_bytes(double value, uint8_t *buffer) {
+    union udouble converter;
+    converter.d = value;
+    for (uint8_t i = 0; i < sizeof(double); i++) {
+        buffer[i] = converter.u[i];
+    }
+}
+double protocol_bytes_to_double(uint8_t *buffer) {
+    union udouble converter;
+    for (uint8_t i = 0; i < sizeof(double); i++) {
+        converter.u[i] = buffer[0];
+    }
+    return converter.d;
+}
 
