a05cd869d7bc0f26396e8c977446203cb66e9000
Warren Janssens <janssensw@richer.ca>
2010-12-15 09:23:39 -0700

    copy working program from 168

--- atmega644/protocol_test/main.c
+++ atmega644/protocol_test/main.c
@@ -16,18 +16,35 @@ int main (void){
 		
 	uint8_t buf[36];
 
-	double x[] = { 0.0, -0.0, 1.0, -1.0, INFINITY, -INFINITY, NAN, 3.14159, -3.14159 };
 	int i;
+
+	// 00000000, 80000000, 3F800000, BF800000, 7F800000, FF800000, 7FC00000, 00000001, 80000001
+	double x1[9] = { 0.0, -0.0, 1.0, -1.0, INFINITY, -INFINITY, NAN, 1.4012985e-45, -1.4012985e-45 };
 	for (i = 0; i < 9; i++) {
-		protocol_double_to_bytes(x[i], buf, i * 4);
+		protocol_double_to_bytes(x1[i], buf, i * 4);
 	}
-	protocol_send_message('X', buf, 36);
+	protocol_send_message('F', buf, 36);
 	
-	double d;
-	for (d = -3.141592653589793; d <= 3.141592653589793; d = d + 0.349065850398866) {
-		protocol_double_to_bytes(d, buf, i * 4);
+	// C0490FDB, C032B8C2, C01C61AA, C0060A92, BFDF66F3, BFB2B8C2, BF860A92, BF32B8C2, BEB2B8C2
+	double x2[9] = { -3.1415927, -2.7925267, -2.4434609, -2.0943952, -1.7453293, -1.3962634, -1.0471976, -6.9813168e-1, -3.4906584e-1 };
+	for (i = 0; i < 9; i++) {
+		protocol_double_to_bytes(x2[i], buf, i * 4);
 	}
-	protocol_send_message('R', buf, 36);
+	protocol_send_message('F', buf, 36);
+	
+	// 40490FDB, 4032B8C2, 401C61AA, 40060A92, 3FDF66F3, 3FB2B8C2, 3F860A92, 3F32B8C2, 3EB2B8C2
+	double x3[9] = { 3.1415927, 2.7925267, 2.4434609, 2.0943952, 1.7453293, 1.3962634, 1.0471976, 6.9813168e-1, 3.4906584e-1 };
+	for (i = 0; i < 9; i++) {
+		protocol_double_to_bytes(x3[i], buf, i * 4);
+	}
+	protocol_send_message('F', buf, 36);
+
+	// 3DCCCCCD, 3E99999A, 3F333333, 3C23D70A, 3CF5C28F, 3D8F5C29, 3A83126F, 3B449BA6, 3BE56042, 
+	double x4[9] = { 0.1, 0.3, 0.7, 0.01, 0.03, 0.07, 0.001, 0.003, 0.007, };
+	for (i = 0; i < 9; i++) {
+		protocol_double_to_bytes(x4[i], buf, i * 4);
+	}
+	protocol_send_message('F', buf, 36);
 	
 	while(1) {
 	}	
