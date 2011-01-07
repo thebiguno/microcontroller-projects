d77d8213da8e02aff4fa03cb33c6f4c0774173a4
Wyatt <wyatt@digitalcave.ca>
2011-01-06 23:27:21 -0700

    Sharing

--- test/serial_read/main.c
+++ test/serial_read/main.c
@@ -5,19 +5,24 @@
 
 int main(){
 
+	//Turn off watchdog timer
+	MCUSR &= ~_BV(WDRF);
+	WDTCSR &= ~_BV(WDE);
+
 	//Init all modules.  We call accel_init last as it forces sei().
 	serial_init_b(57600);
 	shift_init(&PORT_LED_SHIFT_DATA, PIN_LED_SHIFT_DATA, &PORT_LED_SHIFT_CLOCK, PIN_LED_SHIFT_CLOCK, &PORT_LED_SHIFT_LATCH, PIN_LED_SHIFT_LATCH);
 	
-	shift_out(0xFF);
+	shift_out(0xAA);
 	_delay_ms(500);
 	shift_out(0x00);
 	
 	char b;	
 	
+	uint8_t i = 0;
+	
 	//Main program loop
 	while (1) {
-		
 		while (serial_available() && serial_read_c(&b)) {
 			shift_out(b);
 		}
