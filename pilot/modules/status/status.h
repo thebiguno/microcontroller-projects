0e0b7afc636cd8feb050812ead8295965948dbef
Warren Janssens <janssensw@richer.ca>
2011-01-07 22:55:34 -0700

    adding constants

--- pilot/modules/status/status.h
+++ pilot/modules/status/status.h
@@ -5,6 +5,9 @@
 
 #define STATUS_HEARTBEAT	_BV(0)
 #define STATUS_ARMED		_BV(1)
+#define STATUS_MESSAGE_TX	_BV(2)
+#define STATUS_MESSAGE_RX	_BV(3)
+#define STATUS_CHECKSUM_ERR _BV(6)
 #define STATUS_PROTOCOL_ERR	_BV(7)
 
 void status_init();
