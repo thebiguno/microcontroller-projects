97e348aff0168f19f3297082bf27e96cd068d472
Wyatt <wyatt@digitalcave.ca>
2011-01-07 23:59:40 -0700

    Fixed trig logic bug

--- pilot/modules/util/convert.c
+++ pilot/modules/util/convert.c
@@ -3,7 +3,7 @@
 double convert_byte_to_radian(uint8_t x)
 {
 	double r = 0.024639942381096 * x;
-	return (r > M_PI) ? (2.0 * M_PI) - r : r; // convert quadrants III & IV into negative values
+	return (r > M_PI) ? r - (2.0 * M_PI) : r; // convert quadrants III & IV into negative values
 }
 uint8_t convert_radian_to_byte(double x)
 {
