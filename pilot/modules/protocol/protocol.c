482850daf6dc68c28ac4371632d2340a64343d58
Warren Janssens <janssensw@richer.ca>
2011-01-07 22:55:05 -0700

    made consistent with the java side

--- pilot/modules/protocol/protocol.c
+++ pilot/modules/protocol/protocol.c
@@ -145,9 +145,8 @@ void protocol_poll()
             default:
                 if (_pos > MAX_SIZE) continue; // this probably can't happen since the xbee packet size is larger than any of our messages
                 if (_pos == (_len + 2)) {
-                    if ((_chk & 0xff) == 0xff) {
+                    if (_chk == 0xff) {
 						_protocol_dispatch(_api, _len - 1);
-						break; // i.e. maximum one message processed per main loop
 					} else {
 						status_set(STATUS_PROTOCOL_ERR);
 						_err = 1;
