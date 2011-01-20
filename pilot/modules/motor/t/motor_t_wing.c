671b308506980a9acfe34bc2bdca47c7757c5a5d
Warren Janssens <janssensw@richer.ca>
2011-01-19 22:12:11 -0700

    fixed motor math to correlate with motor configuration

--- pilot/modules/motor/t/motor_t_wing.c
+++ pilot/modules/motor/t/motor_t_wing.c
@@ -3,8 +3,8 @@
 #define MOTOR_LIMIT(x) (x > 1.0 ? 1.0 : (x < 0.0 ? 0.0 : x))
 
 void motor_percent(double throttle, vector_t mv, double result[]) {
-    result[0] = MOTOR_LIMIT(throttle + mv.y - mv.z);
-    result[1] = MOTOR_LIMIT(throttle - mv.y - mv.z);
-    result[2] = MOTOR_LIMIT(throttle + mv.x + mv.z);
-    result[3] = MOTOR_LIMIT(throttle - mv.x + mv.z);
+    result[0] = MOTOR_LIMIT(throttle + mv.y + mv.z); // left
+    result[1] = MOTOR_LIMIT(throttle - mv.x - mv.z); // front
+    result[2] = MOTOR_LIMIT(throttle - mv.y + mv.z); // right
+    result[3] = MOTOR_LIMIT(throttle + mv.x - mv.z); // rear
 }
--- pilot/modules/motor/x/motor_x_wing.c
+++ pilot/modules/motor/x/motor_x_wing.c
@@ -3,8 +3,8 @@
 #define MOTOR_LIMIT(x) (x > 1.0 ? 1.0 : (x < 0.0 ? 0.0 : x))
 
 void motor_percent(double throttle, vector_t mv, double result[]) {
-    result[0] = MOTOR_LIMIT(throttle + mv.y + mv.z);
-    result[1] = MOTOR_LIMIT(throttle - mv.y + mv.z);
-    result[2] = MOTOR_LIMIT(throttle - mv.x - mv.z);
-    result[3] = MOTOR_LIMIT(throttle + mv.x - mv.z);
+    result[0] = MOTOR_LIMIT(throttle + mv.x + mv.y + mv.z); // front-left
+    result[1] = MOTOR_LIMIT(throttle - mv.x + mv.y - mv.z); // front-right
+    result[2] = MOTOR_LIMIT(throttle - mv.x - mv.y + mv.z); // rear-right
+    result[3] = MOTOR_LIMIT(throttle + mv.x - mv.y - mv.z); // rear-left
 }
