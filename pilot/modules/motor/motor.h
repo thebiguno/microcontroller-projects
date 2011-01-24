e576b950921b2a42fb98e5a01fd49fc86b75f31b
Warren Janssens <janssensw@richer.ca>
2011-01-24 14:39:49 -0700

    removed unused defines

--- pilot/modules/motor/motor.h
+++ pilot/modules/motor/motor.h
@@ -3,11 +3,6 @@
 
 #include "../../main.h"
 
-#define MOTOR_A = 0
-#define MOTOR_B = 1
-#define MOTOR_C = 2
-#define MOTOR_D = 3
-
 /*
  * Given a throttle percentage (0 .. 1) and vector of percentage adjustments for each axis (-1 .. 1), calculate the motor power percentage (0..1)
  */
