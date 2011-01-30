e5b5a74c435d8eb9d1695b8ba31718577e4e69f0
Warren Janssens <janssensw@richer.ca>
2011-01-30 09:28:41 -0700

    adding new util module

---
+++ control/modules/util/convert.h
@@ -0,0 +1,15 @@
+#ifndef CONVERT_H
+#define CONVERT_H
+
+#include "../../main.h"
+
+void convert_double_to_bytes(double value, uint8_t *buf, uint8_t offset);
+double convert_bytes_to_double(uint8_t *buf, uint8_t offset);
+
+double convert_byte_to_radian(uint8_t x);
+uint8_t convert_radian_to_byte(double x);
+
+double convert_byte_to_percent(uint8_t x);
+uint8_t convert_percent_to_byte(double x);
+
+#endif
