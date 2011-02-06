09a58aa0afc150176e301148b77de859f3cb861c
Warren Janssens <janssensw@richer.ca>
2011-02-06 16:30:42 -0700

    extracted protocol state into a struct implemented full protocol in both directions added a diagnostic message to the pc

--- control/modules/protocol/protocol.c
+++ control/modules/protocol/protocol.c
@@ -9,19 +9,48 @@
 
 #define MAX_SIZE 40
 
-static uint8_t _pos;       // current position in the frame
-static uint8_t _len;       // frame length
-static uint8_t _api;       // frame api code
-static uint8_t _chk;       // checksum
-static uint8_t _esc;       // escape byte seen, unescape next byte
-static uint8_t _err;       // error condition, ignore bytes until next frame start byte
-static uint8_t _buf[MAX_SIZE];
+typedef struct state {
+	uint8_t pos;			// current position in the frame
+	uint8_t len;			// frame length
+	uint8_t api;			// frame api code
+	uint8_t chk;			// checksum
+	uint8_t esc;			// escape byte seen, unescape next byte
+	uint8_t err;			// error condition, ignore bytes until next frame start byte
+	uint8_t buf[MAX_SIZE];
+} state_t;
+
+static state_t pilot;		// messages coming in from the pilot
+static state_t pc;			// messages coming in from the pc
 
 static uint8_t dummy[] = {0}; // zero sized array not allowed in C
 
+static void _protocol_send_byte_to_pc(uint8_t b, uint8_t escape){
+	if (escape && (b == START || b == ESCAPE || b == XON || b == XOFF)) {
+		comm_pc_write(ESCAPE);
+		comm_pc_write(b ^ 0x20);
+	} else {
+		comm_pc_write(b);
+	}
+}
 
-static void _protocol_send_byte(uint8_t b, uint8_t escape)
-{
+static void protocol_send_message_to_pc(uint8_t cmd, uint8_t *bytes, uint8_t length){
+	_protocol_send_byte_to_pc(START, 0);
+	_protocol_send_byte_to_pc(length + 1, 1);
+	_protocol_send_byte_to_pc(cmd, 0);
+	
+	uint8_t checksum = cmd;
+
+	for (uint8_t i = 0; i < length; i++) {
+		_protocol_send_byte_to_pc(bytes[i], 1);
+		checksum += bytes[i];
+	}
+
+	checksum = 0xff - checksum;
+
+	_protocol_send_byte_to_pc(checksum, 1);
+}
+
+static void _protocol_send_byte_to_pilot(uint8_t b, uint8_t escape){
     if (escape && (b == START || b == ESCAPE || b == XON || b == XOFF)) {
         comm_pilot_write(ESCAPE);
         comm_pilot_write(b ^ 0x20);
@@ -30,22 +59,21 @@ static void _protocol_send_byte(uint8_t b, uint8_t escape)
     }
 }
 
-static void protocol_send_message(uint8_t cmd, uint8_t *bytes, uint8_t length)
-{
-	_protocol_send_byte(START, 0);
-	_protocol_send_byte(length + 1, 1);
-	_protocol_send_byte(cmd, 0);
+static void protocol_send_message_to_pilot(uint8_t cmd, uint8_t *bytes, uint8_t length){
+	_protocol_send_byte_to_pilot(START, 0);
+	_protocol_send_byte_to_pilot(length + 1, 1);
+	_protocol_send_byte_to_pilot(cmd, 0);
 	
 	uint8_t checksum = cmd;
 
 	for (uint8_t i = 0; i < length; i++) {
-		_protocol_send_byte(bytes[i], 1);
+		_protocol_send_byte_to_pilot(bytes[i], 1);
 		checksum += bytes[i];
 	}
 
 	checksum = 0xff - checksum;
 
-	_protocol_send_byte(checksum, 1);
+	_protocol_send_byte_to_pilot(checksum, 1);
 }
 
 void protocol_send_control(control_t control){
@@ -54,87 +82,112 @@ void protocol_send_control(control_t control){
 	convert_double_to_bytes(control.pitch, packet, 4);
 	convert_double_to_bytes(control.roll, packet, 8);
 	convert_double_to_bytes(control.yaw, packet, 12);
-	protocol_send_message('C', packet, 16);
+	protocol_send_message_to_pilot('C', packet, 16);
 }
 
-void protocol_send_reset_attitude() {
-	protocol_send_message('R', dummy, 0);
+void protocol_send_reset_attitude(){
+	protocol_send_message_to_pilot('R', dummy, 0);
 }
 
-void protocol_send_calibrate() {
-	protocol_send_message('C', dummy, 0);
+void protocol_send_calibrate(){
+	protocol_send_message_to_pilot('C', dummy, 0);
+}
+
+void protocol_send_diag(char* s) {
+	uint8_t packet[255];
+	uint8_t l = 0;
+	while (*s != '\0') {
+		packet[l] = *s;
+		l++;
+		s++;
+	}
+	protocol_send_message_to_pc('D', packet, l);
 }
 
 /*
- * copy all bytes from the pilot to the pc without interpretting them
+ * copy all messages from source to destintation
+ * messages are decoded and buffered so they can be retransmitted without interfering with messages from the controller
+ * dest of 0x00 is pilot
+ * dest of 0x01 is pc
  */
-void protocol_poll_pilot(){
-	uint8_t b;
-	while (comm_pilot_available() && comm_pilot_read(&b)) {
-		comm_pc_write(b);
+static void _protocol_poll(uint8_t b, state_t *source, uint8_t dest){
+	if (source->err > 0 && b == START) {
+		// recover from error condition
+		source->err = 0;
+		source->pos = 0;
+	} else if (source->err > 0) {
+		return;
+	}
+
+	if (source->pos > 0 && b == START) {
+		// unexpected start of frame
+		source->err = 1;
+		return;
+	}
+	if (source->pos > 0 && b == ESCAPE) {
+		// unescape next byte
+		source->esc = 1;
+		return;
+	}
+	if (source->esc) {
+		// unescape current byte
+		b = 0x20 ^ b;
+		source->esc = 0;
+	}
+	if (source->pos > 1) { // start byte and length byte not included in checksum
+		source->chk += b;
+	}
+
+	switch(source->pos) {
+	case 0: // start frame
+		source->pos++;
+		return;
+	case 1: // length
+		source->len = b;
+		source->pos++;
+		return;
+	case 2:
+		source->api = b;
+		source->pos++;
+		return;
+	default:
+		if (source->pos > MAX_SIZE) return; // this probably can't happen since the xbee packet size is larger than any of our messages
+		if (source->pos == (source->len + 2)) {
+			if (source->chk == 0xff) {
+				if (dest == 0x00) {
+					protocol_send_diag("relay pc->pilot");
+					protocol_send_message_to_pilot(source->api, source->buf, source->len - 1);
+				} else if (dest == 0x01) {
+					protocol_send_diag("relay pilot->pc");
+					protocol_send_message_to_pc(source->api, source->buf, source->len - 1);
+				}
+			} else {
+				source->err = 1;
+			}
+			source->pos = 0;
+			source->chk = 0;
+		} else {
+			source->buf[source->pos++ - 3] = b;
+		}
 	}
 }
 
-/*
- * copy all messages from the pc to the pilot
- * messages are decoded and buffered so they can be retransmitted without interfering with messages from the controller
- */
-void protocol_poll_pc(){
-    uint8_t b;
-    while (comm_pc_available() && comm_pc_read(&b)) {
-        if (_err > 0 && b == START) {
-			// recover from error condition
-			_err = 0;
-			_pos = 0;
-        } else if (_err > 0) {
-            continue;
-        }
-        
-        if (_pos > 0 && b == START) {
-			// unexpected start of frame
-			_err = 1;
-			continue;
-        }
-        if (_pos > 0 && b == ESCAPE) {
-			// unescape next byte
-			_esc = 1;
-			continue;
-        }
-        if (_esc) {
-			// unescape current byte
-			b = 0x20 ^ b;
-			_esc = 0;
-		}
-		if (_pos > 1) { // start byte and length byte not included in checksum
-			_chk += b;
-		}
-        
-        switch(_pos) {
-            case 0: // start frame
-                _pos++;
-                continue;
-            case 1: // length
-                _len = b;
-                _pos++;
-                continue;
-            case 2:
-                _api = b;
-                _pos++;
-                continue;
-            default:
-                if (_pos > MAX_SIZE) continue; // this probably can't happen since the xbee packet size is larger than any of our messages
-                if (_pos == (_len + 2)) {
-                    if (_chk == 0xff) {
-						protocol_send_message(_api, _buf, _len - 1);
-					} else {
-						_err = 1;
-                    }
-                    _pos = 0;
-                    _chk = 0;
-                } else {
-                    _buf[_pos++ - 3] = b;
-                }
-        }
-    }
+static void protocol_poll_pilot(){
+	uint8_t b;
+	while (comm_pilot_available() && comm_pilot_read(&b)) {
+		_protocol_poll(b, &pilot, 0x01);
+	}
+}
+
+static void protocol_poll_pc() {
+	uint8_t b;
+	while (comm_pc_available() && comm_pc_read(&b)) {
+		_protocol_poll(b, &pc, 0x00);
+	}
+}
+
+void protocol_poll() {
+	protocol_poll_pilot();
+	protocol_poll_pc();
 }
 
