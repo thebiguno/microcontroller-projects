96b209af4cf4ea12476e0747881c22efac246242
Warren Janssens <warren@Oaxaca.local>
2011-04-06 20:12:32 -0600

    got compiling and working again

--- control/modules/control/psx/psx_test.c
+++ control/modules/control/psx/psx_test.c
@@ -8,9 +8,10 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include "../../../pins/644.h"
 #include "../control.h"
 #include "../../../lib/psx/psx.h"
-#include "../../../lib/serial/serial.h"
+#include "../../../lib/serial1/serial1.h"
 
 char temp[32];
 
@@ -20,18 +21,21 @@ uint16_t buttons, last_buttons;
 int main (void){
 	//Do setup here
 
-	serial_init(9600, 8, 0, 1);
-
+	serial1_init_b(9600);
+	serial1_write_s("--reset--\n\r");
+	
 	control_init();
+	serial1_write_s("--init--\n\r");
 
 	//Main program loop
 	while (1){
 
 		_delay_ms(10);
 
-		
-		control = get_control();
-        buttons = get_buttons(); 
+		control_update();
+		control = control_read_analog();
+		buttons = control_button_state();
+//		uint16_t buttons_changed = control_button_state_changed();
 
 		if (control.pitch != last_control.pitch 
 				|| control.roll != last_control.roll 
@@ -40,31 +44,27 @@ int main (void){
 			last_control = control;
 
 			sprintf(temp, "%g", control.pitch);
-			serial_write_s(temp);
-			serial_write_s(", ");
+			serial1_write_s(temp);
+			serial1_write_s(", ");
 			sprintf(temp, "%g", control.roll);
-			serial_write_s(temp);
-			serial_write_s(", ");
+			serial1_write_s(temp);
+			serial1_write_s(", ");
 			sprintf(temp, "%g", control.yaw);
-			serial_write_s(temp);
-			serial_write_s(", ");
+			serial1_write_s(temp);
+			serial1_write_s(", ");
 			sprintf(temp, "%g", control.throttle);
-			serial_write_s(temp);
-			serial_write_s("\n\r");
+			serial1_write_s(temp);
+			serial1_write_s("\n\r");
 		}
 		
 		if (buttons != last_buttons) {
             last_buttons = buttons;
             
-            if ((buttons & POWER_ON)) serial_write_s(">");
-            if ((buttons & MODE_STABLE)) serial_write_s("X");
-            if ((buttons & MODE_SPORT)) serial_write_s("O");
-            if ((buttons & MODE_AEROBATIC)) serial_write_s("#");
-            if ((buttons & MODE_HOVER)) serial_write_s("^");
-            serial_write_s("\n\r");
+            if ((buttons & POWER)) serial1_write_s(">");
+            if ((buttons & MODE_STABLE)) serial1_write_s("X");
+            if ((buttons & MODE_SPORT)) serial1_write_s("O");
+            serial1_write_s("\n\r");
 		}
-		
-		
 
 	}
 }
