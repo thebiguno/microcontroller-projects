c54bd6c2495d2b51fde49c49e2678da70cad27fb
Wyatt <wyatt@digitalcave.ca>
2011-04-13 22:14:00 -0600

    Fixed bugs in 644 psx test

--- atmega644/psx_controller/main.c
+++ atmega644/psx_controller/main.c
@@ -8,7 +8,7 @@
 #include <util/delay.h>
 
 #include "lib/psx/psx.h"
-#include "lib/serial1/serial1.h"
+#include "lib/serial/serial.h"
 
 char temp[32];
 uint8_t last_LX = 0;
@@ -19,12 +19,12 @@ uint8_t last_RY = 0;
 int main (void){
 	//Do setup here
 
-	serial1_init(9600, 8, 0, 1);
+	serial_init(9600, 8, 0, 1);
 
-	psx_init(&PORTB, PINB1, //Data (Brown)
-		&PORTB, PINB0, //Clock (Blue)
-		&PORTB, PINB2, //Command (Orange)
-		&PORTB, PINB3); //Attention (Yellow)
+	psx_init(&PORTA, PINA2, //Data (Brown)
+		&PORTA, PINA3, //Clock (Blue)
+		&PORTA, PINA1, //Command (Orange)
+		&PORTA, PINA0); //Attention (Yellow)
 
 	uint16_t state = 0;
 	uint16_t change = 0;
@@ -39,47 +39,46 @@ int main (void){
 		change = state ^ read;
 		state = read;
 		
-		if (state & PSB_SELECT && change & PSB_SELECT) serial1_write_s("Select\n\r");
-		if (psx_button(PSB_L1)) serial1_write_s("Left 1\n\r");
-		if (psx_button(PSB_L2)) serial1_write_s("Left 2\n\r");
-		if (psx_button(PSB_L3)) serial1_write_s("Left 3\n\r");
-		if (psx_button(PSB_R1)) serial1_write_s("Right 1\n\r");
-		if (psx_button(PSB_R2)) serial1_write_s("Right 2\n\r");
-		if (psx_button(PSB_R3)) serial1_write_s("Right 3\n\r");
-		if (state & PSB_START && change & PSB_START) serial1_write_s("Start\n\r");
-		if (psx_button(PSB_PAD_UP)) serial1_write_s("Pad Up\n\r");
-		if (psx_button(PSB_PAD_LEFT)) serial1_write_s("Pad Left\n\r");
-		if (psx_button(PSB_PAD_DOWN)) serial1_write_s("Pad Down\n\r");
-		if (psx_button(PSB_PAD_RIGHT)) serial1_write_s("Pad Right\n\r");
-		if (state & PSB_TRIANGLE && change & PSB_TRIANGLE) serial1_write_s("Triangle\n\r");
-		if (psx_button(PSB_CIRCLE)) serial1_write_s("Circle\n\r");
-		if (psx_button(PSB_CROSS)) serial1_write_s("Cross\n\r");
-		if (psx_button(PSB_SQUARE)) serial1_write_s("Square\n\r");
+		if (psx_button(PSB_SELECT)) serial_write_s("Select\n\r");
+		if (psx_button(PSB_L1)) serial_write_s("Left 1\n\r");
+		if (psx_button(PSB_L2)) serial_write_s("Left 2\n\r");
+		if (psx_button(PSB_L3)) serial_write_s("Left 3\n\r");
+		if (psx_button(PSB_R1)) serial_write_s("Right 1\n\r");
+		if (psx_button(PSB_R2)) serial_write_s("Right 2\n\r");
+		if (psx_button(PSB_R3)) serial_write_s("Right 3\n\r");
+		if (psx_button(PSB_START)) serial_write_s("Start\n\r");
+		if (psx_button(PSB_PAD_UP)) serial_write_s("Pad Up\n\r");
+		if (psx_button(PSB_PAD_LEFT)) serial_write_s("Pad Left\n\r");
+		if (psx_button(PSB_PAD_DOWN)) serial_write_s("Pad Down\n\r");
+		if (psx_button(PSB_PAD_RIGHT)) serial_write_s("Pad Right\n\r");
+		if (psx_button(PSB_TRIANGLE)) serial_write_s("Triangle\n\r");
+		if (psx_button(PSB_CIRCLE)) serial_write_s("Circle\n\r");
+		if (psx_button(PSB_CROSS)) serial_write_s("Cross\n\r");
+		if (psx_button(PSB_SQUARE)) serial_write_s("Square\n\r");
 
 		if (psx_stick(PSS_LX) != last_LX){
 			last_LX = psx_stick(PSS_LX);
-			serial1_write_s("LX: ");
-			serial1_write_s(itoa(last_LX, temp, 16));
-			serial1_write_s("\n\r");
+			serial_write_s("LX: ");
+			serial_write_s(itoa(last_LX, temp, 16));
+			serial_write_s("\n\r");
 		}
 		if (psx_stick(PSS_LY) != last_LY){
 			last_LY = psx_stick(PSS_LY);
-			serial1_write_s("LY: ");
-			serial1_write_s(itoa(last_LY, temp, 16));
-			serial1_write_s("\n\r");
+			serial_write_s("LY: ");
+			serial_write_s(itoa(last_LY, temp, 16));
+			serial_write_s("\n\r");
 		}
 		if (psx_stick(PSS_RX) != last_RX){
 			last_RX = psx_stick(PSS_RX);
-			serial1_write_s("RX: ");
-			serial1_write_s(itoa(last_RX, temp, 16));
-			serial1_write_s("\n\r");
+			serial_write_s("RX: ");
+			serial_write_s(itoa(last_RX, temp, 16));
+			serial_write_s("\n\r");
 		}
 		if (psx_stick(PSS_RY) != last_RY){
 			last_RY = psx_stick(PSS_RY);
-			serial1_write_s("RY: ");
-			serial1_write_s(itoa(last_RY, temp, 16));
-			serial1_write_s("\n\r");
+			serial_write_s("RY: ");
+			serial_write_s(itoa(last_RY, temp, 16));
+			serial_write_s("\n\r");
 		}
-
 	}
 }
