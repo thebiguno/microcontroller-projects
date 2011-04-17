3044f5ee50c3af4901d99a8222dd342227cbc860
Wyatt <wyatt@digitalcave.ca>
2011-04-16 23:47:03 -0600

    Test program draws X on screen

--- atmega168/glcd/main.c
+++ atmega168/glcd/main.c
@@ -8,25 +8,31 @@
 #include <util/delay.h>
 
 #include "lib/glcd/glcd.h"
+#include "lib/glcd/draw.h"
+#include "lib/glcd/glcd_st7565r.h"
 
 
 int main (void){
 	//Do setup here
 	_delay_ms(500);
 	
-	glcd_init(&PORTD, PIND3, &PORTD, PIND2, &PORTD, PIND1, &PORTD, PIND0, &PORTD, PIND4);
+	st7565r_init(&PORTD, PIND3, &PORTD, PIND2, &PORTD, PIND1, &PORTD, PIND0, &PORTD, PIND4);
 	
 	_delay_ms(500);
 	
-	do_something();
 	
+	glcd_draw_line(0, 0, 128, 32);
+	glcd_draw_line(0, 32, 128, 0);
+//	for (uint8_t i = 0; i < 6; i++){
+//		glcd_set_pixel(i, 2, 1);
+//	}
+
+	glcd_write_buffer();
 	
 	DDRB |= _BV(PINB0);
 	
 	while(1){
 		PORTB = ~PORTB;
 		_delay_ms(500);
-		
-		do_something();
 	}
 }
