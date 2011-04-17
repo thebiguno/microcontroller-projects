ce10b12b292db1b4b2a88144b18861db97c6bc90
Wyatt <wyatt@digitalcave.ca>
2011-04-17 16:30:52 -0600

    Updated makefile for internal 8mhz clock

--- atmega168/glcd/main.c
+++ atmega168/glcd/main.c
@@ -14,11 +14,11 @@
 
 int main (void){
 	//Do setup here
-	_delay_ms(500);
+//	_delay_ms(500);
 	
-	st7565r_init(&PORTD, PIND3, &PORTD, PIND2, &PORTD, PIND1, &PORTD, PIND0, &PORTD, PIND4);
+	st7565r_init(&PORTD, PIND3, &PORTD, PIND2, &PORTD, PIND1, &PORTD, PIND0);
 	
-	_delay_ms(500);
+//	_delay_ms(500);
 	
 	
 	glcd_draw_line(0, 0, 128, 32, OVERLAY_OR);
