c3e4f7bdf74567c329fae3552bec8fc2dc47f366
Wyatt <wyatt@digitalcave.ca>
2011-06-25 20:50:29 -0600

    If throttle is < 1%, don't enable motors at all

--- pilot/modules/motor/plus/motor_plus.c
+++ pilot/modules/motor/plus/motor_plus.c
@@ -1,10 +1,10 @@
 #include "../motor.h"
 
-#define MOTOR_LIMIT(x) (x > 1.0 ? 1.0 : (x < 0.0 ? 0.0 : x))
+#define MOTOR_LIMIT(x, t) (t > 0.01 ? (x > 1.0 ? 1.0 : (x < 0.0 ? 0.0 : x)) : 0.0)
 
 void motor_percent(double throttle, vector_t mv, double result[]) {
-    result[0] = MOTOR_LIMIT(throttle + mv.y + mv.z); // left
-    result[1] = MOTOR_LIMIT(throttle + mv.x - mv.z); // front
-    result[2] = MOTOR_LIMIT(throttle - mv.y + mv.z); // right
-    result[3] = MOTOR_LIMIT(throttle - mv.x - mv.z); // back
+    result[0] = MOTOR_LIMIT(throttle + mv.y + mv.z, throttle); // left
+    result[1] = MOTOR_LIMIT(throttle + mv.x - mv.z, throttle); // front
+    result[2] = MOTOR_LIMIT(throttle - mv.y + mv.z, throttle); // right
+    result[3] = MOTOR_LIMIT(throttle - mv.x - mv.z, throttle); // back
 }
