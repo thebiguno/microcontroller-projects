1d2441ba0b8c46363811bf737bbc288594e4a292
Wyatt <wyatt@digitalcave.ca>
2011-06-28 22:51:13 -0600

    Adjusted theoretical values to make 9.6v read as empty battery.  This still needs to be tested in real life

--- pilot/modules/battery/battery.h
+++ pilot/modules/battery/battery.h
@@ -1,8 +1,11 @@
 #ifndef BATTERY_H
 #define BATTERY_H
 
-#define BATTERY_MIN 256
-#define BATTERY_MAX 1024
+
+//When calculating these, the /3.55 comes from the voltage divider, and the /3.3 comes
+// from the AVCC = 3v.
+#define BATTERY_MIN 839		//About 9.6v (9.6/3.55 / 3.3 * 1024)
+#define BATTERY_MAX 1024	//About 12v (12/3.55 / 3.3 * 1024) 
 
 #include "../../main.h"
 
