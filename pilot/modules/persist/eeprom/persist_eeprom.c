ea8223175ce5bf175cd1aceec662902f27ab3815
Wyatt Olson <wyatt.olson@gmail.com>
2011-06-30 16:08:21 -0600

    Added better comments to persist module

--- pilot/modules/persist/eeprom/persist_eeprom.c
+++ pilot/modules/persist/eeprom/persist_eeprom.c
@@ -20,12 +20,16 @@ uint8_t persist_write(uint8_t section, uint8_t *data, uint8_t length){
 		return 0;
 	}
 	
+	//There can be up to 8 sections (0..7), each of which are up to 2^6 - 1 (63, 0x3E) bytes in length.
 	uint16_t eeprom_address = (section & 0x7) << 6;
+	//The checksum starts at 42, and adds (unsigned 8 bit) each byte.  This must be the same when reading out the value, or an error is returned.
 	uint8_t checksum = PERSIST_EEPROM_MAGIC_NUMBER;
+	//We start writing at the address offset + 0, and increment from there.
 	for (uint8_t i = 0; i < length; i++){
 		eeprom_write_c(eeprom_address++, data[i]);
 		checksum += data[i];
 	}
+	//Write the checksum.
 	eeprom_write_c(eeprom_address++, checksum);
 	
 	return length;
@@ -45,7 +49,7 @@ uint8_t persist_read(uint8_t section, uint8_t *data, uint8_t length){
 	}
 	
 	if (checksum != eeprom_read_c(eeprom_address++)){
-		return 0;
+		return 0;	//Checksum didn't match
 	}
 	
 	return length;	
