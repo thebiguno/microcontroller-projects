870705ca9c9b3bce9aa90f09300786f811c54ca2
Wyatt <wyatt@digitalcave.ca>
2011-10-29 21:38:18 -0600

    Improved candle, making more realistic flickering

--- attiny85/candle/main.c
+++ attiny85/candle/main.c
@@ -3,51 +3,51 @@
 #include <util/delay.h>
 #include "lib/pwm/pwm.h"
 
-int main (void){
 /*
-	uint16_t l0 = 100, l1 = 1000;
-	uint16_t s0, s1;	//Step; will be assigned randomly
+ * A flickering candle driver.  Run a number of LEDs off of PORTB0 - PORTB4, 
+ * using transistors as a switch.  Recommended at least two chains (in parallel),
+ * the larger one of amber LEDs being about twice as long as the shorter one of red
+ * LEDs.  Optionally you can have additional chains, of yellow, orange, red, or white
+ * LEDs.  PORTBx is connected to the base of each transistor to switch it.
+ */
 
-	//Do setup here
-	volatile uint8_t *ports[2];
-	ports[0] = &PORTB;
-	ports[1] = &PORTB;
+//Period
+#define P 1000
+//Number of pins, starting at PORTB1.  Max of 4.
+#define NUM 2
+
+int main (void){
+	uint16_t a[NUM];	//Accumulated value
+	uint8_t s[NUM];		//Step; assigned randomly
+	uint8_t d[NUM];		//Direction
 	
+	volatile uint8_t *ports[NUM];
 	uint8_t pins[2];
-	pins[0] = PORTB2;	//Yellow
-	pins[1] = PORTB1;	//Red
+	
+	for (uint8_t i = 0; i < NUM; i++){
+		a[i] = P / 2;
+		d[i] = 0;
+		ports[i] = &PORTB;
+		pins[i] = i + 1;	//Change this to i if we start counting from B0, instead of B1
+	}
 
-	pwm_init(ports, pins, 2, 2000);
-	pwm_set_phase(0, l0);
-	pwm_set_phase(1, l1);
+	pwm_init(ports, pins, NUM, P);
 
 	//Main program loop
 	while (1){
-		s0 = random() & 0xFFF;
-		s1 = random() & 0xFFFFF;	//More flicker in red
-	
-		if (s0 & 0x1) l0 += s0;
-		else l0 -= s0;
+		for (uint8_t i = 0; i < NUM; i++){
+			d[i] = (i == 0 ? 2 : (i == 1 ? 3 : (i == 2 ? 5 : (i == 3 ? 7 : i * 4 + 1))));
+			s[i] = random() & 0x7F;	//The larger the and'ed value, the more flicker
+			a[i] += s[i] * d[i];
+			
+			//Keep accumulated values within bounds
+			if (a[i] < 10 || a[i] > P - 10) a[i] = P / 2;
+			
+			//Update the PWM
+			pwm_set_phase(i, a[i]);
+		}
 		
-		if (s1 & 0x1) l1 += s1;
-		else l1 -= s1;
-
-		pwm_set_phase(0, l0);
-		pwm_set_phase(1, l1);
-		_delay_ms(100);
-	}
-*/
-
-	DDRB |= _BV(PORTB1) | _BV(PORTB2);
-	
-	uint16_t r;
-	while(1){
-		r = random();
-		if (r & 0x1) PORTB |= _BV(PORTB1);
-		else PORTB &= ~_BV(PORTB1);
-		
-		if (r & 0x11) PORTB |= _BV(PORTB2);
-		else PORTB &= ~_BV(PORTB2);
+		_delay_ms(50);
 	}
 }
 
