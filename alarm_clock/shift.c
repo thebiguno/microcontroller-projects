a83d6c0018c48bd253a85af0ced6ab8c4999417e
Wyatt <wyatt@digitalcave.ca>
2012-01-21 10:12:41 -0700

    Modified pin / shift register mappings to work with standalone board connections

--- alarm_clock/shift.c
+++ alarm_clock/shift.c
@@ -44,98 +44,127 @@ void shift_format_data(uint8_t hours, uint8_t minutes, uint8_t flags, uint32_t *
 	//Here we map between LED pins to shift register outputs, counting down from bit 14: 
 	// LED Pin	Shift Data		Shift Pin
 	// 4 (Auto)	23				3.11
-	// 2 (C1)	17				3.5
-	// 1 (C2)	16				3.4
-	// 30/14(:)	15				2.11
-	// 5 (PM)	14				2.12
-	// 6		13				2.13
-	// 7		12				2.14
-	// 8		11				2.7
-	// 9		10				2.6
+	// 2 (C1)	17				3.11
+	// 1 (C2)	16				3.12
+	// 30/14(:)	15				3.11
+	// 5 (PM)	14				2.11
+	// 6		13				2.12
+	// 7		12				2.13
+	// 8						2.14
+	// 9		8				2.4
 	// 10		9				2.5
-	// 12		8				2.4
+	// 11		10				2.6
+	// 12		11				2.7
 	// 13		7				1.11
-	// 15		6				1.12
-	// 16		5				1.13
-	// 17		4				1.14
-	// 18		3				1.7
-	// 19		2				1.6
-	// 20		1				1.5
-	// 21		0				1.4
+	// 14		7				1.14
+	// 15		6				1.13
+	// 16		5				1.12
+	// 17		4				1.11
+	// 18		0				1.4
+	// 19		1				1.5
+	// 20		2				1.6
+	// 21		3				1.7
 	
-	#define CATHODE_1	_BV32(17)
-	#define CATHODE_2	_BV32(16)
+	//Defines mappings between Led Pin X and the data packet index
+	#define LP1		23
+	#define LP2		22
+	#define LP3		//Connected to LP2
+	#define LP4		18
+	#define LP5		19
+	#define LP6		12
+	#define LP7		13
+	#define LP8		14
+	#define LP9		15
+	#define LP10	8
+	#define LP11	9
+	#define LP12	10
+	#define LP13	11
+	#define LP14	4
+	#define LP15	5
+	#define LP16	6
+	#define LP17	7
+	#define LP18	0
+	#define LP19	1
+	#define LP20	2
+	#define LP21	3		
 	
-	#define COLON_1	_BV32(15)	//If we use normal _BV() it assumes unsigned, and fills in all 1's
+	#define CATHODE_1	_BV32(LP1)
+	#define CATHODE_2	_BV32(LP2)
+	
+	#define COLON_1	_BV32(LP14)	//If we use normal _BV() it assumes unsigned, and fills in all 1's
 	#define COLON_2	0
-	#define PM_1	_BV(14)
+	#define PM_1	_BV32(LP4)
 	#define PM_2	0
 	#define AUTO_1	0
-	#define AUTO_2	_BV32(23)
-	
+	#define AUTO_2	_BV32(LP5)
+
 	//    HourMinNumber_Segment_Cathode
 	// H2: A=(1,7), B=(2,6), C=(2,9), D=(1,8), E=(2,8), G=(2,7) (F is not implemented, so 0, 4, 5, 6, 8, 9 are not possible)
-	#define H2_A_1 	_BV(12)
+	#define H2_A_1 	_BV32(LP7)
 	#define H2_A_2 	0
 	#define H2_B_1 	0
-	#define H2_B_2	_BV(13)
+	#define H2_B_2	_BV32(LP6)
 	#define H2_C_1	0
-	#define H2_C_2	_BV(10)
-	#define H2_D_1	_BV(11)
+	#define H2_C_2	_BV32(LP9)
+	#define H2_D_1	_BV32(LP8)
 	#define H2_D_2	0
 	#define H2_E_1	0
-	#define H2_E_2	_BV(11)
+	#define H2_E_2	_BV32(LP8)
 	#define H2_F_1	0
 	#define H2_F_2	0
 	#define H2_G_1	0
-	#define H2_G_2	_BV(12)
-	
+	#define H2_G_2	_BV32(LP7)
+	// 8		8				2.4
+	// 9		9				2.5
+	// 10		10				2.6
+	// 12		11				2.7
+		
 	// H1: A=(2,13), B=(2,10), C=(2,12), D=(1,12), E=(1,9), F=(1,13), G=(1,10)
 	#define H1_A_1	0
-	#define H1_A_2	_BV(7)
+	#define H1_A_2	_BV32(LP13)
 	#define H1_B_1	0
-	#define H1_B_2	_BV(9)
+	#define H1_B_2	_BV32(LP10)
 	#define H1_C_1	0
-	#define H1_C_2	_BV(8)
-	#define H1_D_1	_BV(8)
+	#define H1_C_2	_BV32(LP12)
+	#define H1_D_1	_BV32(LP12)
 	#define H1_D_2	0
-	#define H1_E_1	_BV(10)
+	#define H1_E_1	_BV32(LP9)
 	#define H1_E_2	0
-	#define H1_F_1	_BV(7)
+	#define H1_F_1	_BV32(LP13)
 	#define H1_F_2	0
-	#define H1_G_1	_BV(9)
+	#define H1_G_1	_BV32(LP10)
 	#define H1_G_2	0
 	
 	// M2: A=(1,15), B=(1,16), C=(1,17), F=(2,15), G=(2,16), D=(2,17), E=(2,18)
-	#define M2_A_1	_BV(6)
+	#define M2_A_1	_BV(LP15)
 	#define M2_A_2	0
-	#define M2_B_1	_BV(5)
+	#define M2_B_1	_BV(LP16)
 	#define M2_B_2	0
-	#define M2_C_1	_BV(4)
+	#define M2_C_1	_BV(LP17)
 	#define M2_C_2	0
 	#define M2_D_1	0
-	#define M2_D_2	_BV(4)
+	#define M2_D_2	_BV(LP17)
 	#define M2_E_1	0
-	#define M2_E_2	_BV(3)
+	#define M2_E_2	_BV(LP18)
 	#define M2_F_1	0
-	#define M2_F_2	_BV(6)
+	#define M2_F_2	_BV(LP15)
 	#define M2_G_1	0
-	#define M2_G_2	_BV(5)
+	#define M2_G_2	_BV(LP16)
 
 	// M1: E=(1,18), G=(1,19), D=(1,20), F=(1,21), B=(2,19), C=(2,20), A=(2,21)
 	#define M1_A_1	0
-	#define M1_A_2	_BV(0)
+	#define M1_A_2	_BV(LP21)
 	#define M1_B_1	0
-	#define M1_B_2	_BV(2)
+	#define M1_B_2	_BV(LP19)
 	#define M1_C_1	0
-	#define M1_C_2	_BV(1)
-	#define M1_D_1	_BV(1)
+	#define M1_C_2	_BV(LP20)
+	#define M1_D_1	_BV(LP20)
 	#define M1_D_2	0
-	#define M1_E_1	_BV(3)
+	#define M1_E_1	_BV(LP18)
 	#define M1_E_2	0
-	#define M1_F_1	_BV(0)
+	#define M1_F_1	_BV(LP21)
 	#define M1_F_2	0
-	#define M1_G_1	_BV(2)
+	#define M1_G_1	_BV(LP19)
 	#define M1_G_2	0
 
 	
