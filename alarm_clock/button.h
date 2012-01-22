1d14e60a2b6a89c24afcc9a08d5d833f06ea3453
Wyatt <wyatt@digitalcave.ca>
2012-01-22 15:54:45 -0700

    All buttons are read on PORTB, so we can simplify logic

--- alarm_clock/button.h
+++ alarm_clock/button.h
@@ -4,6 +4,7 @@
 #include <avr/io.h>
 
 #define BUTTON_PORT					PORTB
+#define BUTTON_PIN					PINB
 #define BUTTON_ALARM_PIN			PINB0
 #define BUTTON_TIME_PIN				PINB1
 #define BUTTON_MINUTE_PIN			PINB2
@@ -11,13 +12,14 @@
 #define BUTTON_SLEEP_PIN			PINB4
 #define BUTTON_SNOOZE_PIN			PINB5
 
-//Flags in the return value of button_read()
-#define BUTTON_TIME					_BV(0)
-#define BUTTON_ALARM				_BV(1)
-#define BUTTON_SLEEP				_BV(2)
-#define BUTTON_HOUR					_BV(3)
-#define BUTTON_MINUTE				_BV(4)
-#define BUTTON_SNOOZE				_BV(5)
+//Flags in the return value of button_read().  For convenience and speed
+// we make sure that these are the same as the pins above.
+#define BUTTON_ALARM				_BV(BUTTON_ALARM_PIN)
+#define BUTTON_TIME					_BV(BUTTON_TIME_PIN)
+#define BUTTON_MINUTE				_BV(BUTTON_MINUTE_PIN)
+#define BUTTON_HOUR					_BV(BUTTON_HOUR_PIN)
+#define BUTTON_SLEEP				_BV(BUTTON_SLEEP_PIN)
+#define BUTTON_SNOOZE				_BV(BUTTON_SNOOZE_PIN)
 
 /*
  * Init hardware for reading buttons
