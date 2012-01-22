ddb877dd30c8cb915fb54c950100b256b717f389
Wyatt <wyatt@digitalcave.ca>
2012-01-22 15:54:18 -0700

    Optimized button reads to be in 2 or 3 cycles rather than 12ish (unsure of details due to compiler optimizations)

--- alarm_clock/button.c
+++ alarm_clock/button.c
@@ -9,13 +9,7 @@ void button_init(){
 
 uint8_t button_read(){
 	//We invert PINB so that we don't have to worry about inverse logic due to pullups.
-	uint8_t result = 0x00;
-	if (~PINB & _BV(BUTTON_TIME_PIN)) result |= BUTTON_TIME;
-	if (~PINB & _BV(BUTTON_ALARM_PIN)) result |= BUTTON_ALARM;
-	if (~PINB & _BV(BUTTON_SLEEP_PIN)) result |= BUTTON_SLEEP;
-	if (~PINB & _BV(BUTTON_HOUR_PIN)) result |= BUTTON_HOUR;
-	if (~PINB & _BV(BUTTON_MINUTE_PIN)) result |= BUTTON_MINUTE;
-	if (~PINB & _BV(BUTTON_SNOOZE_PIN)) result |= BUTTON_SNOOZE;
-	
-	return result;
+	return ~BUTTON_PIN & (_BV(BUTTON_TIME_PIN) | _BV(BUTTON_ALARM_PIN)
+			| _BV(BUTTON_SLEEP_PIN) | _BV(BUTTON_HOUR_PIN) 
+			| _BV(BUTTON_MINUTE_PIN) | _BV(BUTTON_SNOOZE_PIN));
 }
