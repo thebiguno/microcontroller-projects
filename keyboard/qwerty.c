2a9ba0ed66e16c58f20662b8fd6b7ed6112bbe62
Wyatt <wyatt@digitalcave.ca>
2012-02-06 08:49:54 -0700

    Added scancodes

--- keyboard/qwerty.c
+++ keyboard/qwerty.c
@@ -1,81 +1,30 @@
 /*
- * Keyboard driver.  Can drive a 8x16 keyboard matrix, plus has ICSP and USB HID output.
- * Uses an ATMega124 (32 I/O) running at 20MHz (requires a crystal).
- * 
- * Pinouts: 
- *		PORTA: 8x Row Drivers
- *		PORTB: 8x Column Readers + ICSP breakout
- *		PORTC: 8x Column Readers
- *		PORTD: Misc I/O
- *			D0, D1:	Serial Breakout (Rx Tx)
- *			D2, D3:	USB Data D+ / D- (via level converters)
- *			D4..D6:	Shift Register Outputs for LEDs
- *			D7: Unused?
+ * The mapping for scan codes to characters for QWERTY.
  */
+//uint8_t map[128] = {
+///*	 	0	1	2	3	4	5	6	7	8	9	A	B	C	D	E	F	*/
+///* 0 */		NP.	UP	LALT		NP0		F6	F5	'		H	G	F4		ESC
+///* 1 */		NP6	NP+		LSFT	NP5	NP4	]	BS	[	F7	Y	T	F3	CAP	TAB
+///* 2 */	LCTL	PU	HOME			INS	DEL	=	F9	-	F8	6	5	F2	F1	~
+///* 3 */		PD	END	PRT		F12	F11	8	F10	0	9	7	4	3	2	1
+///* 4 */		NP9		SL		NP8	NP7	I		P	O	U	R	E	W	Q
+///* 5 */		NP3	NPEN			NP2	NP1	K	\	;	L	J	F	D	S	A
+///* 6 */	RCTL	NP*	BRK		RSFT	NP/	NL	,	EN		.	M	V	C	X	Z
+///* 7 */		NP-	LEFT	LALT		RGHT	DWN		SP	/		N	B
+//}
 
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdlib.h>
-
-#include "lib/serial/serial.h"
-
-/*
- * Sets the row (0..7)
- */
-void set_row(uint8_t row){
-	PORTA = ~(_BV(row));
+uint8_t map[128] = {
+/*	 0	1	2	3	4	5	6	7	8	9	A	B	C	D	E	F	*/
+/* 0 */		NP.	UP	LALT		NP0		F6	F5	'		H	G	F4		ESC
+/* 1 */		NP6	NP+		LSFT	NP5	NP4	]	BS	[	F7	Y	T	F3	CAP	TAB
+/* 2 */	LCTL	PU	HOME			INS	DEL	=	F9	-	F8	6	5	F2	F1	~
+/* 3 */		PD	END	PRT		F12	F11	8	F10	0	9	7	4	3	2	1
+/* 4 */		NP9		SL		NP8	NP7	I		P	O	U	R	E	W	Q
+/* 5 */		NP3	NPEN			NP2	NP1	K	\	;	L	J	F	D	S	A
+/* 6 */	RCTL	NP*	BRK		RSFT	NP/	NL	,	EN		.	M	V	C	X	Z
+/* 7 */		NP-	LEFT	LALT		RGHT	DWN		SP	/		N	B
 }
 
-/*
- * Reads and debounces the column.  High bits indicate a key press.
- */
-uint16_t read_columns(){
-	uint16_t result = PINC;
-	result = result << 8;
-	result += PINB;
-	result = ~result;
-	return result;
-}
+void send_key(uint8_t row, uint8_t column){
 
-int main (void){
-	serial_init_b(9600);
-	
-	//Set rows into low impedence (write) mode
-	DDRA = 0xFF;
-	
-	//Ensure columns are high impedence (read) with pullups
-	DDRB = 0x00;
-	DDRC = 0x00;
-	PORTB = 0xFF;
-	PORTC = 0xFF;
-
-	//Used as itoa destination
-	char temp[32];
-
-	uint8_t row = 0x00;
-	uint16_t columns = 0x00;
-	
-	//Main program loop
-	while (1){
-		set_row(row);
-		_delay_ms(1);
-		columns = read_columns();
-	
-		if (columns != 0x00){
-			for (uint8_t i = 0; i < 16; i++){
-				if (_BV(i) & columns){
-					serial_write_s("Scan Code:\t");
-					serial_write_s(itoa(row, temp, 16));
-					serial_write_s("\t");
-					serial_write_s(itoa(i, temp, 16));
-//					serial_write_s(itoa(columns, temp, 16));
-					serial_write_s("\n\r");
-					_delay_ms(100);
-				}
-			}
-		}
-	
-		row++;
-		if (row > 7) row = 0;
-	}
 }
