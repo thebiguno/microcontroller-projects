08b7b8ba3060669cd2587669206f77503fea41eb
Warren Janssens <warren.janssens@gmail.com>
2012-02-13 16:03:32 -0700

    separate red and green drawing so that orange doesn't double up

--- main/matrix/matrix.c
+++ main/matrix/matrix.c
@@ -53,28 +53,34 @@ uint8_t bits_set(uint8_t v) {
 }
 
 void matrix_draw(uint8_t red[], uint8_t green[]) {
+	static uint8_t color = 0;
 	static uint8_t row = 0;
 	static uint8_t dc_r[8];
 	static uint8_t dc_g[8];
 	
-	matrix_data(0xFF-_BV(row));
-	if (bits_set(red[row]) >= dc_r[row]) {
-		matrix_data(red[row]);
+	shift_data(0xFF-_BV(row));
+	if (color == 0) {
+		if (bits_set(red[row]) >= dc_r[row]) {
+			shift_data(red[row]);
+		} else {
+			shift_data(0x00);
+		}
+		shift_data(0x00);
+		dc_r[row]++;
+		if (dc_r[row] > 7) dc_r[row] = 0;
 	} else {
-		matrix_data(0x00);
+		shift_data(0x00);
+		if (bits_set(green[row]) >= dc_g[row]) {
+			shift_data(green[row]);
+		} else {
+			shift_data(0x00);
+		}
+		dc_g[row]++;
+		if (dc_g[row] > 7) dc_g[row] = 0;
+
+		row++;
+		if (row > 7) row = 0;
 	}
-	if (bits_set(green[row]) >= dc_g[row]) {
-		matrix_data(green[row]);
-	} else {
-		matrix_data(0x00);
-	}
-	matrix_latch();
 	
-	dc_r[row]++;
-	if (dc_r[row] > 7) dc_r[row] = 0;
-	dc_g[row]++;
-	if (dc_g[row] > 7) dc_g[row] = 0;
-	
-	row++;
-	if (row > 7) row = 0;
+	color = ~color;
 }
