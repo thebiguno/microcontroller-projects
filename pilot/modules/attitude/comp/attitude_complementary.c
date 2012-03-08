737aaaf71d068dc927d42ac7142619c5f718dc3a
Warren Janssens <warren.janssens@gmail.com>
2012-03-08 10:59:40 -0700

    updating complementary so that it compiles

--- pilot/modules/attitude/comp/attitude_complementary.c
+++ pilot/modules/attitude/comp/attitude_complementary.c
@@ -1,8 +1,8 @@
 #include <math.h>
+#include <util/delay.h>
 
 #include "../../../main.h"
 
-#define DEG_PT5 0.00872664626
 #define DEG_30 1.570796326794897
 /*
  * A complementary filter derived from the following:
@@ -14,15 +14,39 @@ static vector_t filter;    // complementary filter output (and output of second 
 static vector_t k;         // tuning paramater -- bandwidth of filter
 uint64_t millis;
 
+void attitude_read_tuning() {
+	uint8_t data[24];
+	
+	// uint8_t length = persist_read(PERSIST_SECTION_ATTITUDE, data, 8);
+	// if (length == 8) {
+	// 	k.x = convert_bytes_to_double(data, 0);
+	// 	k.y = convert_bytes_to_double(data, 4);
+	// } else {
+	// 	status_error(STATUS_ERR_ATTITUDE_CHECKSUM);
+	// 	_delay_ms(1000);
+	// 	status_error(0x00);
+		
+		k.x = 1;
+		k.y = 1;
+	// }
+}
+
+void attitude_write_tuning() {
+	uint8_t data[8];
+	
+	convert_double_to_bytes(k.x, data, 0);
+	convert_double_to_bytes(k.y, data, 4);
+	
+	persist_write(PERSIST_SECTION_ATTITUDE, data, 8);
+}
+
 void attitude_init(vector_t gyro, vector_t accel) {
-    // TODO read this from EEPROM
-    k.x = 1;
-    k.y = 1;
-    k.z = 1;
+	attitude_read_tuning();
 
     filter.x = accel.x;
     filter.y = accel.y;
     filter.z = accel.z;
+
     //account for gyro bias
     int_y1.x = -gyro.x;
     int_y1.y = -gyro.y;
@@ -30,42 +54,47 @@ void attitude_init(vector_t gyro, vector_t accel) {
 }
 
 void _attitude(double *gyro, double *_accel, double *_accel_z, double *_int_y1, double *_filter, double k, double dt) {
-    double accel;
-    if (*_accel_z < DEG_PT5 || *_filter > DEG_30 || *_filter < -DEG_30) {
-        accel = *_accel;
-    } else {
-        accel = atan2(*_accel, *_accel_z);
-    }
+	double accel;
+	if (*_accel_z < 0.5 || *_filter > DEG_30 || *_filter < -DEG_30) {
+		// if the craft is falling very fast, or tilted more than 30 degrees
+		// then ignore the accelerometer (accel - *_filter will be zero later on)
+		accel = *_filter;
+	} else {
+		accel = atan2(*_accel, *_accel_z);
+	}
 
-    double int_x1 = (accel - *_filter) * k *  k;                    // input to the first integrator (rad/sec/sec)
-    *_int_y1 += int_x1 * dt;
-    double int_x2 = *_int_y1 + (accel - *_filter) * 2 *  k + *gyro;  // input to the second integrator (rad/sec)
-    *_filter = (int_x2 * dt) + *_filter;
+	double int_x1 = (accel - *_filter) * k * k;							// input to the first integrator (rad/s/s) -- rate of change
+	*_int_y1 += int_x1 * dt;											// (rad/s)
+	double int_x2 = *_int_y1 + (accel - *_filter) * 2 * k + *gyro;		// input to the second integrator (rad/s)
+	*_filter += int_x2 * dt;											// (rad)
 }
 
-vector_t attitude(vector_t gyro, vector_t accel) {
-    double dt_s = dt * 0.001;
-
-    _attitude(&gyro.x, &accel.x, &accel.z, &int_y1.x, &filter.x, k.x, dt_s);
-    _attitude(&gyro.y, &accel.y, &accel.z, &int_y1.y, &filter.y, k.y, dt_s);
-    
-    return filter;
+vector_t attitude(vector_t gyro, vector_t accel, double dt) {
+	_attitude(&gyro.x, &accel.x, &accel.z, &int_y1.x, &filter.x, k.x, dt);
+	_attitude(&gyro.y, &accel.y, &accel.z, &int_y1.y, &filter.y, k.y, dt);
+	return filter;
 }
 
 void attitude_reset() {
-    filter.x = 0;
-    filter.y = 0;
-    filter.z = 0;
+	filter.x = 0;
+	filter.y = 0;
 }
 
 uint8_t attitude_get_id() {
-    return 'C';
+	return 'C';
 }
 
-void attitude_get_params(double params[]) {
+void attitude_send_tuning() {
+	uint8_t length = 8;
+	uint8_t buf[length];
+	
+	convert_double_to_bytes(k.x, buf, 0);
+	convert_double_to_bytes(k.y, buf, 4);
 
+	protocol_send_message('c', buf, length);
 }
 
-void attitude_set_params(double params[]) {
-
+void attitude_receive_tuning(uint8_t *buf) {
+	k.x = convert_bytes_to_double(buf, 0);
+	k.y = convert_bytes_to_double(buf, 4);
 }
