8988682229b81a316bbf6bf2c75fbde060f1628e
Wyatt Olson <olsonw@richer.ca>
2012-03-14 09:32:17 -0600

    Converted from software watchdog to AVR WDT support for throttling down with lost connection

--- pilot/main.c
+++ pilot/main.c
@@ -1,8 +1,12 @@
+#include <avr/wdt.h>
 #include <util/delay.h>
 #include "main.h"
 
-#define WATCHDOG_ALERT	13
 #define HEARTBEAT_OVERFLOW	16
+
+//State variables, used in both main and WDT ISR
+static volatile double throttle = 0.0;			//Throttle as sent from controller
+static volatile vector_t sp = { 0,0,0 };		//Attitude set point
 
 int main(){
 	//********************
@@ -28,6 +32,9 @@ int main(){
 	_delay_ms(500);
 	//Statup status is cleared after all init is completed
 
+	wdt_reset();
+	wdt_enable(WDTO_1S);
+
 	//********************
 	// Variable Declarations
 	//********************
@@ -36,15 +43,12 @@ int main(){
 	double seconds = 0;				//Last measured seconds (with close-to-microsecond resolution)
 	double curr_seconds;			//Current seconds
 	double dt;						//Time since last loop execution (how long the loop took; used for time sensitive calculations like Kalman)
-	uint8_t t = 0;					//Time since last signal was received
 
 	//State variables
 	uint8_t cmd_type;				//Can be any of the flight command types (a superset of armed_type)
-	uint8_t armed_type = 0x00;		//Will be either 'A' or 'M'; initialized to 0x00 until the first command is received.
+	uint8_t armed = 0;				//Boolean; 0 = not armed, !0 = armed
 	double flight_command_data[4];	//Data packet received with a flight command
-	double throttle = 0.0;			//Throttle as sent from controller
-	vector_t sp = { 0,0,0 };		// Attitude set point
-	double motor[4];				// Motor set point
+	double motor[4];				//Motor set point; used as an output from the motor module and input to ESC module
 	
 	uint8_t heartbeat_overflow = 0;	//This increments on every loop; when this reaches 128 (approx 1/2 second) we send heartbeat and telemetry
 	uint8_t battery_overflow = 0;	//This is incremented every time heartbeat_overflow overflows (i.e. just under every second); when this gets to 6 (about 3 seconds) we send battery info
@@ -64,10 +68,15 @@ int main(){
 	while (1) {
 		protocol_poll();
 		cmd_type = protocol_receive_flight_command(flight_command_data);
-		if (cmd_type == 'A' || cmd_type == 'M') {
-			armed_type = cmd_type;
-			//Reset watchdog
-			t = 0;
+		if (cmd_type == 'A') {
+			armed = 1;
+			//Reset watchdog; if we go 1 second without resetting, we hit the ISR
+			wdt_reset();
+		}
+		else if (cmd_type == 'M') {
+			armed = 0;
+			//Reset watchdog if we are unarmed, so that we don't constantly run the ISR
+			wdt_reset();
 		}
 
 		g = gyro_get();
@@ -80,7 +89,7 @@ int main(){
 		
 		pv = attitude(g, a, dt);					// compute PID process variable for x and y using Kalman
 
-		if (armed_type == 'A') {						// attitude command
+		if (armed) {
 			status_set(STATUS_ARMED);
 			
 			throttle = flight_command_data[0];
@@ -93,60 +102,15 @@ int main(){
 			motor_percent(throttle, mv, motor);
 			esc_set(motor);
 			
-			//Watchdog: check for communication timeouts
-			if (t > WATCHDOG_ALERT) {
-				status_clear(STATUS_ARMED);
-				
-				// level out 
-				sp.x = 0;
-				sp.y = 0;
-				sp.z = 0;
-				
-				//This should go from full throttle to off in about 20 seconds.  We are assuming 
-				// that this (the main loop) will execute every 10ms or so.  The maximum throttle 
-				// is 0.8, so to reduce from 0.8 to 0.0 in 2000 iterations (10ms per loop * 20 seconds),
-				// we want to throttle back by 0.8 / 2000 == 0.00004 each iteration.
-				if (throttle > 0.0) {
-					// scale back throttle
-					throttle = throttle - 0.0004;
-				}
-				else {
-					throttle = 0.0;
-				}
-			}
-			
 			//Reset PID if throttle is 0.
 			if (throttle < 0.01) {
 				pid_reset();
 			}
-		} else if (armed_type == 'M') {				// motor command
-			for (uint8_t i = 0; i < 4; i++) {
-				motor[i] = flight_command_data[i];
-			}
-			
-			//Clear armed if all motor values are 0 (this is sent from control module when disarmed)
-			if (motor[0] == 0 && motor[1] == 0 && motor[2] == 0 && motor[3] == 0) {
-				status_clear(STATUS_ARMED);
-			}
-			else {
-				status_set(STATUS_ARMED);
-			}
-			
-			//Watchdog: check for communication timeouts
-			if (t > WATCHDOG_ALERT) {
-				status_clear(STATUS_ARMED);
-
-				// kill the motors completely
-				for (uint8_t i = 0; i < 4; i++) {
-					motor[i] = 0;
-				}
-			}
-			
-			pid_reset();		// since pid isn't used for motor commands, take this opportunity to clear any accumulated error
-			esc_set(motor);
-			throttle = 0;
 		}
 		else {
+			status_clear(STATUS_ARMED);
+		
+			//Reset PID if not armed
 			pid_reset();
 		}
 
@@ -157,10 +121,6 @@ int main(){
 		heartbeat_overflow++;
 		if (heartbeat_overflow >= HEARTBEAT_OVERFLOW){
 			heartbeat_overflow = 0;
-			//Watchdog timer; at heartbeat overflow = HEARTBEAT_OVERFLOW (16), we run this loop at just under 4.5Hz, 
-			// so three seconds overflow will be t = WATCHDOG_ALERT = 4.5 * 3 = 13, which we check for 
-			// in the comm timeout code.
-			t++;
 			
 			status_toggle(STATUS_HEARTBEAT);
 			
@@ -179,3 +139,23 @@ int main(){
 	}
 }
 
+ISR(WDT_vect) { 
+	status_clear(STATUS_ARMED);
+	
+	// level out 
+	sp.x = 0.0;
+	sp.y = 0.0;
+	sp.z = 0.0;
+	
+	//This should go from full throttle to off in about 10 seconds.
+	if (throttle > 0.0) {
+		throttle = throttle - 0.1;
+	}
+
+	//Verify that it is not negative
+	if (throttle <= 0.0) {
+		throttle = 0.0;
+	}
+
+	wdt_reset();
+} 
