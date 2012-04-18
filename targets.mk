97cbf43e4c2f89a5871d718403841a522fa5d921
Wyatt <wyatt@digitalcave.ca>
2012-04-18 17:49:15 -0600

    re-added programmer port

--- targets.mk
+++ targets.mk
@@ -21,7 +21,7 @@ $(PROJECT).out: $(SOURCES)
 
 
 program: $(PROJECT).hex
-	$(AVRDUDE) -V -F -p $(MMCU) \
+	$(AVRDUDE) -V -F -p $(MMCU) -P $(AVRDUDE_PORT) \
 		-c $(AVRDUDE_PROGRAMMER) -b $(AVRDUDE_UPLOAD_RATE)  \
 		-U flash:w:$(PROJECT).hex 
 		
