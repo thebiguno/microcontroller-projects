85fa94f2b9f23170716431a5a682527735320b0b
Warren Janssens <warren.janssens@gmail.com>
2012-04-23 14:07:53 -0600

    adding documentation

--- shiftcpp/shift.h
+++ shiftcpp/shift.h
@@ -3,6 +3,44 @@
 
 #include <avr/io.h>
 
+/*
+ * Shift register library - this is designed for 74595 and compatible shift registers.
+ * You need a minimum of two wires to control a shift register, or three wires to control a shift register with latch.
+ *
+ * 74HC595 pinout:
+ * Pin 1 - 7: Outputs B - H
+ * Pin 8: GND
+ * Pin 9: Output H' (chain this to pin 14 if you are linking multiple registers)
+ * Pin 10: /Clear (connect to VCC, or the port/pin defined in setClear)
+ * Pin 11: Clock (connect to SPI SCK, B5 on 168 series)
+ * Pin 12: Latch (connect to port/pin as defined in setLatch, typically SPI SS, B2 on 168 series)
+ * Pin 13: /Output Enable (Connect to GND, or the port/pin defined in setEnable)
+ * Pin 14: Data (connect to SPI MOSI, B3 on 168 series or previous shift register if chaining)
+ * Pin 15: Output A
+ * Pin 16: VCC
+ *
+ * STPIC6D595 pinout:
+ * Pin 1: VCC
+ * Pin 2: Data (connect to SPI MOSI, B3 on 168 series or previous shift register if chaining)
+ * Pin 3 - 6: Drain 0 - 3
+ * Pin 7: /Clear (connect to VCC, or the port/pin defined in setClear)
+ * Pin 8: /Output Enable (Connect to GND, or the port/pin defined in setEnable)
+ * Pin 9: Serial Output (chain this to pin 14 if you are linking multiple registers)
+ * Pin 10: Latch (connect to port/pin defined in setLatch, typically SPI SS, B2 on 168 series)
+ * Pin 11 - 14: Drain 4 - 7
+ * Pin 15: Clock (connect to SPI SCK, B5 on 168 series)
+ * Pin 16: GND
+ *
+ * 74HC164 pinout:
+ * Pin 1: Data (connect to SPI MOSI, B3 on 168 series or previous shift register if chaining)
+ * Pin 2: Connect to Pin 1 or VCC
+ * Pin 3 - 6: Output A - D
+ * Pin 7: GND
+ * Pin 8: Clock (connect to SPI SCK, B5 on 168 series)
+ * Pin 9: /Clear (connect to VCC, or the port/pin defined in setClear)
+ * Pin 10 - 13: Output E - H
+ * Pin 14: VCC
+ */
 class Shift {
 
 	private:
