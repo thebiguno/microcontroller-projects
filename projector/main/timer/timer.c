ce5cf56e14cf1892b9a07b0cb55f3a077406295b
Warren Janssens <warren.janssens@gmail.com>
2012-05-06 20:53:12 -0600

    removing unused compareb

--- projector/main/timer/timer.c
+++ projector/main/timer/timer.c
@@ -1,6 +1,5 @@
 #include "timer.h"
 #include "../button/button.h"
-#include "../shift/shift.h"
 
 #define COMPA (F_CPU / 256 / 1000)
 
@@ -8,12 +7,12 @@ static volatile uint32_t _timer_millis;
 
 void timer_init(){
 	//Set up the timer to run at F_CPU / 256, in normal mode (TCNT0 is reset in the ISR)
-	TCCR0A = 0x0;			// output a = normal mode; output b = normal mode; waveform generation = normal
+	TCCR0A = 0x00;			// output a = normal mode; output b = normal mode; waveform generation = normal
 	TCCR0B |= _BV(CS02);	// clock select = CLK / 256 (prescaler)
 	
 	OCR0A = COMPA;			// interrupt A = every millisecond
 	
-	TIMSK0 = _BV(OCIE0A) | _BV(OCIE0B);
+	TIMSK0 = _BV(OCIE0A);
 
 #ifndef NO_INTERRUPT_ENABLE
 	sei();
