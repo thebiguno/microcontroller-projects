7e4189b2562b93a49b09ffa5c8132e16e154103b
Warren Janssens <warren.janssens@gmail.com>
2012-05-06 20:51:30 -0600

    fixing init signature

--- led_clock/main/shift/shift.h
+++ led_clock/main/shift/shift.h
@@ -8,10 +8,7 @@
  * You need to pass the ports by reference - e.g. shift_init(&PORTB, PORTB3, &PORTB, PORTB2, &PORTB, PORTB5).
  * This library uses SPI so the data pin must be MOSI and the clock pin must SCK.
  */
-void shift_init(
-		volatile uint8_t *data_port, uint8_t data_pin, 
-		volatile uint8_t *clock_port, uint8_t clock_pin, 
-		volatile uint8_t *latch_port, uint8_t latch_pin);
+void shift_init(volatile uint8_t *latch_port, uint8_t latch_pin);
 
 void shift_data(uint8_t i, uint8_t t, uint8_t data);
 
