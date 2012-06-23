bbf604e2075ce5c59946154b9d7848fb0ef13d04
Wyatt <wyatt@digitalcave.ca>
2012-06-23 16:37:45 -0600

    Updated sample to use converted Arduino FM tuner library

--- atmega168/fm_tuner/main.c
+++ atmega168/fm_tuner/main.c
@@ -1,19 +1,25 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "lib/fm/fm.h"
+#include "lib/si4703/si4703.h"
+#include "lib/serial/serial.h"
 
 using namespace digitalcave;
 
 int main (void){
 	//Do setup here
-	FM fm(&PORTD, PORTD2);
+	serial_init_b(9600);
+	Si4703_Breakout fm(&PORTD, PORTD2);
 
+	char temp[10];
 	fm.setVolume(2);
 	fm.setChannel(977);
 	
 	//Main program loop
 	while (1){
-		_delay_ms(1000);
-		fm.seek(1);
+		_delay_ms(5000);
+//		fm.setChannel(977);
+		uint16_t chan = fm.seekUp();
+		serial_write_s(itoa(chan, temp, 10));
+		serial_write_s("\r\n");
 	}
 }
