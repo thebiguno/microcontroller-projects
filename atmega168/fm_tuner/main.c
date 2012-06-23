a902d21fc11b61aee22887f8f9e49ac44ee33b24
Wyatt Olson <olsonw@richer.ca>
2012-06-23 13:13:21 -0600

    Added fm tuner sample

---
+++ atmega168/fm_tuner/main.c
@@ -0,0 +1,15 @@
+#include <avr/io.h>
+#include "lib/fm/fm.h"
+
+using namespace digitalcave;
+
+int main (void){
+	//Do setup here
+	FM(&PORTD, PORTD2);
+	
+	//Main program loop
+	while (1){
+		
+	}
+}
+
