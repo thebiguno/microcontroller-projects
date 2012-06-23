aeb6555df95fd7595208358062ffb4519d6e5e4c
Wyatt <wyatt@digitalcave.ca>
2012-06-23 14:51:24 -0600

    WIP fm tuner

--- atmega168/fm_tuner/main.c
+++ atmega168/fm_tuner/main.c
@@ -1,15 +1,19 @@
 #include <avr/io.h>
+#include <util/delay.h>
 #include "lib/fm/fm.h"
 
 using namespace digitalcave;
 
 int main (void){
 	//Do setup here
-	FM(&PORTD, PORTD2);
+	FM fm(&PORTD, PORTD2);
+
+	fm.setVolume(2);
+	fm.setChannel(977);
 	
 	//Main program loop
 	while (1){
-		
+		_delay_ms(1000);
+		fm.seek(1);
 	}
 }
-
