1dd5e172d516d2a00f1abd8c29c26827a019c4b2
Wyatt <wyatt@digitalcave.ca>
2012-09-21 17:18:56 -0600

    Matrix stub

--- matrix/main.c
+++ matrix/main.c
@@ -5,17 +5,18 @@
 #include <avr/interrupt.h>
 #include "lib/serial/serial.h"
 
-static Shift shift(1);
-uint8_t data[1];
+static Shift shift(2);
+uint8_t data[2];
 
 int main (void){
-	data[0] = 0x01;
+	data[0] = 0xFF;
+	uint8_t row = 0;
 	serial_init_b(57600);
 	shift.setLatch(&PORTB, PORTB2);
 	while (1) {
 		serial_write_s("loop\n\r");
-		data[0] *= 2;
-		if (data[0] == 0) data[0] = 0x01;
+		if (row > 7) row = 0;
+		data[1] = _BV(row);
 		if (shift.cts()) {
 			shift.shift(data);
 			serial_write_s("shift\n\r");
