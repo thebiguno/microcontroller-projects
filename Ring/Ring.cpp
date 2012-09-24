fb8abde2fc16532210552cd92c73db6dde8fe317
Wyatt Olson <olsonw@richer.ca>
2012-09-24 12:10:19 -0600

    Deleting C version of ring buffer, and replacing it with C++ version.

--- Ring/Ring.cpp
+++ Ring/Ring.cpp
@@ -1,4 +1,4 @@
-#include "ring.h"
+#include "Ring.h"
 
 Ring::Ring(uint8_t size) {
 	this->buffer = (uint8_t*) malloc(size);
@@ -7,21 +7,21 @@ Ring::Ring(uint8_t size) {
 	this->tail = 0x00;
 }
 
-inline uint8_t Ring::isEmpty(){
+uint8_t Ring::isEmpty(){
 	return (this->head == this->tail);
 }
 
-inline uint8_t Ring::isFull(){
+uint8_t Ring::isFull(){
 	return ((this->head + 1) % this->size == this->tail);
 }
 
-inline uint8_t Ring::get(){
+uint8_t Ring::get(){
 	char c = this->buffer[this->tail];
 	if (++this->tail >= this->size) this->tail = 0;
 	return c;
 }
 
-inline void Ring::put(uint8_t data){
+void Ring::put(uint8_t data){
 	this->buffer[this->head] = data;
 	if (++this->head >= this->size) this->head = 0;
 }
