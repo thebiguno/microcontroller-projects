9063bfa4b09357e4d6684d5f8d8a437ccde97ac4
Wyatt <wyatt@digitalcave.ca>
2012-09-30 23:05:06 -0600

    Working test sample of matrix display

--- matrix/main.c
+++ matrix/main.c
@@ -1,34 +1,39 @@
-#include "lib/shiftcpp/shift.h"
+#include "lib/draw/draw.h"
+#include "lib/draw/matrix/matrix.h"
+#include "lib/serial/serial.h"
+#include "lib/timer/timer.h"
 #include <util/delay.h>
 #include <avr/io.h>
-#include <stdlib.h>
-#include <avr/interrupt.h>
-#include "lib/serial/serial.h"
-
-static Shift shift(13);
-uint8_t data[13];
+#include "lib/draw/fonts/small.h"
+#include "lib/draw/fonts/ascii.h"
 
 int main (void){
-	data[0] = 0xFF;
-	data[1] = 0xFF;
-	data[2] = 0xFF;
-	data[3] = 0xFF;
-	data[4] = 0xFF;
-	data[5] = 0xFF;
-	data[6] = 0xFF;
-	data[7] = 0xFF;
-	data[8] = 0xFF;
-	data[9] = 0xFF;
-	data[10] = 0xFF;
-	data[11] = 0xFF;
+	matrix_init();
+	serial_init_b(9600);
+	timer_init();
 
-	uint8_t row = 0;
-	shift.setLatch(&PORTB, PORTB2);
+	//draw_text(0, 1, (char*) "Wyatt", FONT_XSMALL_WIDTH, FONT_XSMALL_HEIGHT, ORIENTATION_NORMAL, font_xsmall, codepage_ascii_caps, 0x03, OVERLAY_OR);
+	//draw_text(0, 9, (char*) "Monica", FONT_XSMALL_WIDTH, FONT_XSMALL_HEIGHT, ORIENTATION_NORMAL, font_xsmall, codepage_ascii_caps, 0x03, OVERLAY_OR);
+	
+	/*
+	draw_line(0, 0, 23, 15, 0x3, OVERLAY_OR);
+	draw_line(0, 15, 23, 0, 0x3, OVERLAY_OR);
+	
+	set_pixel(23, 8, 0x3, OVERLAY_OR);
+	set_pixel(22, 9, 0x3, OVERLAY_OR);
+	set_pixel(21, 10, 0x3, OVERLAY_OR);
+	set_pixel(20, 11, 0x3, OVERLAY_OR);
+	set_pixel(19, 12, 0x3, OVERLAY_OR);
+	set_pixel(18, 13, 0x3, OVERLAY_OR);
+	set_pixel(17, 14, 0x3, OVERLAY_OR);
+	set_pixel(16, 15, 0x3, OVERLAY_OR);
+	*/
+	
+	char temp[6];
+
 	while (1) {
-		if (shift.cts()) {
-			data[12] = ~_BV(row++);
-			if (row > 7) row = 0;
-			shift.shift(data);
-		}
+		draw_rectangle(0, 1, 23, 7, DRAW_FILLED, 0xFF, OVERLAY_NAND);
+		draw_text(0, 1, itoa((uint8_t) (timer_millis() / 1000), temp, 10), FONT_SMALL_WIDTH, FONT_SMALL_HEIGHT, ORIENTATION_NORMAL, font_small, codepage_ascii_caps, 0x03, OVERLAY_OR);
+		_delay_ms(100);
 	}
 }
