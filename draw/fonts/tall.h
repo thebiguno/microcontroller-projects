3e509b3ea1a0212919c67d513cd265485de02f78
Wyatt Olson <olsonw@richer.ca>
2012-10-01 16:25:59 -0600

    Added tall font (5x13)

---
+++ draw/fonts/tall.h
@@ -0,0 +1,11 @@
+#ifndef FONT_XLARGE_H
+#define FONT_XLARGE_H
+
+extern prog_uchar codepage_tall[];
+
+extern prog_uchar font_tall[];
+
+#define FONT_TALL_WIDTH	5
+#define FONT_TALL_HEIGHT	13
+
+#endif
