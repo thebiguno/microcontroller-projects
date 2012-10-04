b003b0a27f1bfbd517c143f2bb9c392c26486918
Wyatt Olson <olsonw@richer.ca>
2012-10-04 13:15:25 -0600

    More WIP.

--- matrix_cpu/main.c
+++ matrix_cpu/main.c
@@ -1,21 +1,95 @@
-#include "lib/draw/matrix/matrix.h"
 #include "lib/draw/draw.h"
+#include "lib/analog/analog.h"
+#include "lib/draw/matrix/matrix.h"
+#include "lib/serial/serial.h"
+#include "lib/timer/timer.h"
+#include "lib/draw/fonts/tall.h"
 #include "lib/Shift/Shift.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+uint8_t get_neighbor_count(uint8_t x, uint8_t y){
+	uint8_t count = 0;
+	if (matrix_get_display_pixel(x - 1, y - 1)) count++;
+	if (matrix_get_display_pixel(x - 1, y)) count++;
+	if (matrix_get_display_pixel(x - 1, y + 1)) count++;
+	if (matrix_get_display_pixel(x, y - 1)) count++;
+	if (matrix_get_display_pixel(x, y + 1)) count++;
+	if (matrix_get_display_pixel(x + 1, y - 1)) count++;
+	if (matrix_get_display_pixel(x + 1, y)) count++;
+	if (matrix_get_display_pixel(x + 1, y + 1)) count++;
+	return count;
+}
+
+void setup(){
+	srandom(analog_read_p(0) * analog_read_p(1) * analog_read_p(2) * analog_read_p(3) * analog_read_p(4) * analog_read_p(5));
+
+	for (uint8_t x = 0; x < MATRIX_WIDTH; x++){
+		for (uint8_t y = 0; y < MATRIX_HEIGHT; y++){
+			if ((random() & 0x3) == 0x3){		//25% chance
+				set_pixel(x, y, RED_3, OVERLAY_OR);
+			}
+		}
+	}
+	matrix_flush();
+}
 
 int main (void){
+	uint8_t analog_pins[6] = {0, 1, 2, 3, 4, 5};
 	Shift shift(&PORTB, PORTB0, &PORTB, PORTB1);
-	matrix_init();
-	
-	uint8_t incoming = 0x00;
+
+	uint8_t value[5] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 	while (1) {
-		//Loop until we get the sync frame
-		while(incoming != 0x42) shift.receive(&incoming, 1);
-		incoming = 0x00;
-		
-		//Read the data directly into the working buffer
+		shift.send(value, 5);
+	}
+
+	/*
+	serial_init_b(9600);
+	timer_init();
+	analog_init(analog_pins, 6, ANALOG_INTERNAL);
+
+	setup();
+	
+	uint8_t sync_frame = 0x42;
+
+	while (1) {
+		for (uint8_t x = 0; x < MATRIX_WIDTH; x++){
+			for (uint8_t y = 0; y < MATRIX_HEIGHT; y++){
+				uint8_t count = get_neighbor_count(x, y);
+				uint8_t alive = matrix_get_display_pixel(x, y);
+				if (alive) {
+					if (count < 2) set_pixel(x, y, 0xF, OVERLAY_NAND);
+					else if (count <= 3){
+						if (alive == GRN_3) {
+							set_pixel(x, y, 0xF, OVERLAY_NAND);
+							set_pixel(x, y, RED_3 | GRN_3, OVERLAY_OR);
+						}
+						else if (alive == (GRN_3 | RED_3)) {
+							set_pixel(x, y, 0xF, OVERLAY_NAND);
+							set_pixel(x, y, RED_3, OVERLAY_OR);
+						}
+					}
+					else if (count > 3) set_pixel(x, y, 0xF, OVERLAY_NAND);
+				}
+				else if (count == 3){
+					set_pixel(x, y, GRN_3, OVERLAY_OR);
+				}
+			}
+		}
+
+		shift.send(&sync_frame, 1);
 		shift.receive(matrix_get_working_buffer(), MATRIX_WIDTH * (MATRIX_HEIGHT >> 1));
 		
-		matrix_flush();
+		_delay_ms(10);
+		
+		if (timer_millis() > 10000){
+			timer_init();
+			setup();
+		}
 	}
+	*/
 }
