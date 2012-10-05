a739fc7f04822f5dfd34448ff5837e9193188bcc
Wyatt <wyatt@digitalcave.ca>
2012-10-04 19:49:55 -0600

    More work on CPU / GPU mode.  Serial communication is working, but sync frames are not (we need to sync the beginning of the transmission as well, or have a timeout or something…)

--- matrix_cpu/main.c
+++ matrix_cpu/main.c
@@ -11,85 +11,22 @@
 #include <math.h>
 
 
-uint8_t get_neighbor_count(uint8_t x, uint8_t y){
-	uint8_t count = 0;
-	if (matrix_get_display_pixel(x - 1, y - 1)) count++;
-	if (matrix_get_display_pixel(x - 1, y)) count++;
-	if (matrix_get_display_pixel(x - 1, y + 1)) count++;
-	if (matrix_get_display_pixel(x, y - 1)) count++;
-	if (matrix_get_display_pixel(x, y + 1)) count++;
-	if (matrix_get_display_pixel(x + 1, y - 1)) count++;
-	if (matrix_get_display_pixel(x + 1, y)) count++;
-	if (matrix_get_display_pixel(x + 1, y + 1)) count++;
-	return count;
-}
-
-void setup(){
-	srandom(analog_read_p(0) * analog_read_p(1) * analog_read_p(2) * analog_read_p(3) * analog_read_p(4) * analog_read_p(5));
-
-	for (uint8_t x = 0; x < MATRIX_WIDTH; x++){
-		for (uint8_t y = 0; y < MATRIX_HEIGHT; y++){
-			if ((random() & 0x3) == 0x3){		//25% chance
-				set_pixel(x, y, RED_3, OVERLAY_OR);
-			}
-		}
-	}
-	matrix_flush();
-}
-
 int main (void){
 	uint8_t analog_pins[6] = {0, 1, 2, 3, 4, 5};
 	Shift shift(&PORTB, PORTB0, &PORTB, PORTB1);
 
-	uint8_t value[5] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
-	while (1) {
-		shift.send(value, 5);
-	}
-
-	/*
-	serial_init_b(9600);
 	timer_init();
 	analog_init(analog_pins, 6, ANALOG_INTERNAL);
+	draw_rectangle(0, 0, MATRIX_WIDTH - 1, MATRIX_HEIGHT - 1, DRAW_FILLED, RED_3, OVERLAY_OR);
 
-	setup();
-	
 	uint8_t sync_frame = 0x42;
 
-	while (1) {
-		for (uint8_t x = 0; x < MATRIX_WIDTH; x++){
-			for (uint8_t y = 0; y < MATRIX_HEIGHT; y++){
-				uint8_t count = get_neighbor_count(x, y);
-				uint8_t alive = matrix_get_display_pixel(x, y);
-				if (alive) {
-					if (count < 2) set_pixel(x, y, 0xF, OVERLAY_NAND);
-					else if (count <= 3){
-						if (alive == GRN_3) {
-							set_pixel(x, y, 0xF, OVERLAY_NAND);
-							set_pixel(x, y, RED_3 | GRN_3, OVERLAY_OR);
-						}
-						else if (alive == (GRN_3 | RED_3)) {
-							set_pixel(x, y, 0xF, OVERLAY_NAND);
-							set_pixel(x, y, RED_3, OVERLAY_OR);
-						}
-					}
-					else if (count > 3) set_pixel(x, y, 0xF, OVERLAY_NAND);
-				}
-				else if (count == 3){
-					set_pixel(x, y, GRN_3, OVERLAY_OR);
-				}
-			}
-		}
+	_delay_ms(50);
 
-		shift.send(&sync_frame, 1);
-		shift.receive(matrix_get_working_buffer(), MATRIX_WIDTH * (MATRIX_HEIGHT >> 1));
+	while (1) {
+//		shift.send(&sync_frame, 1);
+		shift.send(matrix_get_working_buffer(), MATRIX_WIDTH * (MATRIX_HEIGHT >> 1));
 		
 		_delay_ms(10);
-		
-		if (timer_millis() > 10000){
-			timer_init();
-			setup();
-		}
 	}
-	*/
 }
