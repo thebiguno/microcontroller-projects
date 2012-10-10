ba0072a8d59839237aae94654fdaca3678c50091
Wyatt Olson <olsonw@richer.ca>
2012-10-10 15:19:46 -0600

    WIP for TwoWire test

--- atmega168/twowire/main.c
+++ atmega168/twowire/main.c
@@ -1,23 +1,54 @@
 /*
- * Test program for i2c.  This particular test is made for the MMA7660FC accelerometer, although it 
- * can be easily modified for any other device by changing the address and the packet sequence.
+ * Test program for TwoWire library.
  */
 
 #include <stdlib.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "lib/TwoWire/TwoWire.h"
+#include "lib/serial/serial.h"
+
+#define MASTER
+
+#ifdef SLAVE
+void receiveEvent(int howMany){
+	uint8_t data = 0;
+	while (Wire.available() > 0){
+		data = Wire.receive();
+		serial_write_c(data);
+	}
+	serial_write_s("\n\r");
+}
 
 int main (void){
 	//Do setup here
+	serial_init_b(9600);
 	Wire.begin(42);
+	Wire.onReceive(receiveEvent);
+	DDRB |= _BV(PORTB0);
 	
-	uint8_t data;
-
 	//Main program loop
 	while (1){
-		while (Wire.available() > 0){
-			data = Wire.receive();
-		}
+		PORTB = ~PORTB;
+		_delay_ms(1000);
 	}
 }
+#endif
+#ifdef MASTER
+int main (void){
+	//Do setup here
+	Wire.begin();
+	DDRB |= _BV(PORTB0);
+	sei();
+	
+	//Main program loop
+	while (1){
+		Wire.beginTransmission(42);
+		Wire.send('a');
+		Wire.endTransmission();
+		PORTB = 0xFF;
+		_delay_ms(1000);
+	}
+}
+#endif
