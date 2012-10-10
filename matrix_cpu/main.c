ddbe862678eef5854dd27c76479c6c0895416dab
Wyatt <wyatt@digitalcave.ca>
2012-10-10 10:38:56 -0600

    Updated matrix and matrix_cpu to use split matrix driver library.  Not quite working yet.

--- matrix_cpu/main.c
+++ matrix_cpu/main.c
@@ -10,10 +10,12 @@
 
 int main (void){
 	draw_rectangle(0, 0, MATRIX_WIDTH - 1, MATRIX_HEIGHT - 1, DRAW_FILLED, RED_3, OVERLAY_OR);
+
+	_delay_ms(1000);
+
 	Wire.begin();
 	
 	while (1) {
-//		shift.send(&sync_frame, 1);
 		Wire.beginTransmission(42);
 		Wire.send(matrix_get_working_buffer(), MATRIX_WIDTH * (MATRIX_HEIGHT >> 1));
 		Wire.endTransmission();
