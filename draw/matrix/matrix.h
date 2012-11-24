47583e3ea94d2c266063dacd4e6ddb7c744eb0ab
Wyatt <wyatt@digitalcave.ca>
2012-11-23 21:04:26 -0700

    Removed defines and functions which are no longer needed

--- draw/matrix/matrix.h
+++ draw/matrix/matrix.h
@@ -8,13 +8,6 @@
 #define MATRIX_HEIGHT 16
 #endif
 
-#define GRN_3	0xF0
-#define GRN_2	0x80
-#define GRN_1	0x40
-#define RED_3	0x0F
-#define RED_2	0x08
-#define RED_1	0x04
-
 #include <avr/io.h>
 #include "../draw.h"
 #include "../../ShiftRegister/ShiftRegister.h"
@@ -32,29 +25,9 @@
 void matrix_init();
 
 /*
- * Flushes the working buffer to the display buffer.  Call this after writing using the draw API and set_pixel.
- */
-void matrix_flush(uint8_t* working_buffer, uint8_t* display_buffer);
-
-/*
- * Identical to get_pixel, except it returns the pixel from the display buffer, rather than the working buffer.
- */
-uint8_t matrix_get_display_pixel(uint8_t x, uint8_t y);
-
-/*
- * Identical to set_pixel, except it sets the pixel in the display buffer, rather than the working buffer.
- */
-void matrix_set_display_pixel(uint8_t x, uint8_t y, uint8_t value, uint8_t overlay);
-
-/*
  * Raw access to the working buffer.  Use at your own risk.
  */
 uint8_t* matrix_get_working_buffer();
-
-/*
- * Raw access to the display buffer.  Use at your own risk.
- */
-uint8_t* matrix_get_display_buffer();
 
 /*
  * Finished API methods
