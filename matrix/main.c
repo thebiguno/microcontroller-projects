b196c1a6e373f1254350dd5bbf3b4e320989ef08
Wyatt <wyatt@digitalcave.ca>
2012-11-23 21:05:28 -0700

    Got 4 bit colour working on matrix board

--- matrix/main.c
+++ matrix/main.c
@@ -10,7 +10,7 @@
 static uint8_t _buffer[MATRIX_WIDTH][MATRIX_HEIGHT];
 
 //ShiftRegister object
-static ShiftRegister shift(13);			//TODO change the size to be dynamically calculated based on width and height values
+static ShiftRegister shift(((MATRIX_WIDTH * MATRIX_HEIGHT) >> 5) + 1);
 
 //We assume the *data is a pointer to the data we need to fill; thus data[0] is the LSB 
 // shift register, and data[1] is the MSB.
@@ -23,33 +23,25 @@ static void _fill_data(uint8_t* data, uint8_t x, uint8_t y, uint8_t dc){
 	uint8_t* b = _buffer[x] + y;
 	uint8_t gdc = dc << 4;	//dc for green (shift dc once, rather than green many times)
 	
-	//By checking for non-zero values in b[x], we can potentially reduce the time for a large number of 
-	// updates for blank / sparse rows.
-//	if (b[0]){
-		if (b[1] > gdc)				data[1] |= 0x01;
-		if ((b[1] & 0x0F) > dc)		data[1] |= 0x02;
-		if (b[0] > gdc)				data[1] |= 0x40;
-		if ((b[0] & 0x0F) > dc)		data[1] |= 0x80;
-//	}
-//	if (b[4]){
-		if ((b[5] & 0x0F) > dc)		data[1] |= 0x04;
-		if (b[5] > gdc)				data[1] |= 0x08;
-		if ((b[4] & 0x0F) > dc)		data[1] |= 0x10;
-		if (b[4] > gdc)				data[1] |= 0x20;
-//	}
+	if (b[1] > gdc)				data[1] |= 0x01;
+	if ((b[1] & 0x0F) > dc)		data[1] |= 0x02;
+	if (b[0] > gdc)				data[1] |= 0x40;
+	if ((b[0] & 0x0F) > dc)		data[1] |= 0x80;
 
-//	if (b[2]){
-		if (b[3] > gdc)				data[0] |= 0x01;
-		if ((b[3] & 0x0F) > dc)		data[0] |= 0x02;
-		if (b[2] > gdc)				data[0] |= 0x40;
-		if ((b[2] & 0x0F) > dc)		data[0] |= 0x80;
-//	}
-//	if (b[6]){
-		if ((b[7] & 0x0F) > dc)		data[0] |= 0x04;
-		if (b[7] > gdc)				data[0] |= 0x08;
-		if ((b[6] & 0x0F) > dc)		data[0] |= 0x10;
-		if (b[6] > gdc)				data[0] |= 0x20;
-//	}
+	if ((b[5] & 0x0F) > dc)		data[1] |= 0x04;
+	if (b[5] > gdc)				data[1] |= 0x08;
+	if ((b[4] & 0x0F) > dc)		data[1] |= 0x10;
+	if (b[4] > gdc)				data[1] |= 0x20;
+
+	if (b[3] > gdc)				data[0] |= 0x01;
+	if ((b[3] & 0x0F) > dc)		data[0] |= 0x02;
+	if (b[2] > gdc)				data[0] |= 0x40;
+	if ((b[2] & 0x0F) > dc)		data[0] |= 0x80;
+
+	if ((b[7] & 0x0F) > dc)		data[0] |= 0x04;
+	if (b[7] > gdc)				data[0] |= 0x08;
+	if ((b[6] & 0x0F) > dc)		data[0] |= 0x10;
+	if (b[6] > gdc)				data[0] |= 0x20;
 }
 
 static void _callback(){
@@ -74,7 +66,7 @@ static void _callback(){
 	if (row > 7){
 		row = 0;
 		dc++;
-		if (dc > 8) dc = 0;
+		if (dc > 15) dc = 0;
 	}
 	
 	
@@ -97,12 +89,7 @@ int main (void){
 	shift.setCallback(_callback);
 	
 	_callback();	//Start shifting
-	
-	_buffer[0][0] = 0xFF;
-	_buffer[23][0] = 0xFF;
-	_buffer[0][15] = 0xFF;
-	_buffer[23][15] = 0xFF;
-	
+
 	while (1) {
 		//Nothing to see here... all the work is done in ISRs
 	}
