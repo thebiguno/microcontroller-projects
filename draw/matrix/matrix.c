adb36d43993b80d1020fd08f4c0d07640a0b5a63
Wyatt Olson <olsonw@richer.ca>
2012-11-27 19:38:21 -0700

    The concept of the matrix library is starting to solidify.  It now looks like this library will be the device driver for the DigitalCave LED matrix board, accessed over TWI.  This driver takes care of creating the display buffer, writing to the buffer, and flushing the buffer to the display board over TWI.  It does not drive the LED board itself; that is handled by a separate project.

--- draw/matrix/matrix.c
+++ draw/matrix/matrix.c
@@ -9,30 +9,53 @@
 
 //Data is copies to the working buffer first, and then flushed to _buffer for display by the user.  This
 // prevents flickering when doing animations (and in fact any live drawing).
-static uint8_t _working_buffer[MATRIX_WIDTH][MATRIX_HEIGHT];
+static uint8_t buffer[MATRIX_WIDTH][MATRIX_HEIGHT];
+static uint8_t mode = 0x00;
+
+static uint8_t twi_master_tx_writer(uint16_t i){
+	if (i == 0){
+		return mode;
+	}
+	else {
+		return ((uint8_t*) buffer)[i - 1];
+	}
+}
+
+void matrix_init(){
+	twi_init();
+	twi_atatch_master_tx_writer(twi_master_tx_writer);
+}
 
 void set_pixel(uint8_t x, uint8_t y, uint8_t value, uint8_t overlay){
 	if (x >= MATRIX_WIDTH || y >= MATRIX_HEIGHT) return;	//Bounds check
 
 	if (overlay == OVERLAY_REPLACE){
-		_working_buffer[x][y] = value;
+		buffer[x][y] = value;
 	}
 	else if (overlay == OVERLAY_OR){
-		_working_buffer[x][y] |= value;
+		buffer[x][y] |= value;
 	}
 	else if (overlay == OVERLAY_NAND){
-		_working_buffer[x][y] &= ~value;
+		buffer[x][y] &= ~value;
 	}
 	else if (overlay == OVERLAY_XOR){
-		_working_buffer[x][y] ^= value;
+		buffer[x][y] ^= value;
 	}
+}
+
+void matrix_write_buffer(){
+	twi_write_to(MATRIX_DRIVER_SLAVE_ADDRESS, (uint8_t*) buffer, MATRIX_LENGTH + 1, TWI_BLOCK, TWI_STOP);
+}
+
+void matrix_set_mode(uint8_t new_mode){
+	mode = new_mode;
 }
 
 uint8_t get_pixel(uint8_t x, uint8_t y){
 	if (x >= MATRIX_WIDTH || y >= MATRIX_HEIGHT) return 0;	//Bounds check
-	return _working_buffer[x][y];
+	return buffer[x][y];
 }
 
-uint8_t* matrix_get_working_buffer(){
-	return (uint8_t*) _working_buffer;
+uint8_t* matrix_get_buffer(){
+	return (uint8_t*) buffer;
 }
--- draw/matrix/matrix.h
+++ draw/matrix/matrix.h
@@ -8,9 +8,12 @@
 #define MATRIX_HEIGHT 16
 #endif
 
-#include <avr/io.h>
+#ifndef MATRIX_DRIVER_SLAVE_ADDRESS
+#define MATRIX_DRIVER_SLAVE_ADDRESS 42
+#endif
+
 #include "../draw.h"
-#include "../../ShiftRegister/ShiftRegister.h"
+#include "../../twi/twi.h"
 
 /*************
  * IMPORTANT *
@@ -19,15 +22,28 @@
  * You must include a driver, which includes the following API functions.
  */
 
- /*
- * Initialize the matrix device driver
+/*
+ * Initializes the code to write to the matrix driver; specifically, this sets up the TWI hardware.  If you are using 
+ * the TWI library elsewhere you can skip this step.
  */
 void matrix_init();
+
+ /*
+ * Writes the buffer to the LED matrix, using the TWI library.
+ */
+void matrix_write_buffer();
+
+/*
+ * Sets the mode of the matrix driver.  Default is 0x00 (raw buffer copy).  Other modes include
+ * 4 bit gradient, 4 bit high contrast, 2 bit modes of various color combinations, and 1 bit modes
+ * of various combinations.  See matrix driver code for details.
+ */
+void matrix_set_mode(uint8_t new_mode);
 
 /*
  * Raw access to the working buffer.  Use at your own risk.
  */
-uint8_t* matrix_get_working_buffer();
+uint8_t* matrix_get_buffer();
 
 /*
  * Finished API methods
