d6306d98822baed9c13c5c6b598c5f0df8c37ffb
Wyatt Olson <olsonw@richer.ca>
2013-04-26 10:41:57 -0600

    (Hopefully) fixed some timing bugs.  Added logic for HH pedal.  Finalized hardware layout.  Code cleanup.  Now we just need to see if the stupid thing works...

--- src/drummaster.c
+++ src/drummaster.c
@@ -7,17 +7,18 @@
  * computer via the serial port.	Each signal is sent in a packet, using a binary protocol 
  * consisting of 2 bytes / packet:
  *
- *		|cccccckk|vvvvvvvv|
- *      <channel:6><checksum:2><value:8>
+ *		|sscccckk|vvvvvvvv|
+ *      <start:2><channel:4><checksum:2><velocity:8>
  *
  * Each portion of the packet is described below:
- *		<channel> is the 6 bit representation of a channel number between 0x00 and 0x27 (39).  
- *				Analog channels are 0x00..0x1F, and digital channels are 0x20..0x27.
- *		<value> is the 8 bit representation of the actual analog value between 0x0 and 0xFF.
+ *		<start> is a 2 bit flag 0x3 to signal the start of a new packet.
+ *		<channel> is the 4 bit representation of a channel number between 0x00 and 0x0F (15).  
+ *				Analog channels are 0..11, and digital channels are 12..15.
  *		<checksum> is the 2 bit checksum on the rest of the packet, calculated using the 
  *				2 bit parity word algorithm.  Each 2-bit chunk of the packet is XOR'd together. 
  *				The slave software will XOR all 8 2 bit words together; if the result is not 0x0
  *				then we know there was an error in transmission.
+ *		<velocity> is the 8 bit representation of the actual analog value between 0x0 and 0xFF.
  *
  * Slave software, running on the computer, must take these data packets and map them to digital
  * audio samples, based on the channel, velocity, and current state of the program.
@@ -30,11 +31,29 @@
 								extremely expensive to run (64 bit arithmetic, even simple adds, can take upwards as 
 								long as the ADC conversions themselves)
 								-Eliminated reliance on triggers; use the ADC for all values now.
-								-All variables are now 8 bit
+								-(Almost) all variables are now 8 bit
 								-Velocity is now 8 bit (drop the 2 LSB of the 10 bit value).  The bottom two bits
 								were just noise anyway, so why waste space sending them?
-								-Simplified transmission protocol, reducing size to 2 bytes from 3.
-								-Possibly reducing number of inputs from 32 to 8 (or 16?)  Need to test for speed...
+								-Use ADC sleep noise reduction mode when reading inputs, to improve accuracy
+								-Simplified transmission protocol, reducing size from 3 bytes to 2.
+								-Reducing number of inputs from 32 to 12, with 4 digital pins.  This gives us a 4 bit channel.
+								-Hard coded pad assignments as follows:
+									0: Snare
+									1: Hi Hat
+									2: Hi Hat Pedal (Analog)
+									3: Bass
+									4: Tom 1
+									5: Tom 2
+									6: Tom 3
+									7: Ride
+									8: Crash 1
+									9: Crash 2
+									10: Splash 1
+									11: Splash 2
+									12: Crash 1 Mute
+									13: Crash 2 Mute
+									14: Ride Mute
+									15: Hi Hat Pedal (Digital)
  									
  * 1.2.0.2 - January 23 2011:	-For digital channels, send 0x1 for closed (button pressed) and 0x0 for button open. 
  * 								This is backwards from the logical state (since we use pullup resistors forcing 
@@ -69,30 +88,20 @@
 
 #include <avr/io.h>
 #include <stdlib.h>
+#include <util/delay.h>
 #include "../lib/analog/analog.h"
 #include "../lib/serial/serial.h"
 
 //The smallest ADC value which we will report
-#define MIN_VELOCITY 5
+#define MIN_VELOCITY 10
+
+//The smallest ADC value change on an active channel (i.e. HiHat pedal) which we will report
+#define MIN_ACTIVE_CHANGE 10
 
 //This is used for a number of data buffers.	It should be set to the number of channels (this is
 // 40 in default hardware).	 You will only want to change this if you have modified / custom hardware.
 #define BUFFER_SIZE 40
 
-
-//Is it time to read active channels?	 Set to true at the beginning of a loop if this is the case, and
-// reset at the end.
-uint8_t read_active_channels;
-
-//Active counter.	 Increments each cell by one each time a value for the corresponding channel is
-// read; if there is no value read, it will reset the cell to 0.	Once a given cell gets above
-// MIN_ACTIVE_CHANNEL_POLL_COUNT, we will poll on a less frequent basis, and will only report large
-// changes over the serial port.
-uint8_t consecutive_reads[BUFFER_SIZE];
-
-//Keep track of active channels, to reduce computations.	Once a channel is set to be active,
-// only a reset will change it back.
-uint8_t active_channel[BUFFER_SIZE];
 
 //What was the last value read from each channel?
 uint8_t last_value[BUFFER_SIZE];
@@ -100,7 +109,7 @@ uint8_t last_value[BUFFER_SIZE];
 //When we send a value, we reset this to a large positive number (the exact value depends on how
 // long you want to debounce for).  Each iteration after, we decrement.  Once we hit zero we can
 // potentially read the value again.
-uint8_t debounce_counter[BUFFER_SIZE];
+uint16_t debounce_counter[BUFFER_SIZE];
 
 /*
  * Sets pins S2 - S0 to select the multiplexer output.
@@ -113,11 +122,12 @@ void set_port(uint8_t port){
 }
 
 /*
- * Convert from a bank / port tuple to single channel number (value from 0 - 39 inclusive)
+ * Convert from a bank / port tuple to single channel number (value from 0 - 15 inclusive)
  * for sending to Drum Slave software.
  */
 uint8_t get_channel(uint8_t bank, uint8_t port){
-	return (bank << 3) | port;
+	if (bank == 0x00) return port;
+	else return port + 0x08;
 }
 
 /*
@@ -128,22 +138,21 @@ uint8_t get_velocity(uint8_t pin){
 }
 
 /*
- * Sends data.  Channel is a 6 bit number, between 0x00 and 0x27 inclusive.  Velocity is a 8 bit number.
+ * Sends data.  Channel is a 4 bit number, between 0x00 and 0x0F inclusive.  Velocity is a 8 bit number.
  */
 void send_data(uint8_t channel, uint8_t velocity){
-	uint8_t packet;
-	uint8_t checksum = 0x0;
+	uint8_t packet = 0xC0;	//Start bits
+	uint8_t checksum = 0x3;	//0x03 is the checksum of the start bits
 	
 	//First packet consists of 6 channel bits, and (eventually) 2 checksum bits.
-	packet = ((channel << 0x02) & 0xFC);
+	packet |= ((channel << 0x02) & 0x3C);
 	
 	//Add channel to checksum
-	checksum ^= channel >> 0x04;
 	checksum ^= (channel >> 0x02) & 0x03;
 	checksum ^= channel & 0x3;
 	
-	//Add velocity to packet
-	checksum ^= velocity >> 0x06;
+	//Add velocity to checksum
+	checksum ^= (velocity >> 0x06) & 0x03;
 	checksum ^= (velocity >> 0x04) & 0x03;
 	checksum ^= (velocity >> 0x02) & 0x03;
 	checksum ^= velocity & 0x03;
@@ -157,9 +166,10 @@ void send_data(uint8_t channel, uint8_t velocity){
 
 int main (void){
 	//Init libraries	
-	uint8_t apins[1];
+	uint8_t apins[2];
 	apins[0] = 0;
-	analog_init(apins, 1, ANALOG_AREF);
+	apins[1] = 1;
+	analog_init(apins, 2, ANALOG_AREF);
 	
 	serial_init_b(57600);
 
@@ -173,45 +183,47 @@ int main (void){
 	//The three MUX selector switches need to be set to output mode
 	DDRB |= _BV(DDB0) | _BV(DDB1) | _BV(DDB2);
 
-	//Initialize array counters
-	for (port = 0; port < BUFFER_SIZE; port++){
-		//Initialize the active consecutive reads counter
-		consecutive_reads[port] = 0;
-
-		//Reset the active channels to all 0
-		active_channel[port] = 0;
-	}
-
 	//Main program loop
 	while (1){
-		for (bank = 0x00; bank < 0x05; bank++){	//bank is one ADC (for 0x00..0x03) or digital (0x04) input
-			for (port = 0x00; port < 0x08; port++){	//port is one channel on a multiplexer
+		for (port = 0x00; port < 0x08; port++){	//port is one channel on a multiplexer
+			for (bank = 0x00; bank < 0x02; bank++){	//bank is one of ADC 0/1 (for channel 0..11) or digital (channel 12..15) input
 				set_port(port);
+				
+				//The MUX switches pretty much instantly, but it appears that it is helpful to give a bit of time
+				// to let the analog values settle before reading them.  TODO verify timings for this.
+				_delay_us(10);
 
-				//Read the analog pins
 				channel = get_channel(bank, port);
 		
 				if (debounce_counter[channel] > 0){
 					debounce_counter[channel]--;
 				}
 				else {
-					if (bank < 0x04){
+					if (channel == 2){
+						//Read Hi-Hat Analog Pedal channel
 						velocity = get_velocity(bank);
-						if (velocity >= MIN_VELOCITY || channel == 0x02){	//Channel 2 is Hi Hat Pedal, which is active channel.
+						if (debounce_counter[channel] == 0 || abs(last_value[channel] - velocity) > MIN_ACTIVE_CHANGE){
+							send_data(channel, last_value[channel]);
+							debounce_counter[channel] = 0xFF;	//TODO Change this based on actual measurements.
+						}
+					}
+					else if (channel < 12){
+						//Read the analog pins
+						velocity = get_velocity(bank);
+						if (velocity >= MIN_VELOCITY){
 							//We watch the values, and send as it starts to decrease
 							if (last_value[channel] > velocity){
 								send_data(channel, last_value[channel]);
 								debounce_counter[channel] = 0xFF;	//TODO Change this based on actual measurements.
+								last_value[channel] = 0;
 							}
 							else {
 								last_value[channel] = velocity;
 							}
 						}
 					}
-					else if (bank == 0x04){
+					else {
 						//Read the digital pins
-						channel = get_channel(bank, port);
-				
 						//Remember that digital switches in drum master are reversed, since they 
 						// use pull up resisitors.	Logic 1 is open, logic 0 is closed.	 When 
 						// sending the readings, we invert the value to make this easy to keep straight.
