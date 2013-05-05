cea71a9d324f5f7de82c8d7d9b3e158c35852c06
Wyatt Olson <olsonw@richer.ca>
2013-05-05 10:31:30 -0600

    Added the (enabled by default) option to use ADC noise reducing sleep mode when reading synchronous analog values.  This can be disabled by defining ADC_NO_SLEEP_MODE if there is conflicting code which would be harmed by this (potentially other interrupt driven code).  Currently this mode is enabled by default; if we find that it causes problems in many other situations, we can change this to a disabled-by-default approach.

--- analog/analog_synchronous.c
+++ analog/analog_synchronous.c
@@ -1,4 +1,6 @@
 #include "analog.h"
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
 
 //This is a 3 bit value; refer to the data sheet for what each mask means.  A 
 // reasonable default seems to be 0x4 (/16) or 0x5 (/32).  Higher prescalers
@@ -6,6 +8,20 @@
 #ifndef ADC_PRESCALER_MASK
 #define ADC_PRESCALER_MASK 0x4
 #endif
+
+//If defined, we will not use the ADC sleep mode to reduce noise, and instead
+// will busy-wait for conversion to complete.  For synchronous analog reads, 
+// ADC sleep mode makes sense for most applications, unless there are other critical
+// systems which are running in interrupts which may be interfered with by sleeping.
+//Only define this if you have a good reason to.
+//#define ADC_NO_SLEEP_MODE
+
+/*
+ * This is a synchronous implementation to read the ADC (synchronous meaning that when you 
+ * call analog_read_p() it will start a new reading and not return until the reading is
+ * completed).  Since we block on read anyway, we also use the sleep mode ADC noise cancelling
+ * option to make for a better quality signal.
+ */
 
 static uint8_t pins[8];
 static uint8_t pin_count;
@@ -15,7 +31,7 @@ void analog_init(uint8_t analog_pins[], uint8_t count, uint8_t aref){
 
 	for (int i = 0; i < count; i++){
 		//Store which pins are needed
-		pins[i] = analog_pins[i];
+		pins[i] = (analog_pins[i] & 0x0F);
 
 		//Disable digital input on selected pins.
 		//TODO Should we do this?
@@ -40,17 +56,43 @@ void analog_read_a(uint16_t *data){
 	}
 }
 
+#ifdef ADC_NO_SLEEP_MODE
 uint16_t analog_read_p(uint8_t index){
 	//Set up which pin to read
 	ADMUX = (ADMUX & 0xF0) | pins[index];
 
-	//Start conversion	
+	//Start conversion
 	ADCSRA |= _BV(ADSC);
-	
+
 	//Wait until conversion is complete
 	while (ADCSRA & _BV(ADSC));
+
+	//Return conversion result
+	return ADC;
+}
+#else
+uint16_t analog_read_p(uint8_t index){
+	//Set up which pin to read
+	ADMUX = (ADMUX & 0xF0) | pins[index];
+
+	ADCSRA |= _BV( ADIE ); 	//Set ADC Interrupt
+	set_sleep_mode(SLEEP_MODE_ADC); 
+	// Enter Sleep Mode To Trigger ADC Measurement 
+	// CPU Will Wake Up From ADC Interrupt
+	sleep_enable();
+	sei();		//Ensure interrupts are enabled, or else CPU will hang until hard reset
+	sleep_cpu();
+	sleep_disable();
+	
+	//Confirm that conversion is complete - this is generally not needed,
+	// but the AVR may wake up from another interrupt source before 
+	// conversion is done, and we want to ensure that we send the correct data.
+//	while (ADCSRA & _BV(ADSC));
 	
 	//Return conversion result
 	return ADC;
 }
 
+// ADC Interrupt Is Used To Wake Up CPU From Sleep Mode 
+EMPTY_INTERRUPT (ADC_vect)
+#endif
