a87fa9faa8cc3ee3a4a5f570ead510277fae1cfc
Wyatt <wyatt@digitalcave.ca>
2014-03-03 15:09:22 -0700

    Many changes to the main program.  Reduction of all messages to 1 byte (no message state required).  Addition of support for different commands: enable / disable single button, enable / disable sticks, disable / set polling interval, etc.  Code compiles but has not been tested on the hardware yet.

--- psx_controller/main.c
+++ psx_controller/main.c
@@ -1,44 +1,75 @@
 /*
  * Read the buttons on the PS2 Controller, and send them over the 
- * serial link (probably to an XBee).  This code is meant to constantly 
- * send the state of the buttons, even if the buttons are not pushed.
+ * serial link (probably to an XBee).  We send all button events
  * We let the receiving end decide what to do with this information.
  * Initially designed for a hexapod controller, but it should be usable 
  * for most robotics / gaming interface tasks.
  *
+ * There is an idle poll which re-sends button state on a regular basis
+ * (default of about 500ms) even if there was no change of state.  This
+ * can ensure that the receiving client does not miss an event.  The idle
+ * polling does not re-send analog data.
+ *
+ * Every message, whether outgoing or incoming, is contained within a single 
+ * byte.  This allows us to avoid keeping track of message state on both sides,
+ * and also allows us to ignore the need for checksums (checksumming of a single
+ * byte is handled by the XBee; checksumming a multiple byte message would have
+ * to be done by the application layer).
+ *
  * Protocol is as follows:
- * Buttons: [11CCXXXX]
- *   (two high bits, two checksum bits, four data bits)
- * Stick: [0000YYCC][XXXXXXXX] 
- *   (four low bits, two address bits, eight data bits)
- *   address bits are as follows:
- *     bit 0 = Left / Right (L = 0, R = 1)
- *     bit 1 = X / Y (X = 0, Y = 1)
- *   data bits are raw value of stick
  *
- * By default the controller does not send analog (stick) data; to turn on
- * analog controllers, send a non-zero byte.  To disable analog controllers, send
- * byte 0x00.  
- * TODO Do we want a checksummed protocol for receiving data?  Perhaps if there is more
- * than just this...
+ * Sending: Controller status:
+ *  
+ *  No Buttons Pressed: [10000000]
+ *    Static packet meaning 'no buttons pressed'.
+ *  Button Press: [1111XXXX]
+ *    1: Four high bits
+ *    X: Four data bits, indicating button number 0 - 15
+ *  Stick: [0AAXXXXX]
+ *    0: One low bit
+ *    A: Two address bits
+ *      bit 0 = Left / Right (L = 0, R = 1)
+ *      bit 1 = X / Y (X = 0, Y = 1)
+ *    X: Five value bits
  *
- * All checksums are 2 bit checksum on the rest of the packet, calculated using the 
- *  2 bit parity word algorithm.  Each 2-bit chunk of the packet is XOR'd together. 
- *  The receiver software will XOR all 8 2 bit words together; if the result is not 0x0
- *  then we know there was an error in transmission.
+ *
+ *
+ * Receiving: Message format: [CCXXXXXX]
+ *     C: Command (see below)
+ *     X: Command-specific message, left padded with zeros
+ *
+ *  Commands:
+ *    00: Enable / Disable Buttons
+ *      [000AAAAX]
+ *      A: Button address.  Specify a button to modify (0x00 - 0x0F).  Ignored if B = 0
+ *      X: New value.  1 = enabled, 0 = disabled
+ *    01: Enable / Disable Analog Sticks
+ *      [0100000X]
+ *      X: New value.  1 = enabled, 0 = disabled
+ *    10: Set poll frequency
+ *      [10XXXXXX]
+ *      X: New poll value, to be shifted 8 bits left and set in OCR1A.
+ *         For instance, to set OCR1A = 0x0F00 (the default value), you would send the 
+ *         value 001111 (0x0F) as shifting this 8 bits gives 0x0F00.
+ *         0x0F (expanded to 0x0F00) results in an idle poll time of about 500ms.  
+ *         Disable idle polling completely (only send change events) by 
+ *         sending value 0x00.
+ *    11: Reserved
  */
 
 
 #include <stdlib.h>
 #include <avr/io.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
 
 #include "lib/psx/psx.h"
 #include "lib/serial/serial.h"
 
+//Configuration state
 static uint8_t analog_enabled = 0;
+static uint16_t button_mask = 0xFFFF;
 
+//Temporal controller state
 static uint8_t last_lx = 0;
 static uint8_t last_ly = 0;
 static uint8_t last_rx = 0;
@@ -66,104 +97,82 @@ int main (void){
 		
 	//Main program loop
 	while (1){
+	
+		//Check if there are any incoming command messages
 		if (serial_available()){
-			serial_read_c((char*) &analog_enabled);
+			uint8_t message;
+			serial_read_c((char*) &message);
+
+			switch (message >> 6){
+				case 0x00:
+					if (message & 0x01){
+						button_mask |= _BV((message >> 1));
+					}
+					else {
+						button_mask &= ~_BV((message >> 1));
+					}
+					break;
+				case 0x01:
+					analog_enabled = message & 0x01;
+					break;
+				case 0x02:
+					if ((message & 0x3F) == 0x00){
+						OCR1A = 0x0000;
+						TIMSK1 &= ~_BV(OCIE1A);  //Disable compare interrupt
+					}
+					else {
+						OCR1A = ((message & 0x3F) << 8);
+						TIMSK1 |= _BV(OCIE1A);  //Enable compare interrupt					
+					}
+					break;
+			}
 		}
 	
+		//Poll the gamepad
 		psx_read_gamepad();
-		uint16_t buttons = psx_buttons();
-		uint8_t lx = psx_stick(PSS_LX);
-		uint8_t ly = psx_stick(PSS_LY);
-		uint8_t rx = psx_stick(PSS_RX);
-		uint8_t ry = psx_stick(PSS_RY);
-
-		if (buttons != last_buttons || (analog_enabled && (lx != last_lx || ly != last_ly || rx != last_rx || ry != last_ry))){
+		
+		//Check digital (button) state
+		uint16_t buttons = psx_buttons() & button_mask;
+		if (buttons != last_buttons){
 			last_buttons = buttons;
 			
 			TCNT1 = 0x00;	//Reset idle timer
 			
 			if (buttons == 0x00){
-				serial_write_c(0x00);
+				serial_write_c((char) 0x80);		//Static message meaning 'no buttons pressed'
 			}
 			else {
 				for (uint8_t x = 0; x < 16; x++){
 					if (buttons & _BV(x)){
-						uint8_t button_message = 0xC0;			//Start bits
-						uint8_t checksum = 0x3;					//0x03 is the checksum of the start bits
-
-						button_message |= (x & 0x0F);			//Button (0 - 15)
-
-						checksum ^= (x >> 0x02) & 0x03;			//Add 2 MSB of button to checksum
-						checksum ^= x & 0x03;					//Add 2 LSB of button to checksum
-					
-						button_message |= (checksum << 0x04);	//Set current checksum
-					
-						serial_write_c(button_message);			//Send message
+						serial_write_c(x | 0xF0);	//Four high bits, then 4 bit button address
 					}
 				}
 			}
 		}
 
+		//Check analog (stick) state
 		if (analog_enabled){
+			//We only send the 5 MSB of the sticks.
+			uint8_t lx = psx_stick(PSS_LX) >> 3;
+			uint8_t ly = psx_stick(PSS_LY) >> 3;
+			uint8_t rx = psx_stick(PSS_RX) >> 3;
+			uint8_t ry = psx_stick(PSS_RY) >> 3;
+
 			if (lx != last_lx){
 				last_lx = lx;
-				uint8_t stick_message = 0x00;		//LX
-				uint8_t checksum = 0x00;			//0x00 is the checksum of the start bits
-			
-				checksum ^= (lx >> 0x06) & 0x03;	//Add 2 MSB of stick value to checksum
-				checksum ^= (lx >> 0x04) & 0x03;	//Add next 2 bits of stick value to checksum
-				checksum ^= (lx >> 0x02) & 0x03;	//Add next 2 bits of stick value to checksum
-				checksum ^= lx & 0x03;				//Add 2 LSB of stick value to checksum
-
-				stick_message |= checksum;			//Set current checksum
-			
-				serial_write_c(stick_message);		//Send message (two bytes);
-				serial_write_c(lx);
+				serial_write_c(0x00 | lx);		//Address = 00
 			}
 			if (ly != last_ly){
 				last_ly = ly;
-				uint8_t stick_message = 0x01;		//LY
-				uint8_t checksum = 0x01;			//0x01 is the checksum of the start bits
-			
-				checksum ^= (ly >> 0x06) & 0x03;	//Add 2 MSB of stick value to checksum
-				checksum ^= (ly >> 0x04) & 0x03;	//Add next 2 bits of stick value to checksum
-				checksum ^= (ly >> 0x02) & 0x03;	//Add next 2 bits of stick value to checksum
-				checksum ^= ly & 0x03;				//Add 2 LSB of stick value to checksum
-
-				stick_message |= checksum;			//Set current checksum
-			
-				serial_write_c(stick_message);		//Send message (two bytes);
-				serial_write_c(ly);
+				serial_write_c(0x20 | ly);		//Address = 01
 			}
 			if (rx != last_rx){
 				last_rx = rx;
-				uint8_t stick_message = 0x02;		//RX
-				uint8_t checksum = 0x02;			//0x02 is the checksum of the start bits
-			
-				checksum ^= (rx >> 0x06) & 0x03;	//Add 2 MSB of stick value to checksum
-				checksum ^= (rx >> 0x04) & 0x03;	//Add next 2 bits of stick value to checksum
-				checksum ^= (rx >> 0x02) & 0x03;	//Add next 2 bits of stick value to checksum
-				checksum ^= rx & 0x03;				//Add 2 LSB of stick value to checksum
-
-				stick_message |= checksum;			//Set current checksum
-			
-				serial_write_c(stick_message);		//Send message (two bytes);
-				serial_write_c(rx);
+				serial_write_c(0x40 | rx);		//Address = 10
 			}
 			if (ry != last_ry){
 				last_ry = ry;
-				uint8_t stick_message = 0x03;		//RY
-				uint8_t checksum = 0x03;			//0x03 is the checksum of the start bits
-			
-				checksum ^= (ry >> 0x06) & 0x03;	//Add 2 MSB of stick value to checksum
-				checksum ^= (ry >> 0x04) & 0x03;	//Add next 2 bits of stick value to checksum
-				checksum ^= (ry >> 0x02) & 0x03;	//Add next 2 bits of stick value to checksum
-				checksum ^= ry & 0x03;				//Add 2 LSB of stick value to checksum
-
-				stick_message |= checksum;			//Set current checksum
-			
-				serial_write_c(stick_message);		//Send message (two bytes);
-				serial_write_c(ry);
+				serial_write_c(0x60 | ry);		//Address = 11
 			}
 		}
 	}
@@ -172,10 +181,7 @@ int main (void){
 EMPTY_INTERRUPT(TIMER1_COMPB_vect)
 EMPTY_INTERRUPT(TIMER1_OVF_vect)
 ISR(TIMER1_COMPA_vect){
-	last_buttons = 0xFF;	//Force a re-send of state
-	last_lx = 0xFF;
-//	last_ly = 0xFF;
-//	last_rx = 0xFF;
-//	last_ry = 0xFF;
+	//Force a re-send of button state
+	last_buttons = 0xFF;
 }
 
