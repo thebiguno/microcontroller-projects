8f0729b8082fd09f52ff2bf27fcb371d524dd787
Wyatt <wyatt@digitalcave.ca>
2014-03-05 14:30:43 -0700

    Manchester TX seems to be working (at least when eyeballing the output).  Manchester RX is not yet working, but is getting closer.

--- manchester/manchester_sync_rx.c
+++ manchester/manchester_sync_rx.c
@@ -5,9 +5,9 @@
 #include "manchester.h"
 #include <avr/interrupt.h>
 
-static uint8_t lower;	// 1/4
-static uint8_t mid;   	// 3/4
-static uint8_t upper;	// 5/4
+static uint16_t lower;	// 1/4
+static uint16_t mid;   	// 3/4
+static uint16_t upper;	// 5/4
 
 static volatile uint8_t mbox;
 static volatile uint8_t buf;
@@ -21,24 +21,16 @@ void manchester_init_rx(uint16_t baud){
 	TCCR0A = 0x0; 				// normal mode
 	TCCR0B |= _BV(CS02);        // F_CPU / 256 prescaler
 	
-	uint8_t half = F_CPU / 255 / baud;
-	lower = half / 2;
-	mid = lower + half;
-	upper = mid + half;
+	uint8_t half = F_CPU / 256 / baud;		//208 at 16MHz and 300 baud
+	lower = half / 2;						//104 at "
+	mid = lower + half;						//312 at "
+	upper = mid + half;						//520 at "
 
-	EICRA |= _BV(ISC00);		// any logical change on int0
-	EIMSK |= _BV(INT0);			// enable external interrupts on int0
-	TIMSK0 = _BV(OCIE0A);		// enable timer
+	EICRA |= _BV(ISC10);		// any logical change on int0
+	EIMSK |= _BV(INT1);			// enable external interrupts on int0
 	
 	sei();
 }
-
-void manchester_write_c(char data){
-	//Nop loop to wait until last transmission has completed
-	while (!(UCSR0A & _BV(UDRE0)));
-	UDR0 = data;
-}
-
 
 uint8_t manchester_available() {
 	return mbox > 0;
@@ -52,9 +44,10 @@ uint8_t manchester_read(uint8_t *b) {
 }
 
 static inline void readBit() {
-	uint8_t input = PORTD & _BV(PIND6);
+	uint8_t input = PORTD & _BV(PIND2);
 	
 	if (packet_state == 0) {
+		PORTC ^= _BV(PINC2);
 		// preamble
 		if (input) {
 			bit_count++;
@@ -68,6 +61,7 @@ static inline void readBit() {
 			buf = 0x00;
 		}
 	} else {
+		PORTC ^= _BV(PINC3);
 		// data
 		if (input) {
 			buf |= _BV(bit_count);
@@ -79,15 +73,21 @@ static inline void readBit() {
 			mbox = 1;
 			packet_state = 0;
 			bit_count = 0;
+			TIMSK0 &= ~_BV(OCIE0A);		// disable timer
+			TCNT0 = 0x00;				//reset timer counter
 			// TODO fire an interrupt
 		}
 	}
 }
 
-ISR(INT0_vect) {
+ISR(INT1_vect) {
+	PORTC ^= _BV(PINC1);
+	TIMSK0 |= _BV(OCIE0A);		// enable timer
+	
 	uint8_t ck = TCNT0;
 	if (ck < lower) {
 		// error
+		PORTC ^= _BV(PINC0);
 		signal_state = 0; // clear signal state
 	} else if (ck < mid) {
 		// T
@@ -99,17 +99,19 @@ ISR(INT0_vect) {
 		// 2T
 		if (signal_state == 0) {
 			readBit();
-		} else {
-			// error
-			signal_state = 0; // clear signal state
 		}
+		signal_state ^= _BV(1); // toggle signal state
 	} else {
 		// error
+		PORTC ^= _BV(PINC0);
 		signal_state = 0;
 	}
 	
 	TCNT0 = 0x00;
 }
 
+EMPTY_INTERRUPT(TIMER0_COMPA_vect)
+EMPTY_INTERRUPT(TIMER0_COMPB_vect)
+EMPTY_INTERRUPT(TIMER0_OVF_vect)
 
 
--- manchester/manchester_sync_tx.c
+++ manchester/manchester_sync_tx.c
@@ -1,9 +1,9 @@
 #include "manchester.h"
 #include <avr/interrupt.h>
 
-static volatile uint8_t *port;
-static volatile uint8_t *ddr;
-static uint8_t pin;
+static volatile uint8_t *_port;
+static volatile uint8_t *_ddr;
+static uint8_t _pin;
 
 static volatile uint8_t buf;
 static volatile uint8_t bit_count;
@@ -13,8 +13,10 @@ static volatile uint8_t signal_state; // signal position in the bit (1 = T, 0 = 
  * Using timer 0 the baud rate can range from 300 to 62500 at 16 MHz
  */
 void manchester_init_tx(volatile uint8_t *port, uint8_t pin, uint16_t baud){
-	ddr = port - 0x1;
-	*ddr |= _BV(pin);	//Enable output on selected pin
+	_port = port;
+	_pin = pin;
+	_ddr = port - 0x1;
+	*_ddr |= _BV(pin);	//Enable output on selected pin
 	
 	TCCR0A = 0x0; 				// normal mode
 	TCCR0B |= _BV(CS02);        // F_CPU / 256 prescaler
@@ -25,9 +27,9 @@ void manchester_init_tx(volatile uint8_t *port, uint8_t pin, uint16_t baud){
 }
 
 void write(uint8_t data) {
-	PORTD &= ~_BV(PORTD4);	//TODO
+	PORTD ^= _BV(PORTD2);	//TODO
 	while (TIMSK0 & _BV(OCIE0A));	//Wait until the compare a interrupt is no longer enabled
-	PORTD |= _BV(PORTD4);	//TODO
+	PORTD ^= _BV(PORTD3);	//TODO
 	buf = data;
 	bit_count = 0;
 	signal_state = 0;
@@ -36,7 +38,7 @@ void write(uint8_t data) {
 }
 
 void manchester_write(uint8_t data) {
-	write(0x7E);
+	write(0x7F);
 	write(data);
 }
 
@@ -44,28 +46,30 @@ EMPTY_INTERRUPT(TIMER0_COMPB_vect)
 EMPTY_INTERRUPT(TIMER0_OVF_vect)
 ISR(TIMER0_COMPA_vect) {
 	TCNT0 = 0x00;		//reset timer counter
-	PORTD |= _BV(PORTD5);	//TODO
+	PORTD ^= _BV(PORTD4);	//TODO
 	uint8_t bit = (buf & _BV(bit_count));
-	PORTD |= _BV(PORTD7);	//TODO
 	if (bit) {
-		
+		PORTD |= _BV(PORTD5);	//TODO
 		if (signal_state == 0) {
-			*port &= ~_BV(pin); // low
+			*_port &= ~_BV(_pin); // low
 		} else {
-			*port |= _BV(pin);  // to high
+			*_port |= _BV(_pin);  // to high
 		}
 	} else {
-		
+		PORTD &= ~_BV(PORTD5);	//TODO
 		if (signal_state == 0) {
-			*port |= _BV(pin);  // high
+			*_port |= _BV(_pin);  // high
 		} else {
-			*port &= ~_BV(pin); // to low
+			*_port &= ~_BV(_pin); // to low
 		}
 	}
 	
-	PORTD &= ~_BV(PORTD7);	//TODO
+	signal_state ^= 0x01;
 	
-	bit_count++;
+	if (signal_state == 0) bit_count++;
+	
+	PORTD ^= _BV(PORTD7);	//TODO
+	
 	if (bit_count >= 8) {
 		TIMSK0 &= ~_BV(OCIE0A);		// disable timer
 		PORTD = 0x00;	//TODO
