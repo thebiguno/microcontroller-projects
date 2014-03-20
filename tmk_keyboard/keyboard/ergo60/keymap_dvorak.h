0da3990967c6c69263bf214b64c6ccd2862934c7
Warren Janssens <warren.janssens@gmail.com>
2014-03-19 22:49:30 -0600

    updating keymaps

--- tmk_keyboard/keyboard/ergo60/keymap_dvorak.h
+++ tmk_keyboard/keyboard/ergo60/keymap_dvorak.h
@@ -12,7 +12,7 @@
 * |  ;:  |   Q  |   J  |   K  |   X  |                                   |   B  |   M  |   W  |   V  |   Z  |
 * |------+------+------+------+------|                                   |------+------+------+------+------|
 * |  =+  |  -_  | Left | Right|      |                                   |      | Up   | Down |  [{  |  ]}  |
-* | Ctrl | Alt  | Cmd  | Shift|  Fn  |                                   |  Fn  | Shift| Cmd  | Alt  | Ctrl |
+* | Shift| Ctrl | Alt  | Gui  |  Fn  |                                   |  Fn  | Gui  | Alt  | Ctrl | Shift|
 * `----------------------------------'                                   `----------------------------------'
 *                                    ,-------------.       ,-------------.
 *                                    |      | \|   |       | `~   | §±   |
@@ -27,16 +27,16 @@
 * |------+------+------+------+------|                                   |------+------+------+------+------
 * |  F11 |  F12 |  F13 |  F14 | Vol+ |                                   |  >>  |   7  |   8  |   9  |   =  |
 * |------+------+------+------+------|                                   |------+------+------+------+------|
-* | Caps |      |      |      | Mute |                                   |  >|| |   4  |   5  |   6  |   +  |
+* |      |      |      |      | Mute |                                   |  >|| |   4  |   5  |   6  |   +  |
 * |------+------+------+------+------|                                   |------+------+------+------+------|
 * |      |      |      |      | Vol- |                                   |  <<  |   1  |   2  |   3  |   -  |
 * |------+------+------+------+------'                                   |------+------+------+------+------|
-* | TRNS | TRNS | Home | End  | TRNS |                                   | TRNS | PgUp | PgDn | TRNS | TRNS |
+* | TRNS | TRNS | Home | End  | TRNS |                                   | TRNS | PgUp | PgDn |   .  | Clr  |
 * `----------------------------------'                                   `----------------------------------'
 *                                    ,-------------.       ,-------------.
 *                                    |      |      |       |   *  |   /  |
 *                             ,------|------|------|       |------+------+------.
-*                             | Del  | TRNS | TRNS |       | TRNS | KEnt |   0  |
+*                             | Del  | Caps |      |       |      | KEnt |   0  |
 *                             `--------------------'       `--------------------'
 *
 */
@@ -48,57 +48,50 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            QUOT,COMM,DOT ,P   ,Y   ,
            A   ,O   ,E   ,U   ,I   ,
            SCLN,Q   ,J   ,K   ,X   ,
-           FN4 ,FN5 ,FN6 ,FN7 ,NO  ,
-                     NO  ,NO ,
-                     FN1 ,FN2 ,FN3 ,
+           FN1 ,FN2 ,FN3 ,FN4 ,FN5  ,
+                          CAPS,BSLS,
+                     BSPC,ESC ,QUOT ,
            6   ,7   ,8   ,9   ,0   ,
            F   ,G   ,C   ,R   ,L   ,
            D   ,H   ,T   ,N   ,S   ,
            B   ,M   ,W   ,V   ,Z   ,
-           NO  ,FN8 ,FN9 ,FN10,FN11,
-           NO  ,NO ,
-           FN12,FN13,FN14 
+           FN6 ,FN7 ,FN8 ,FN9,FN10,
+           GRV ,NUBS,
+           TAB ,ENT ,SPC
     ),
     /* Layer 1: Symbols */
     KEYMAP(
            F1  ,F2  ,F3  ,F4  ,F5  ,
            F11 ,F12 ,F13 ,F14 ,VOLU,
            NO  ,NO  ,NO  ,NO  ,MUTE,
-           NO  ,NO  ,HOME,END ,VOLD,
-           TRNS,TRNS,TRNS,TRNS,TRNS,
-                          TRNS,TRNS,
-                     TRNS,TRNS,TRNS,
+           NO  ,NO  ,NO  ,NO  ,VOLD,
+           TRNS,TRNS,HOME,END ,TRNS,
+                          NO  ,NO  ,
+                     DEL ,CAPS,NO  ,
            F6  ,F7  ,F8  ,F9  ,F10 ,
-           MNXT,FN15,FN16,FN17,FN18,
-           MPLY,BSLS,EQL ,MINS,NUBS,
-           MPRV,PGUP,PGDN,NO  ,NO  ,
-           TRNS,TRNS,TRNS,TRNS,TRNS,
-           TRNS,TRNS,
-           TRNS,TRNS,TRNS
+           MNXT,P7  ,P8  ,P9  ,PEQL,
+           MPLY,P4  ,P5  ,P6  ,PPLS,
+           MPRV,P1  ,P2  ,P3  ,PMNS,
+           TRNS,PGUP,PGDN,PDOT,NLCK,
+           PAST,PSLS,
+           NO  ,PENT,P0  
     ),};
 enum function_id {
     TEENSY_KEY,
 };
 static const uint16_t PROGMEM fn_actions[] = {
 		[0] = ACTION_FUNCTION(TEENSY_KEY),
-		[1] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_BSPC),
-		[2] = ACTION_LAYER_TAP_KEY(1, KC_DEL),
-		[3] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_ESC),
-		[4] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_SLSH),
-		[5] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_NUBS),
-		[6] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_LEFT),
-		[7] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_RGHT),
-		[8] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_UP),
-		[9] = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_DOWN),
-		[10] = ACTION_MODS_TAP_KEY(MOD_RALT, KC_LBRC),
-		[11] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_RBRC),
-		[12] = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_TAB),
-		[13] = ACTION_LAYER_TAP_KEY(1, KC_ENT),
-		[14] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_SPC),
-		[15] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),
-		[16] = ACTION_MODS_KEY(MOD_LSFT, KC_EQL),
-		[17] = ACTION_MODS_KEY(MOD_LSFT, KC_MINS),
-		[18] = ACTION_MODS_KEY(MOD_LSFT, KC_NUBS)
+		[1] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_EQL),
+		[2] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_MINS),
+		[3] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_LEFT),
+		[4] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_RGHT),
+		[5] = ACTION_LAYER_TAP_TOGGLE(1),
+		[6] = ACTION_LAYER_TAP_TOGGLE(1),
+		[7] = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_UP),
+		[8] = ACTION_MODS_TAP_KEY(MOD_RALT, KC_DOWN),
+		[9] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_LBRC),
+		[10] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_RBRC)
+
 };
 void action_function(keyrecord_t *event, uint8_t id, uint8_t opt) {
     if (id == TEENSY_KEY) {
--- tmk_keyboard/keyboard/ergo60/keymap_qwerty.h
+++ tmk_keyboard/keyboard/ergo60/keymap_qwerty.h
@@ -11,8 +11,8 @@
 * |------+------+------+------+------|                                   |------+------+------+------+------|
 * |   Z  |   X  |   C  |   V  |   B  |                                   |   N  |   M  |  .<  |  ,>  |  /?  |
 * |------+------+------+------+------|                                   |------+------+------+------+------|
+* | Shift| Ctrl | Alt  | Gui  |  Fn  |                                   |  Fn  | Gui  | Alt  | Ctrl | Shift|
 * |  =+  |  -_  | Left | Right|      |                                   |      | Up   | Down |  [{  |  ]}  |
-* | Ctrl | Alt  | Cmd  | Shift|  Fn  |                                   |  Fn  | Shift| Cmd  | Alt  | Ctrl |
 * `----------------------------------'                                   `----------------------------------'
 *                                    ,-------------.       ,-------------.
 *                                    |      | \|   |       | `~   | §±   |
@@ -27,16 +27,16 @@
 * |------+------+------+------+------|                                   |------+------+------+------+------
 * |  F11 |  F12 |  F13 |  F14 | Vol+ |                                   |  >>  |   7  |   8  |   9  |   =  |
 * |------+------+------+------+------|                                   |------+------+------+------+------|
-* | Caps |      |      |      | Mute |                                   |  >|| |   4  |   5  |   6  |   +  |
+* |      |      |      |      | Mute |                                   |  >|| |   4  |   5  |   6  |   +  |
 * |------+------+------+------+------|                                   |------+------+------+------+------|
 * |      |      |      |      | Vol- |                                   |  <<  |   1  |   2  |   3  |   -  |
 * |------+------+------+------+------'                                   |------+------+------+------+------|
-* | TRNS | TRNS | Home | End  | TRNS |                                   | TRNS | PgUp | PgDn | TRNS | TRNS |
+* | TRNS | TRNS | Home | End  | TRNS |                                   | TRNS | PgUp | PgDn |   .  | Clr  |
 * `----------------------------------'                                   `----------------------------------'
 *                                    ,-------------.       ,-------------.
 *                                    |      |      |       |   *  |   /  |
 *                             ,------|------|------|       |------+------+------.
-*                             | Del  | TRNS | TRNS |       | TRNS | KEnt |   0  |
+*                             | Del  | Caps | TRNS |       | TRNS | KEnt |   0  |
 *                             `--------------------'       `--------------------'
 *
 */
@@ -46,49 +46,51 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(
            1   ,2   ,3   ,4   ,5   ,
            Q   ,W   ,E   ,R   ,T   ,
-           A   ,S   ,D   ,F   ,G   ,     
+           A   ,S   ,D   ,F   ,G   ,
            Z   ,X   ,C   ,V   ,B   ,
-           LCTL,LALT,LGUI,TAB ,NO  ,          
-                     NO  ,DEL ,
-                     FN1 ,LSFT,BSPC ,
+           FN1 ,FN2 ,FN3 ,FN4 ,FN5  ,
+                          CAPS,BSLS,
+                     BSPC,ESC ,SLSH ,
            6   ,7   ,8   ,9   ,0   ,
            Y   ,U   ,I   ,O   ,P   ,
            H   ,J   ,K   ,L   ,SCLN,
            N   ,M   ,COMM,DOT ,SLSH,
-           NO  ,ENT ,RGUI,RALT,LCTL,
-           ESC ,NO ,
-           SPC ,RSFT,FN1 
+           FN6 ,FN7 ,FN8 ,FN9,FN10,
+           GRV ,NUBS,
+           TAB ,ENT ,SPC
     ),
     /* Layer 1: Symbols */
     KEYMAP(
            F1  ,F2  ,F3  ,F4  ,F5  ,
-           GRV ,HOME,UP  ,PGUP,VOLU,
-           FN8 ,LEFT,DOWN,RGHT,MUTE,     
-           F11 ,END ,F12 ,PGDN,VOLD,
-           TRNS,TRNS,TRNS,TRNS,TRNS,         
-                          TRNS,TRNS,
-                     TRNS,TRNS,TRNS,
+           F11 ,F12 ,F13 ,F14 ,VOLU,
+           NO  ,NO  ,NO  ,NO  ,MUTE,
+           NO  ,NO  ,NO  ,NO  ,VOLD,
+           TRNS,TRNS,HOME,END ,TRNS,
+                          NO  ,NO  ,
+                     DEL ,CAPS,NO  ,
            F6  ,F7  ,F8  ,F9  ,F10 ,
-           MNXT,LBRC,RBRC,FN2 ,FN3 ,
-           MPLY,BSLS,SLSH,FN4 ,FN5 ,
-           MPRV,EQL ,MINS,FN6 ,FN7 ,
-           TRNS,TRNS,TRNS,TRNS,TRNS,
-           TRNS,CAPS,
-           TRNS,TRNS,TRNS
+           MNXT,P7  ,P8  ,P9  ,PEQL,
+           MPLY,P4  ,P5  ,P6  ,PPLS,
+           MPRV,P1  ,P2  ,P3  ,PMNS,
+           TRNS,PGUP,PGDN,PDOT,NLCK,
+           PAST,PSLS,
+           NO  ,PENT,P0  
     ),};
 enum function_id {
     TEENSY_KEY,
 };
 static const uint16_t PROGMEM fn_actions[] = {
 		[0] = ACTION_FUNCTION(TEENSY_KEY),
-		[1] = ACTION_LAYER_MOMENTARY(1),
-		[2] = ACTION_MODS_KEY(MOD_LSFT, KC_LBRC),
-		[3] = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC),
-		[4] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),
-		[5] = ACTION_MODS_KEY(MOD_LSFT, KC_SLSH),
-		[6] = ACTION_MODS_KEY(MOD_LSFT, KC_EQL ),
-		[7] = ACTION_MODS_KEY(MOD_LSFT, KC_MINS),
-		[8] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV )
+		[1] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_EQL),
+		[2] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_MINS),
+		[3] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_LEFT),
+		[4] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_RGHT),
+		[5] = ACTION_LAYER_TAP_TOGGLE(1),
+		[6] = ACTION_LAYER_TAP_TOGGLE(1),
+		[7] = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_UP),
+		[8] = ACTION_MODS_TAP_KEY(MOD_RALT, KC_DOWN),
+		[9] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_LBRC),
+		[10] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_RBRC)
 };
 void action_function(keyrecord_t *event, uint8_t id, uint8_t opt) {
     if (id == TEENSY_KEY) {
