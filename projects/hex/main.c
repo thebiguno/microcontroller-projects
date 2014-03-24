132e32ae7311e949725916792b643e439ef2eb1c
Wyatt <wyatt@digitalcave.ca>
2014-03-23 20:47:06 -0600

    Added per-servo neutral calibration

--- projects/hex/main.c
+++ projects/hex/main.c
@@ -32,7 +32,31 @@
 #define MID_PHASE	1500
 #define MAX_PHASE	2200
 
-
+int16_t leg_neutral_offset[18] = {
+	//Front:
+	-150,	//L_COXA
+	0,		//L_FEMUR
+	-180,		//L_TIBIA
+	-120,	//R_COXA
+	0,		//R_FEMUR
+	-30,		//R_TIBIA
+	
+	//Middle:
+	-120,	//L_COXA
+	0,		//L_FEMUR
+	-130,		//L_TIBIA
+	-150,		//R_COXA
+	0,		//R_FEMUR
+	-120,		//R_TIBIA
+	
+	//Rear
+	-50,	//L_COXA
+	0,		//L_FEMUR
+	-150,	//L_TIBIA
+	-100,	//R_COXA
+	0,		//R_FEMUR
+	-50		//R_TIBIA
+};
 
 int main (void){
 	volatile uint8_t *ports[18];
@@ -82,10 +106,11 @@ int main (void){
 	pwm_init(ports, pins, 18, 20000);
 	
 	for (uint8_t i = 0; i < 18; i++){
-		pwm_set_phase(i, 1500);	//Set to neutral position
+		pwm_set_phase(i, 1500 + leg_neutral_offset[i]);	//Set to neutral position
 	}
 	
 	while(1){
+		/*
 		for (uint8_t i = 0; i < 18; i++){		
 			pwm_set_phase(i, MAX_PHASE);
 		}
@@ -98,5 +123,6 @@ int main (void){
 			pwm_set_phase(i, MID_PHASE);
 		}
 		_delay_ms(4000);
+		*/
 	}
 }
