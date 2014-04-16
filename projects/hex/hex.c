e5a2717e6e8774a035facb6861c2e0d699e58cfc
Wyatt <wyatt@digitalcave.ca>
2014-04-16 09:37:04 -0600

    Test code for servo pushrod conversion.  Will be reverted later.

--- projects/hex/hex.c
+++ projects/hex/hex.c
@@ -8,7 +8,20 @@ int main (void){
 	
 	DDRA |= _BV(PORTA1) | _BV(PORTA4);
 	
+	for (uint8_t l = 0; l < LEG_COUNT; l++){
+		servo_set_angle(l, COXA, 0);
+	}
+	_delay_ms(2000);
+	
 	while(1){
-		ripple_step();
+		//ripple_step();
+		for (uint8_t l = 0; l < LEG_COUNT; l++){
+			servo_set_angle(l, COXA, 1.5);
+		}
+		_delay_ms(2000);
+		for (uint8_t l = 0; l < LEG_COUNT; l++){
+			servo_set_angle(l, COXA, -1.5);
+		}
+		_delay_ms(2000);
 	}
 }
