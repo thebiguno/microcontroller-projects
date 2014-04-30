7677c6a7d0355e4a1359204fc48e276fd0c40702
Wyatt <wyatt@digitalcave.ca>
2014-04-29 21:10:03 -0600

    Re-working Ripple gait to work with current code, as an alternative to tripod.  Not quite there yet...

--- projects/stubby/ripple.c
+++ projects/stubby/ripple.c
@@ -1,5 +1,10 @@
 #include "gait.h"
 #define DELAY 		80
+#define COXA_STEP (COXA_FORWARD - COXA_REVERSE) / 5
+
+
+#define PRIMARY_CUTOFF 0.3
+#define SECONDARY_CUTOFF 0.2
 
 //The order in which we lift legs
 static uint8_t gait_leg_order[6] = {
@@ -11,13 +16,37 @@ static uint8_t gait_leg_order[6] = {
 	MIDDLE_RIGHT
 };
 
-/**
- * Take a ripple gait step with the specified leg.  This is the same as wave gait, but it only affects one side at a time, and 
- * each side is interleaved together.
- */
-void gait_step(double velocity, double direction){
+static inline double get_delay_multiplier(double *velocity, double *direction, uint8_t measurement){
+	
+	comm_read(velocity, direction);
+
+	double value = 0;
+	if (measurement == 0) value = fabs(*velocity);
+	else if (measurement == 1) value = fabs(*direction);
+	
+	if (value >= 0.8) return 1;
+	else if (value >= 0.7) return 1.2;
+	else if (value >= 0.6) return 1.4;
+	else return 1.8;
+}
+
+void ripple_gait_step(){
 	static uint8_t i = 0x00;
 
+	//Movement variables
+	double velocity;
+	double direction;
+	
+	//We control the direction of legs on each side based on the combination of velocity and direction values.
+	double left_direction;
+	double right_direction;
+	double delay_multiplier;	//The larger the multiplier, the slower it goes
+	double delay;				//Temp variable to avoid multiple re-calculations
+	
+	uint8_t measurement;
+	
+	comm_read(&velocity, &direction);
+	
 	uint8_t leg = gait_leg_order[i];
 	if (direction > 0){
 		i++;
@@ -27,94 +56,77 @@ void gait_step(double velocity, double direction){
 		i--;
 		if (i >= LEG_COUNT) i = LEG_COUNT - 1;	//i is unsigned, so will loop to 255.
 	}
-
-	
-	//We control the direction of legs on each side based on the combination of velocity and direction values.
-	double left_direction;
-	double right_direction;
 	
 	//Base case - no direction component, velocity only.  Set each side to forward / backward motion
-	if (fabs(direction) < 0.1){
+	if (fabs(velocity) >= PRIMARY_CUTOFF && fabs(direction) < SECONDARY_CUTOFF){
 		left_direction = velocity;
 		right_direction = velocity;
+		
+		measurement = 0;
 	}
-	//If there are both direction and velocity components, then still move forward / backward but veer.
-	else if (fabs(direction) >= 0.1 && fabs(velocity) >= 0.1){
+	//Velocity is greater than direction, but both are present
+	else if (fabs(velocity) >= PRIMARY_CUTOFF && fabs(direction) >= SECONDARY_CUTOFF && fabs(velocity) > fabs(direction)){
 		if (direction < 0) {	//Veer left
 			right_direction = velocity;
-			left_direction = velocity + direction;
+			left_direction = velocity + (velocity > 0 ? 1 : -1) * fmin(1, (direction * 2));
 		}
 		else {	//Veer right
-			right_direction = velocity - direction;
+			right_direction = velocity - (velocity > 0 ? 1 : -1) * fmin(1, (direction * 2));
 			left_direction = velocity;
 		}
+		measurement = 0;
+	}
+	//Direction is greater than velocity, but both are present
+	else if (fabs(direction) >= PRIMARY_CUTOFF && fabs(velocity) >= SECONDARY_CUTOFF && fabs(direction) > fabs(velocity)){
+		if (velocity < 0) {	//Veer left
+			right_direction = direction * -1 + velocity;
+			left_direction = direction;
+		}
+		else {	//Veer right
+			right_direction = direction * -1 - velocity;
+			left_direction = direction;
+		}
+		measurement = 1;
 	}
 	//If there is only a direction component, then just turn in place.
-	else {
+	else if (fabs(direction) >= PRIMARY_CUTOFF && fabs(velocity) < SECONDARY_CUTOFF){
 		right_direction = direction * -1;
 		left_direction = direction;
+		
+		measurement = 1;
+	}
+	else {
+		//Everything is 0
+		return;
 	}
 	
 	//Lift tibia (z)
-	leg_set_current_position_relative(leg, 0, 0, TIBIA_STEP);
-	leg_delay_ms(DELAY * 2);
+	delay_multiplier = get_delay_multiplier(&velocity, &direction, measurement);
+	delay = DELAY * 2 * delay_multiplier;
+	leg_set_desired_position_relative(leg, 0, 0, TIBIA_STEP, delay * 1.5);
+	leg_delay_ms(delay);
 	
 	//Which leg are we controlling: right or left?
 	double relevant_direction = ((leg & 0x01) == 0x00) ? left_direction : right_direction;
 	
 	//Move the coxa forward to starting position if we are moving forward; if we are moving backward, move it to the reverse position.
-	double starting_position = relevant_direction > 0 ? COXA_REVERSE : COXA_FORWARD;
-	leg_set_current_position_relative(leg, 0, starting_position, 0);
-	//leg_delay_ms(DELAY * 2);
+	delay_multiplier = get_delay_multiplier(&velocity, &direction, measurement);
+	delay = DELAY * 2 * delay_multiplier;
+	leg_set_desired_position_absolute(leg, 0, relevant_direction > 0 ? COXA_REVERSE : COXA_FORWARD, TIBIA_RAISED, delay * 1.5);
 	
-	//... while moving the other coxas back or forward by STEP.
-	for (uint8_t i = 0; i < LEG_COUNT; i++){
-		if (i != leg){
-			double coxa_step;
-			if ((i & 0x01) == 0x00) {	//this is an even leg, therefore the left side
-				coxa_step = left_direction * COXA_STEP;
-			}
-			else {
-				coxa_step = right_direction * COXA_STEP;
-			}
-			leg_set_current_position_relative(i, 0, coxa_step, 0);
+	//... while moving the other coxas back or forward; how much depends on velocity and direction.
+	// Here we assume that COXA_REVERSE == COXA_FORWARD * -1; in the event that this is not the case, you will
+	// have to handle each case separately.
+	for (uint8_t l = 0; l < LEG_COUNT; l++){
+		if (l != leg){
+			leg_set_desired_position_absolute(i, 0, COXA_STEP * ((l & 0x01) == 0x00 ? left_direction : right_direction), TIBIA_LOWERED, delay);
 		}
-	}
-	leg_delay_ms(DELAY * 1);
+	}	
+	leg_delay_ms(delay);
 
 	//Drop tibia to neutral position
-	leg_set_current_position_absolute(leg, 0, starting_position, TIBIA_LOWERED);
-	leg_delay_ms(DELAY * 1);
-}
-
-void gait_init(){
-	leg_set_current_position_relative(REAR_LEFT, 0, 0, TIBIA_STEP);
-	leg_set_current_position_relative(FRONT_RIGHT, 0, 0, TIBIA_STEP);
-	leg_delay_ms(DELAY / 2);
-	leg_set_current_position_absolute(REAR_LEFT, 0, COXA_NEUTRAL, TIBIA_RAISED);
-	leg_set_current_position_absolute(FRONT_RIGHT, 0, COXA_NEUTRAL, TIBIA_RAISED);
-	leg_delay_ms(DELAY * 2);
-	leg_set_current_position_absolute(REAR_LEFT, 0, COXA_NEUTRAL, TIBIA_LOWERED);
-	leg_set_current_position_absolute(FRONT_RIGHT, 0, COXA_NEUTRAL, TIBIA_LOWERED);
-	leg_delay_ms(DELAY / 4);
-
-	leg_set_current_position_relative(MIDDLE_LEFT, 0, 0, TIBIA_STEP);
-	leg_set_current_position_relative(MIDDLE_RIGHT, 0, 0, TIBIA_STEP);
-	leg_delay_ms(DELAY / 2);
-	leg_set_current_position_absolute(MIDDLE_LEFT, 0, COXA_NEUTRAL, TIBIA_RAISED);
-	leg_set_current_position_absolute(MIDDLE_RIGHT, 0, COXA_NEUTRAL, TIBIA_RAISED);
-	leg_delay_ms(DELAY * 2);
-	leg_set_current_position_absolute(MIDDLE_LEFT, 0, COXA_NEUTRAL, TIBIA_LOWERED);
-	leg_set_current_position_absolute(MIDDLE_RIGHT, 0, COXA_NEUTRAL, TIBIA_LOWERED);
-	leg_delay_ms(DELAY / 4);
-
-	leg_set_current_position_relative(FRONT_LEFT, 0, 0, TIBIA_STEP);
-	leg_set_current_position_relative(REAR_RIGHT, 0, 0, TIBIA_STEP);
-	leg_delay_ms(DELAY / 2);
-	leg_set_current_position_absolute(FRONT_LEFT, 0, COXA_NEUTRAL, TIBIA_RAISED);
-	leg_set_current_position_absolute(REAR_RIGHT, 0, COXA_NEUTRAL, TIBIA_RAISED);
-	leg_delay_ms(DELAY * 2);
-	leg_set_current_position_absolute(FRONT_LEFT, 0, COXA_NEUTRAL, TIBIA_LOWERED);
-	leg_set_current_position_absolute(REAR_RIGHT, 0, COXA_NEUTRAL, TIBIA_LOWERED);
-	leg_delay_ms(DELAY / 4);
+	delay_multiplier = get_delay_multiplier(&velocity, &direction, measurement);
+	delay = DELAY * 1 * delay_multiplier;
+	leg_set_desired_position_absolute(leg, 0, relevant_direction > 0 ? COXA_REVERSE : COXA_FORWARD, TIBIA_LOWERED, delay * 1.2);
+	leg_delay_ms(delay);
 }
