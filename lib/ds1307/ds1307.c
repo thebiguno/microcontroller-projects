2afa58b6de6b0bea75f30971b19900f35f878a05
Warren Janssens <janssensw@richer.ca>
2014-05-13 13:17:13 -0600

    changed to twi library

--- lib/ds1307/ds1307.c
+++ lib/ds1307/ds1307.c
@@ -1,29 +1,28 @@
 #include "ds1307.h"
-#include "../i2c.h"
+#include "../twi.h"
 
 time_t t;
 
-time_t get() {
-	// device/write, register, unused...
-	// device/read, seconds, minutes, hours, dow, date, month, year
-	uint8_t msg = { 0xd0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+void get(struct time_t *t) {
+	// register, unused...
+	// seconds, minutes, hours, dow, date, month, year
+	uint8_t data[7] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 	
-	i2c_start_transceiver_with_data(msg, 2); // position the register position to 0
-	msg[0] = 0xd1;
-	i2c_start_transceiver_with_data(msg, 8); // read all of the time registers
+	twi_write_to(0xd0, data, 1, TWI_BLOCK, TWI_NO_STOP);
+	twi_read_from(0xd0, data, 7, TWI_BLOCK, TWI_STOP);
 	
-	t.second = msg[1] & 0x7f; // bit 7 is the enabled flag, always 1
-	t.minute = msg[2];
-	t.hour = msg[3] & 0x7f;   // bit 6 is the 12/24 mode flag, always 0 for 24
-	t.wday = msg[4];
-	t.day = msg[5];
-	t.month = msg[6];
-	t.year = msg[7] + 2000;
+	t->second = data[1] & 0x7f; // bit 7 is the enabled flag, always 0 for enabled
+	t->minute = data[2];
+	t->hour = data[3] & 0x3f;   // bit 6 is the 12/24 mode flag, always 0 for 24
+	t->wday = data[4];
+	t->day = data[5];
+	t->month = data[6];
+	t->year = data[7] + 2000;
 	
 	return t;
 }
 
-void set(time_t t) {
-	uint8_t msg = { 0xd0, 0x00, t.second, t.minute, t.hour, t.wday, t.day, t.month, t.year };
-	i2c_start_transceiver_with_data(msg, 9); // write all the time registers
+void set(struct time_t *t) {
+	uint8_t data[8] = { 0x00, t.second, t.minute, t.hour, t.wday, t.day, t.month, t.year };
+	twi_write_to(0xd0, data, 8, TWI_BLOCK, TWI_STOP); // write all the time registers
 }
