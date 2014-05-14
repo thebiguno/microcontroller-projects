6e83135a6f34ac6c823df259e1c37c1c6168499d
Warren Janssens <janssensw@richer.ca>
2014-05-14 09:26:16 -0600

    changing prescale to 1024 and calculating constants from F_CPU

--- lib/remote/remote.c
+++ lib/remote/remote.c
@@ -1,20 +1,20 @@
 /*
 NEC IR transmission protocol:
 http://techdocs.altium.com/display/ADRR/NEC+Infrared+Transmission+Protocol
-														@ 8 Mhz/256
-9 ms leading pulse										281 (overflow)
-4.5 ms leading space									140
+														@ 8 MHz/1024	@ 16 MHz/1024	@ 20MHz/1024
+9 ms leading pulse										70 				140				176
+4.5 ms leading space									35				70				88
 8 bit device address							
 8 bit logical inverse of the device address
 8 bit command
 8 bit logical inverse of the command
-562.5 us trailing pulse									17
-2250 us: 562.5 us high, 1687.5 us low = logical 1		70 = 17 + 53 
-1125 us: 562.5 us high, 562.5 us low = logical 0		35 = 17 + 17
+562.5 us trailing pulse									4				8				11
+2250 us: 562.5 us high, 1687.5 us low = logical 1		17 = 4 + 13		43 = 17 + 26	43 = 11 + 33
+1125 us: 562.5 us high, 562.5 us low = logical 0		8 = 4 + 4		16 = 8 + 8		22 = 11 + 11
 
-9 ms leading pulse										281
-2.25 ms leading pulse									70
-562.5 us trailing pulse									17
+9 ms leading pulse										70				140				176
+2.25 ms leading pulse									17				35				44
+562.5 us trailing pulse									4				8				11
 
 Apple aluminum remote codes: (Apple ID EE 87, Command, Remote ID)
 https://en.wikipedia.org/wiki/Apple_Remote
@@ -27,6 +27,13 @@ https://en.wikipedia.org/wiki/Apple_Remote
 	Play key:   EE 87 5E/5F 59
 */
 
+// leading pulse must be at least 4.5 ms
+#define LEADING_PULSE = F_CPU / 1024 / 222
+// leading space must be at least 3.4 ms for a new command, otherwise it's a repeat command
+#define LEADING_SPACE = F_CPU / 1024 / 296
+// bit space must be at least 1126 us for logical 1, otherwise it's logical 0
+#define BIT_SPACE = F_CPU / 1024 / 888
+
 volatile uint8_t _state;	// 0 = idle; 1 = leading pulse; 2 = message;
 volatile uint8_t _byte;
 volatile uint8_t _bit;
@@ -34,11 +41,11 @@ volatile uint8_t _packet[4];
 volatile uint8_t _command;
 
 void remote_init() {
-	// TODO dynamic prescale based on F_CPU; current values appropriate only for 8MHz
-	
-	#if defined(__AVR_ATtiny13__)
 	TCCR0A = 0x0; 						// normal mode
-	TCCR0B |= _BV(CS02);				// F_CPU / 256 prescaler
+	TCCR0B |= _BV(CS02) | _BV(CS00);	// F_CPU / 1024 prescaler
+
+	#if defined(__AVR_ATtiny13__)      || \
+		defined(__AVR_ATtiny85__)
 	MCUCR |= _BV(ISC00);				// logical change generates interrupt
 	GIMSK |= _BV(INT0);					// enable external interrupts on int0
 	#elif defined(__AVR_ATmega168__)   || \
@@ -49,8 +56,6 @@ void remote_init() {
 		defined(__AVR_ATmega644P__)    || \
 		defined(__AVR_ATmega644PA__)   || \
 		defined(__AVR_ATmega1284P__)
-	TCCR0A = 0x0; 						// normal mode
-	TCCR0B |= _BV(CS02);				// F_CPU / 256 prescaler
 	EICRA |= _BV(ISC00);				// logical change generates interrupt
 	EIMSK |= _BV(INT0);					// enable external interrupts on int0
 	#endif
@@ -69,18 +74,15 @@ uint8_t remote_command() {
 }
 
 ISR(TIM0_OVF_vect) {
-	if (_state == 0 && (PORTB & _BV(PB1))) {
-		_state = 1;
-	}
+	_state = 0;
 }
 EMPTY_INTERRUPT(TIM0_COMPA_vect)
-}
 EMPTY_INTERRUPT(TIM0_COMPB_vect)
 
 ISR(INT0_vect) {
 	if (PORTB & _BV(PB1)) {
 		if (_state == 1) {
-			if (TCNT > 105) {
+			if (TCNT > LEADING_SPACE) {
 				_state = 2;
 				_byte = 0;
 				_bit = 0;
@@ -93,10 +95,11 @@ ISR(INT0_vect) {
 				_command = _packet[2];
 			}
 		} else if (_state == 2) {
-			if (TCNT > 35) { // TODO define this constant based on F_CPU
+			if (TCNT > BIT_SPACE) {
 				_packet[_byte] &= _BV(_bit++);
 			}
-			if (bit == 0) {
+			if (bit == 8) {
+				bit = 0;
 				byte++;
 				
 				if (byte == 5) {
@@ -105,6 +108,12 @@ ISR(INT0_vect) {
 					}
 					_state = 0;
 				}
+			}
+		}
+	} else {
+		if (_state == 0) {
+			if (TCNT > LEADING_PULSE) {
+				_state = 1;
 			}
 		}
 	}
