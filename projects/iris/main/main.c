0d966b98d19df62408955a668751daecec325d68
Warren Janssens <janssensw@richer.ca>
2014-05-29 10:00:37 -0600

    updating palette

--- projects/iris/main/main.c
+++ projects/iris/main/main.c
@@ -23,26 +23,28 @@
 #define MODE_MIN 131
 #define MODE_SEC 132
 
+extern time_t __system_time;
+
 int main() {
 	
 	DDRB |= _BV(1); // led strip output
 	
 	ws2811_t black = {.red = 0x00, .green = 0x00, .blue = 0x00 };
-//	ws2811_t base03 = {.red = 0x00, .green = 0x1b, .blue = 0x26 };			// background
-//	ws2811_t base02 = {.red = 0x07, .green = 0x26, .blue = 0x22 };
+//	ws2811_t base03 = {.red = 0x00, .green = 0x1b, .blue = 0x26 };
+	ws2811_t base02 = {.red = 0x07, .green = 0x26, .blue = 0x22 };		// background
 //	ws2811_t base01 = {.red = 0x48, .green = 0x5e, .blue = 0x65 };
 //	ws2811_t base00 = {.red = 0x55, .green = 0x6b, .blue = 0x73 };
 //	ws2811_t base0 = {.red = 0x73, .green = 0x84, .blue = 0x86 };
-//	ws2811_t base1 = {.red = 0x83, .green = 0x91, .blue = 0x91 };
-//	ws2811_t base2 = {.red = 0xde, .green = 0xd8, .blue = 0xc5 };		// foreground
+	ws2811_t base1 = {.red = 0x83, .green = 0x91, .blue = 0x91 };		// foreground
+	ws2811_t base2 = {.red = 0xde, .green = 0xd8, .blue = 0xc5 };		// markers
 //	ws2811_t base3 = {.red = 0xed, .green = 0xe6, .blue = 0xd3 };
 //	ws2811_t yellow = {.red = 0xa5, .green = 0x79, .blue = 0x00 };		// month summer
 //	ws2811_t orange = {.red = 0xa5, .green = 0x3b, .blue = 0x06 };		// month autumn
-	ws2811_t red = {.red = 0xcc, .green = 0x22, .blue = 0x1f };
+//	ws2811_t red = {.red = 0xcc, .green = 0x22, .blue = 0x1f };
 	ws2811_t magenta = {.red = 0xc3, .green = 0x26, .blue = 0x72 };
-	ws2811_t magenta_low = {.red = 0x0c, .green = 0x02, .blue = 0x07 };
+//	ws2811_t magenta_low = {.red = 0x0c, .green = 0x02, .blue = 0x07 };
 	ws2811_t violet = {.red = 0x5c, .green = 0x61, .blue = 0xb4 };
-	ws2811_t violet_low = {.red = 0x05, .green = 0x06, .blue = 0x0b };
+//	ws2811_t violet_low = {.red = 0x05, .green = 0x06, .blue = 0x0b };
 //	ws2811_t blue = {.red = 0x16, .green = 0x7b, .blue = 0xc2 };		// month winter
 //	ws2811_t cyan = {.red = 0x1a, .green = 0x91, .blue = 0x88 };
 //	ws2811_t green = {.red = 0x75, .green = 0x89, .blue = 0x00 };		// month spring
@@ -56,15 +58,14 @@ int main() {
 	remote_init();
 	
 	EICRA |= _BV(ISC11);	// trigger on falling edge of int1
-	EIMSK |= _BV(INT1);					// enable external interrupts on int1
+	EIMSK |= _BV(INT1);		// enable external interrupts on int1
 	
-	DDRD &= ~_BV(PD3);					// set 1 Hz int1 as input
-	PORTD |= _BV(PD3);					// set 1 Hz int1 as tri-state
-	
+	DDRD &= ~_BV(PD3);		// set 1 Hz int1 as input
+	PORTD |= _BV(PD3);		// set 1 Hz int1 as tri-state
 	
 	pcf8563_init();
 
-	int8_t mday = -1;					// used to avoid computing sunrise, sunset, and moon phase more than once per day;
+	int8_t mday = -1;		// used to avoid computing sunrise, sunset, and moon phase more than once per day;
 	time_t prev_systime = 0;
 	time_t systime;
 	time_t risetime;
@@ -91,12 +92,13 @@ int main() {
 	// read rtc (&sys);
 	systime = mktime(&sys);
 	set_system_time(systime);
+	
 	while (1) {
 		;
 		// 
 		// 	// recompute time structure
 		// 	prev_systime = systime;
-		localtime_r(&systime, &sys);
+		localtime_r(&__system_time, &sys);
 		// 
 		// 	if (sys.tm_mday != mday) {
 		// 		// recompute sunrise, sunset, moon phase
@@ -111,8 +113,10 @@ int main() {
 		// 	}
 		// 
 		// 	// update display
-		struct ws2811_t fg = magenta_low;
-		struct ws2811_t bg = violet_low;
+		if (__system_time != systime) {
+			systime = __system_time;
+			struct ws2811_t fg = magenta_low;
+			struct ws2811_t bg = violet_low;
 			if (mode == 0) {
 		// 		// hours, minutes, seconds
 		// 		if (systime > risetime && systime < settime) {
@@ -121,26 +125,23 @@ int main() {
 		// 			bg = base2;
 		// 		}
 				for (uint8_t i = 0; i < 60; i++) {
-					colors[i] = black;
+					colors[i] = base02;
 				}
 				// markers
 				for (uint8_t i = 0; i < 60; i = i + 5) {
-					colors[i] = magenta_low;
+					colors[i] = base2;
 				}
 				// hour fill
 				uint8_t hour = (sys.tm_hour % 12) * 5;
 				for (uint8_t i = hour + 1; i < hour + 5; i++) {
-					colors[i] = violet_low;
+					colors[i] = base1;
 				}
 				colors[sys.tm_min] = violet;
-				colors[sys.tm_sec] = red;
+				colors[sys.tm_sec] = magenta;
 			} else if (mode == 1) {
 				// month, day of month
 				for (uint8_t i = 0; i < 60; i++) {
-					colors[i] = black;
-				}
-				for (uint8_t i = 0; i < 60; i = i + 5) {
-					colors[i] = magenta_low;
+					colors[i] = base02;
 				}
 				//if (sys.tm_mon < 11) fg = orange;
 				//if (sys.tm_mon < 8) fg = yellow;
@@ -148,19 +149,23 @@ int main() {
 				//if (sys.tm_mon < 2) fg = blue;
 				// markers
 				for (uint8_t i = 0; i < 60; i = i + 5) {
-					colors[i] = magenta_low;
+					colors[i] = base2;
 				}
 				// month fill
 				uint8_t hour = (sys.tm_mon % 12) * 5;
 				for (uint8_t i = hour + 1; i < hour + 5; i++) {
-					colors[i] = violet_low;
+					colors[i] = base1;
 				}
 				// day fill
 				if (sys.tm_mday < 31) {
-					// the 31st is not shown
 					uint8_t i = sys.tm_mday * 2;
 					colors[i] = violet;
 					colors[i+1] = violet;
+				} else {
+					colors[58] = violet;
+					colors[59] = violet;
+					colors[0] = violet;
+					colors[1] = violet;
 				}
 			} else if (mode == 2) {
 				// week, day of week
@@ -212,10 +217,10 @@ int main() {
 		// 			}
 			}
 
-		// 	
-		ws2811_set(colors, 60, 1);
-		_delay_ms(250);
-		// 
+			// TODO translate the array
+			ws2811_set(colors, 60, 1);
+		}
+
 		// 	// read ir
 		uint8_t command = remote_get();
 		if (command == REMOTE_LEFT) {
@@ -225,7 +230,6 @@ int main() {
 			UDR0 = 'R';
 			if (mode < MODE_MOON) mode++;
 		}
-	//	}
 	}
 }
 
