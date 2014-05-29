17bc120121231d662b3142cad481d5ed09c0914b
Warren Janssens <warren.janssens@gmail.com>
2014-05-28 19:21:40 -0600

    driving the ws2811 sample at 8MHz!

--- samples/avr/atmega48/ws2811/main.c
+++ samples/avr/atmega48/ws2811/main.c
@@ -1,12 +1,14 @@
+#define WS2811_PORT PORTB
+
 #include <avr/io.h>
-#include "lib/ws2811/ws2811.h"
-#include <util/delay.h>
+#include "lib/ws2811/ws2811_8.h"
+#include <util/delay_basic.h>
 
 int main (void) {
 
 	//Do setup here
 	struct ws2811_t colors[60];
-	_delay_ms(500);
+	DDRB = _BV(1);
 	
 	//Main program loop
 	while (1) {
@@ -16,12 +18,11 @@ int main (void) {
 				colors[j].green = 0x6;
 				colors[j].blue = 0xb;
 			}
-			colors[i].red = 0x5c;
-			colors[i].green = 0x61;
-			colors[i].blue = 0xb4;
+			colors[i].red = 0xff;
+			colors[i].green = 0x00;
+			colors[i].blue = 0x00;
 
-			ws2811_set(colors, 60);
-			_delay_ms(255);	
+			ws2811_set(colors, 60, 1);
 		}
 	}
 }
