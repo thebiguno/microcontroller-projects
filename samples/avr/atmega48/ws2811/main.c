cf01f4852ea285287c8dbdf1e94823dc03a49b9e
Warren Janssens <warren.janssens@gmail.com>
2014-06-02 23:14:23 -0600

    wip plasma

--- samples/avr/atmega48/ws2811/main.c
+++ samples/avr/atmega48/ws2811/main.c
@@ -1,10 +1,38 @@
 #define WS2811_PORT PORTB
 
 #include <avr/io.h>
+#include <stdlib.h>
 #include "lib/ws2811/ws2811_8.h"
 #include <util/delay_basic.h>
 
+uint8_t r(uint8_t max) {
+	return rand() / (RAND_MAX / max + 1);
+}
+void a(uint8_t p1, uint8_t p2, uint8_t *x) {
+	if (p1 == p2) return;
+	
+	uint8_t p = p1 + p2;
+	if (p2 < p1) p += 60;
+	p /= 2;
+	if (p == p1 || p == p2) return;
+	p %= 60;
+	
+	uint8_t v = x[p1] + x[p2];
+	if (x[p2] < x[p1]) v += 12;
+	v /= 2;
+	//v = 1;
+	//v += (r(2) - 1);
+	//v %= 60;
+	x[p] = v;
+	
+	a(p1, p, x);
+	a(p, p2, x);
+}
+
 int main (void) {
+
+	TCCR0A = 0x00;
+	TCCR0B = _BV(CS02) | _BV(CS00);
 
 	ws2811_t black = {.red = 0x00, .green = 0x00, .blue = 0x00 };
 	ws2811_t red = {.red = 0xff, .green = 0x00, .blue = 0x00 };
@@ -20,29 +48,45 @@ int main (void) {
 	ws2811_t magenta = {.red = 0xff, .green = 0x00, .blue = 0xff };
 	ws2811_t rose = {.red = 0xdd, .green = 0x00, .blue = 0x33 };
 
-	//Do setup here
+	ws2811_t palette[12] = { red, orange, yellow, chartreuse, green, spring, cyan, azure, blue, violet, magenta, rose };
+	uint8_t x[60];
+	
+	// start with random colors at random locations
+	uint8_t p1 = 2;
+	uint8_t p2 = 22;
+	uint8_t p3 = 42;
+	x[p1] = 0;
+	x[p2] = 4;
+	x[p3] = 8;
+	
 	struct ws2811_t colors[60];
 	DDRB = _BV(1);
 	
-	for (uint8_t i = 0; i < 60; i++) {
-		if (i < 5) colors[i] = red;
-		else if (i < 10) colors[i] = orange;
-		else if (i < 15) colors[i] = yellow;
-		else if (i < 20) colors[i] = chartreuse;
-		else if (i < 25) colors[i] = green;
-		else if (i < 30) colors[i] = spring;
-		else if (i < 35) colors[i] = cyan;
-		else if (i < 40) colors[i] = azure;
-		else if (i < 45) colors[i] = blue;
-		else if (i < 50) colors[i] = violet;
-		else if (i < 55) colors[i] = magenta;
-		else if (i < 60) colors[i] = rose;
-		else colors[i] = black;
-	}
 	ws2811_set(colors, 60, 1);
-
-	//Main program loop
+	
 	while (1) {
+		a(p1, p2, x);
+		a(p2, p3, x);
+		a(p3, p1, x);
+
+		for (uint8_t i = 0; i < 60; i++) {
+			colors[i] = palette[x[i]];
+		}
+		ws2811_set(colors, 60, 1);
+		
+		p1 += 1;//(((int8_t) r(2)) - 1);
+		p2 += 1;//(((int8_t) r(2)) - 1);
+		//p3 += 1;//(((int8_t) r(2)) - 1);
+		if (p1 < 0) p1 = 59;
+		if (p2 < 0) p2 = 59;
+		if (p3 < 0) p3 = 59;
+		if (p1 > 59) p1 = 0;
+		if (p2 > 59) p2 = 0;
+		if (p3 > 59) p3 = 0;
+		
+		while (TCNT0 < 0xff) {
+			;
+		}
+		TCNT0 = 0;
 	}
 }
-
