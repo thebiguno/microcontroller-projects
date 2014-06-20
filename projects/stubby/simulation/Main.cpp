fb88fb674e2a8c08ceadc39f9f17d18a2762d564
Wyatt <wyatt@digitalcave.ca>
2014-06-20 13:01:10 -0600

    Getting really close... all legs are moving as mirrors of the ones opposite them (which is good), but there is still something wrong with the calculations for the front and back legs; they seem to move along the x axis instead of along the y axis.  They all move identically, so it is likely a software bug...

--- projects/stubby/simulation/Main.cpp
+++ projects/stubby/simulation/Main.cpp
@@ -6,20 +6,19 @@ int main (void){
 	//Set up the leg objects, including servo details and mounting angle
 	volatile uint8_t p = 0;
 	Leg legs[LEG_COUNT] = {
-		Leg(FRONT_LEFT,		&p, p, &p, p, &p, p, 2 * M_PI / 3),
-		Leg(FRONT_RIGHT,	&p, p, &p, p, &p, p, M_PI / 3),
-		Leg(MIDDLE_LEFT,	&p, p, &p, p, &p, p, 3 * M_PI / 3),
-		Leg(MIDDLE_RIGHT,	&p, p, &p, p, &p, p, 0),
-		Leg(REAR_LEFT,		&p, p, &p, p, &p, p, 4 * M_PI / 3),
-		Leg(REAR_RIGHT,		&p, p, &p, p, &p, p, 5 * M_PI / 3)
+		Leg(FRONT_LEFT,		&p, p, &p, p, &p, p, 2 * LEG_MOUNTING_ANGLE),
+		Leg(FRONT_RIGHT,	&p, p, &p, p, &p, p, 1 * LEG_MOUNTING_ANGLE),
+		Leg(MIDDLE_LEFT,	&p, p, &p, p, &p, p, 3 * LEG_MOUNTING_ANGLE),
+		Leg(MIDDLE_RIGHT,	&p, p, &p, p, &p, p, 0 * LEG_MOUNTING_ANGLE),
+		Leg(REAR_LEFT,		&p, p, &p, p, &p, p, 4 * LEG_MOUNTING_ANGLE),
+		Leg(REAR_RIGHT,		&p, p, &p, p, &p, p, 5 * LEG_MOUNTING_ANGLE)
 	};
 	
-	legs[FRONT_LEFT].setPosition(-84, 122, 0);
-	legs[FRONT_RIGHT].setPosition(84, 122, 0);
-	legs[MIDDLE_LEFT].setPosition(-140, 0, 0);
-	legs[MIDDLE_RIGHT].setPosition(140, 0, 0);
-	legs[REAR_LEFT].setPosition(-84, -122, 0);
-	legs[REAR_RIGHT].setPosition(84, -122, 0);
-	
+	legs[FRONT_LEFT].setPosition(-65, 130, 0);
+	legs[FRONT_LEFT].setPosition(-65, 120, 0);
+	legs[FRONT_LEFT].setPosition(-65, 110, 0);
+	legs[FRONT_LEFT].setPosition(-65, 100, 0);
+	legs[FRONT_LEFT].setPosition(-65, 90, 0);
+	legs[FRONT_LEFT].setPosition(-65, 80, 0);
 	return 0;
 }
