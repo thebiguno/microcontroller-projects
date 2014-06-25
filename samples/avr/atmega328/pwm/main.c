d397054bd53cd75141dd66c9ce76e59a8d824963
Wyatt <wyatt@digitalcave.ca>
2014-06-24 21:18:43 -0600

    updated sample again

--- samples/avr/atmega328/pwm/main.c
+++ samples/avr/atmega328/pwm/main.c
@@ -4,24 +4,30 @@
 
 int main (void){
 	//Do setup here
-	volatile uint8_t *ports[3];
+	volatile uint8_t *ports[4];
 	ports[0] = &PORTD;
 	ports[1] = &PORTC;
 	ports[2] = &PORTC;
+	ports[3] = &PORTC;
 	
-	uint8_t pins[3];
-	pins[0] = PORTD2;
-	pins[1] = PORTC4;
-	pins[2] = PORTC3;
+	uint8_t pins[4];
+	pins[0] = PORTD1;
+	pins[1] = PORTC5;
+	pins[2] = PORTC4;
+	pins[3] = PORTC3;
 
-	pwm_init(ports, pins, 3, 20000);
-	pwm_set_phase_batch(0, 2000);
-	pwm_set_phase_batch(1, 1000);
-	pwm_set_phase_batch(2, 10000);
-	pwm_apply_batch();
+	pwm_init(ports, pins, 4, 20000);
 
 	//Main program loop
-	DDRD = _BV(PORTD0);
+	DDRD |= _BV(PORTD0);
 	while (1){
+
+		pwm_set_phase_batch(0, 2000);
+		pwm_set_phase_batch(1, 1000);
+		pwm_set_phase_batch(2, 2000);
+		pwm_set_phase_batch(3, 3000);
+		pwm_apply_batch();
+
+		_delay_ms(100);
 	}
 }
