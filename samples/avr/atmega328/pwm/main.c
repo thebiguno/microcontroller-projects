95a83daaa943fbd8a40e6025b6f07b45d7bad986
Wyatt <wyatt@digitalcave.ca>
2014-06-24 21:03:15 -0600

    more work on ASM for PWM.  Still not working properly.

--- samples/avr/atmega328/pwm/main.c
+++ samples/avr/atmega328/pwm/main.c
@@ -5,19 +5,20 @@
 int main (void){
 	//Do setup here
 	volatile uint8_t *ports[3];
-	ports[0] = &PORTC;
+	ports[0] = &PORTD;
 	ports[1] = &PORTC;
 	ports[2] = &PORTC;
 	
 	uint8_t pins[3];
-	pins[0] = PORTC5;
+	pins[0] = PORTD2;
 	pins[1] = PORTC4;
 	pins[2] = PORTC3;
 
 	pwm_init(ports, pins, 3, 20000);
-	pwm_set_phase(0, 100);
-	pwm_set_phase(1, 1000);
-	pwm_set_phase(2, 10000);
+	pwm_set_phase_batch(0, 2000);
+	pwm_set_phase_batch(1, 1000);
+	pwm_set_phase_batch(2, 10000);
+	pwm_apply_batch();
 
 	//Main program loop
 	DDRD = _BV(PORTD0);
