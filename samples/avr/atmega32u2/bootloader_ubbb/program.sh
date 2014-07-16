d470dc94ee3a207139b41a790bffdd6f0c43bde7
Wyatt <wyatt@digitalcave.ca>
2014-07-15 21:31:14 -0600

    Program custom bootloader before changing fuses; apparently you can't change fuses on the stock DFU bootloader?

--- samples/avr/atmega32u2/bootloader_ubbb/program.sh
+++ samples/avr/atmega32u2/bootloader_ubbb/program.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-avrdude -F -V -c usbtiny -p atmega32u2 -U efuse:w:0xF4:m -U hfuse:w:0xD9:m -U lfuse:w:0xDE:m
 avrdude -V -F -p atmega32u2 -c usbtiny -U flash:w:BootloaderDFU.hex
+avrdude -F -V -c usbtiny -p atmega32u2 -U efuse:w:0xF4:m -U hfuse:w:0xD9:m -U lfuse:w:0xDE:m
+
