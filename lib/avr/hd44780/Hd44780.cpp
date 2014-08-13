7a0ae1dd99638f3f9c48792bdbacab1387a8af38
Warren Janssens <janssensw@richer.ca>
2014-08-13 16:13:08 -0600

    converting hd44780 to C++

--- lib/avr/hd44780/Hd44780.cpp
+++ lib/avr/hd44780/Hd44780.cpp
@@ -1,27 +1,32 @@
 /*
- * Driver for HD44780 in 4 bit mode using SPI and a shift register with no latch.
- * This driver requires only MOSI and SCK.
- * See http://www.circuitvalley.com/2011/12/two-wire-serial-lcd-16x2-graphics.html
+ * Driver for HD44780 in 8 bit mode using SPI and a shift register with or without a latch.
+ * If the shift register has a latch it must be connected with SCK and HD44780_LATCH must be defined.
+ * This driver uses the SPI hardware and requires MOSI and SCK as well as one other pin connected to E.
+ * MOSI and SCK may be shared with other SPI devices.  MOSI drives both the shift register and RS.  E functions as type of chip select.
+ * Q1~Q8 on the shift register connect to D1~D8 on the HD44780.
+ * MOSI on the AVR connects to DI on the shift register and RS on the HD44780.
+ * SCK on the AVR connects to CLK (and RCLK) on the shift register.
  */
 
 #include <avr/io.h>
 #include <util/delay_basic.h>
 
-void hd44780_init() {
-	
-	DDRB |= (1<<PB3) | (1<<PB5); // Set MOSI and SCK output
-	SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0); // enable, master, F_CPU/64
-	
-	hd44780_clear();
-}
+using namespace digitalcave;
 
+Hd44780::Hd44780(volatile uint8_t *e_port, uint8_t e_pin, volatile uint8_t *spi_port, uint8_t mosi_pin, uint8_t sclk_pin) {
+	this->e_port = e_port;
+	this->e_bv = _BV(e_pin);
+	this->spi_port = spi_port;
+	this->mosi_bv = _BV(mosi_pin);
+	this->sclk_bv = _BV(sclk_pin);
+}
 /* 
  * Clear all display data, and set DDRAM address to 0x00.
  * Return cursor to the left edge of the first line.
  * Make the entry mode 'increment' (I/D = 1).
  */
-void hd44780_clear() {
-	cmd(0x01);
+void Hd44780::clear() {
+	this->cmd(0x01);
 }
 
 /* 
@@ -30,8 +35,8 @@ void hd44780_clear() {
  * Return the display to its original status if shifted.
  * Contents of DDRAM are NOT changed.
  */
-void hd44780_home() {
-	cmd(0x02);
+void Hd44780::home() {
+	this->cmd(0x02);
 }
 
 /* 
@@ -39,8 +44,8 @@ void hd44780_home() {
  * Bit 0 (SH) is the display shift control; 0 = no shift, 1 = shift.
  * Bit 1 (I/D) is the increment/decrement control; 0 = left/decrement, 1 = right/increment.
  */
-void hd44780_set_mode(uint8_t b) {
-	cmd(b & 0x03 | 0x04);
+void Hd44780::setMode(uint8_t b) {
+	this->cmd(b & 0x03 | 0x04);
 }
 
 /* 
@@ -49,8 +54,8 @@ void hd44780_set_mode(uint8_t b) {
  * Bit 1 (C) is cursor on/off control; 0 = off, 1 = on.
  * Bit 2 (D) is display on/off control; 0 = off, 1 = on.
  */
-void hd44780_set_display(uint8_t b) {
-	cmd(b & 0x07 | 0x08);
+void Hd44780::setDisplay(uint8_t b) {
+	this->cmd(b & 0x07 | 0x08);
 }
 
 /* 
@@ -58,8 +63,8 @@ void hd44780_set_display(uint8_t b) {
  * Bit 2 (R/L) is left/right control; 0 = left, 1 = right.
  * Bit 3 (S/C) is screen/cursor control; 0 = cursor, 1 = screen.
  */
-void hd44780_set_shift(uint8_t b) {
-	cmd(b & 0x0f | 0x10);
+void Hd44780::setShift(uint8_t b) {
+	this->cmd(b & 0x0f | 0x10);
 }
 
 /* 
@@ -68,15 +73,15 @@ void hd44780_set_shift(uint8_t b) {
  * Bit 3 (N) is line number control; 0 = 1 line, 1 = 2 line.
  * Bit 4 (DL) is data length control; 0 = 4-bit, 1 = 8-bit.
  */
-void hd44780_set_function(uint8_t b) {
-	cmd(b & 0x1f | 0x10 | 0x20);
+void Hd44780::setFunction(uint8_t b) {
+	this->cmd(b & 0x1f | 0x10 | 0x20);
 }
 
 /*
  * Sets the address pointer for CGRAM, allowing read or write of a byte of CGRAM data.
  */
-void hd44780_set_cgram_address(uint8_t b) {
-	cmd(b & 0x3f | 0x40);
+void Hd44780::setCgramAddress(uint8_t b) {
+	this->cmd(b & 0x3f | 0x40);
 }
 
 /*
@@ -85,18 +90,28 @@ void hd44780_set_cgram_address(uint8_t b) {
  * For a 2 line display, the valid address range is 0x00 to 0x27 for line one and 0x40 to 0x67 for line 2.
  * (displays with more lines have individually addressable modules).
  */
-void hd44780_set_ddram_address(uint8_t address) {
-	cmd(b & 0x7f | 0x80);
+void Hd44780::setDdramAddress(uint8_t address) {
+	this->cmd(b & 0x7f | 0x80);
 }
+
+void rs0();
+void rs1();
+void latch();
 
 /*
  * Writes a byte of data to either DDRAM or CGRAM.
  * The address may be automatically incremented/decremented according to the entry mode.
  */
-void hd44780_set(uint8_t b) {
-	rs1();
+void Hd44780::setByte(uint8_t b) {
 	SPDR = b;
 	while(!(SPSR & (1<<SPIF)));
+	rs1();
+	latch();
+}
+void Hd44780::cmd(uint8_t b) {
+	SPDR = b;
+	while(!(SPSR & (1<<SPIF)));
+	rs0();
 	latch();
 }
 
@@ -105,8 +120,8 @@ void hd44780_set(uint8_t b) {
  * Bit 7 (BF) is the busy flag; 0 = not busy, 1 = busy.
  * Bits 0 to 7 (AC) are the DDRAM address.
  */
-uint8_t hd44780_get_busyflag_address() {
-	// this driver can't implement this since the data pins are connected to the shift register
+uint8_t Hd44780::getBusyFlagAddress() {
+	// can't implement this since the data pins are connected to the shift register
 	return 0;
 }
 
@@ -114,56 +129,37 @@ uint8_t hd44780_get_busyflag_address() {
  * Reads a byte of data to either DDRAM or CGRAM.
  * The address may be automatically incremented/decremented according to the entry mode.
  */
-uint8_t hd44780_get() {
-	// this driver can't implement this since the data pins are connected to the shift register
+uint8_t Hd44780::getByte() {
+	// can't implement this since the data pins are connected to the shift register
 	return 0;
 }
 
 /*
  * Writes sz bytes to either CGRAM or DDRAM.  The starting address must be set in advance and no bounds checking is done.
  */
-void hd44780_set_c(char* text, uint8_t sz) {
+void Hd44780::setText(char* text, uint8_t sz) {
 	for (uint8_t i = 0; i < sz; i++) {
-		hd44780_set(text[i]);
+		this->set(text[i]);
+		_delay_us(37);
 	}
 }
 /*
  * Writes sz bytes to either CGRAM or DDRAM.  The starting address must be set in advance and no bounds checking is done.
  */
-void hd44780_set_b(uint8_t* bytes, uint8_t sz) {
+void Hd44780::setBytes(uint8_t* bytes, uint8_t sz) {
 	for (uint8_t i = 0; i < sz; i++) {
-		hd44780_set(bytes[i]);
+		this->set(bytes[i]);
+		_delay_us(37);
 	}
 }
 
 inline void rs0() {
-	PORTB |= ~_BV(PB3); // set MOSI/RS low
+	this->spi_port &= ~this->mosi_bv;
 }
 inline void rs1() {
-	PORTB &= _BV(PB3); // set MOSI/RS high
+	this->spi_port |= this->mosi_bv;
 }
-inline void cmd(uint8_t b) {
-	rs0();
-	SPDR = b;
-	while(!(SPSR & (1<<SPIF)));
-	latch();
-}
-inline void latch() {
-	#ifdef HD44780_LATCH
-	// pulse SCK one more time latch the current data on the shift register
-	// this is required when using a shift register with a latch and CLK and RCLK are wired together
-	PORTB |= _BV(PB5);
-	PORTB &= ~_BV(PB5);
-	#endif
-
-	HD44780_E_PORT &= _BV(HD44780_E_PIN);
-	pulse();
-	HD44780_E_PORT |= ~_BV(HD44780_E_PIN);
-	pulse();
-	#endif
-}	
-
-inline void pulse() {
+inline void wait() {
 	#if F_CPU > 3333333
 	asm volatile("nop\n\t" 
 	#if F_CPU > 6666666
@@ -187,3 +183,18 @@ inline void pulse() {
 	::);
 	#endif
 }
+inline void latch() {
+	#ifdef HD44780_LATCH
+	// pulse SCK one more time latch the current data on the shift register
+	// this is required when using a shift register with a latch and CLK and RCLK are wired together
+	this->spi_port |= this->sclk_bv;
+	this->spi_port &= ~this->sclk_bv;
+	#endif
+
+	this->e_port |= this->e_bv;
+	wait();
+	this->e_port &= ~this->e_bv;
+	wait();
+	#endif
+}
+
--- lib/avr/hd44780/hd44780.h
+++ lib/avr/hd44780/hd44780.h
@@ -1,107 +1,122 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-void hd44780_init();
+using namespace digitalcave;
 
-/* 
- * Clear all display data, and set DDRAM address to 0x00.
- * Return cursor to the left edge of the first line.
- * Make the entry mode 'increment' (I/D = 1).
- * The device will remain busy for 1.52 ms.
- */
-void hd44780_clear();
+class Hd44780 {
+	
+	private:
+	volatile uint8_t *e_port;
+	volatile uint8_t e_bv;
+	volatile uint8_t *spi_port;
+	volatile uint8_t mosi_bv;
+	volatile uint8_t sclk_bv;
+	void cmd(uint8_t cmd);
 
-/* 
- * Set DDRAM address to 0x00.
- * Return cursor to the left edge of the first line.
- * Return the display to its original status if shifted.
- * Contents of DDRAM are NOT changed.
- * The device will remain busy for 1.52 ms.
- */
-void hd44780_home();
+	public:
+	/*
+	 * Creates a new HD44780 module in 3-wire mode.
+	 * The port and pin are that are connected to E on the module.
+	 * MOSI and SCK must be configured for output externally.
+	 * SPI must be configured as master and enabled externally.
+	 * MOSI must be connected to DI on the shift register and to RS on the HD44780 module.
+	 * SCK must be connected to SCK on the shift register.
+	 */
+	Hd44780(volatile uint8_t *port, uint8_t pin);
+	
+	// TODO add constructors for 2-wire and 7-wire operation
+	
+	/* 
+	 * Clear all display data, and set DDRAM address to 0x00.
+	 * Return cursor to the left edge of the first line.
+	 * Make the entry mode 'increment' (I/D = 1).
+	 * The device will remain busy for 1.52 ms.
+	 */
+	void clear();
 
-/* 
- * Set the moving direction of cursor and display.
- * Bit 0 (SH) is the display shift control; 0 = no shift, 1 = shift.
- * Bit 1 (I/D) is the increment/decrement control; 0 = left/decrement, 1 = right/increment.
- * The device will remain busy for 37 us.
- */
-void hd44780_set_mode(uint8_t mode);
+	/* 
+	 * Set DDRAM address to 0x00.
+	 * Return cursor to the left edge of the first line.
+	 * Return the display to its original status if shifted.
+	 * Contents of DDRAM are NOT changed.
+	 * The device will remain busy for 1.52 ms.
+	 */
+	void home();
 
-/* 
- * Set the moving direction of cursor and display.
- * Bit 0 (B) is cursor blink on/off control; 0 = off, 1 = on.
- * Bit 1 (C) is cursor on/off control; 0 = off, 1 = on.
- * Bit 2 (D) is display on/off control; 0 = off, 1 = on.
- * The device will remain busy for 37 us.
- */
-void hd44780_set_display(uint8_t display);
+	/* 
+	 * Set the moving direction of cursor and display.
+	 * Bit 0 (SH) is the display shift control; 0 = no shift, 1 = shift.
+	 * Bit 1 (I/D) is the increment/decrement control; 0 = left/decrement, 1 = right/increment.
+	 * The device will remain busy for 37 us.
+	 */
+	void setMode(uint8_t mode);
 
-/* 
- * Shifts the cursor or screen the left or right.
- * Bit 2 (R/L) is left/right control; 0 = left, 1 = right.
- * Bit 3 (S/C) is screen/cursor control; 0 = cursor, 1 = screen.
- * The device will remain busy for 37 us.
- */
-void hd44780_set_shift(uint8_t shift);
+	/* 
+	 * Set the moving direction of cursor and display.
+	 * Bit 0 (B) is cursor blink on/off control; 0 = off, 1 = on.
+	 * Bit 1 (C) is cursor on/off control; 0 = off, 1 = on.
+	 * Bit 2 (D) is display on/off control; 0 = off, 1 = on.
+	 * The device will remain busy for 37 us.
+	 */
+	void setDisplay(uint8_t display);
 
-/* 
- * Set the moving direction of cursor and display.
- * Bit 2 (F) is dot format control; 0 = 5x8, 1 = 5x11.
- * Bit 3 (N) is line number control; 0 = 1 line, 1 = 2 line.
- * Bit 4 (DL) is data length control; 0 = 4-bit, 1 = 8-bit.
- * The device will remain busy for 37 us.
- */
-void hd44780_set_function(uint8_t function);
+	/* 
+	 * Shifts the cursor or screen the left or right.
+	 * Bit 2 (R/L) is left/right control; 0 = left, 1 = right.
+	 * Bit 3 (S/C) is screen/cursor control; 0 = cursor, 1 = screen.
+	 * The device will remain busy for 37 us.
+	 */
+	void setShift(uint8_t shift);
 
-/*
- * Sets the address pointer for CGRAM, allowing read or write of a byte of CGRAM data.
- * Depending on the module, CGRAM is either 0x00 to 0x07 or 0x00 to 0x0f.
- * The device will remain busy for 37 us.
- */
-void hd44780_set_cgram_address(uint8_t address);
+	/* 
+	 * Set the moving direction of cursor and display.
+	 * Bit 2 (F) is dot format control; 0 = 5x8, 1 = 5x11.
+	 * Bit 3 (N) is line number control; 0 = 1 line, 1 = 2 line.
+	 * Bit 4 (DL) is data length control; 0 = 4-bit, 1 = 8-bit.
+	 * The device will remain busy for 37 us.
+	 */
+	void setFunction(uint8_t function);
 
-/*
- * Sets the address pointer for DDRAM, allowing read or write of a byte of DDRAM data.
- * The device will remain busy for 37 us.
- */
-void hd44780_set_ddram_address(uint8_t address);
+	/*
+	 * Sets the address pointer for CGRAM, allowing read or write of a byte of CGRAM data.
+	 * Depending on the module, CGRAM is either 0x00 to 0x07 or 0x00 to 0x0f.
+	 * The device will remain busy for 37 us.
+	 */
+	void setCgramAddress(uint8_t address);
 
-/*
- * Writes a byte of data to either DDRAM or CGRAM.
- * The address may be automatically incremented/decremented according to the entry mode.
- * The device will remain busy for 37 us.
- */
-void hd44780_set(uint8_t b);
+	/*
+	 * Sets the address pointer for DDRAM, allowing read or write of a byte of DDRAM data.
+	 * The device will remain busy for 37 us.
+	 */
+	void setDdramAddress(uint8_t address);
 
-/*
- * Reads the busy flag and DDRAM address.
- * Bit 7 (BF) is the busy flag; 0 = not busy, 1 = busy.
- * Bits 0 to 7 (AC) are the DDRAM address.
- */
-uint8_t hd44780_get_busyflag_address();
+	/*
+	 * Writes a byte of data to either DDRAM or CGRAM.
+	 * The address may be automatically incremented/decremented according to the entry mode.
+	 * The device will remain busy for 37 us.
+	 */
+	void setByte(uint8_t b);
 
-/*
- * Reads a byte of data to either DDRAM or CGRAM.
- * The address may be automatically incremented/decremented according to the entry mode.
- */
-uint8_t hd44780_get();
+	/*
+	 * Reads the busy flag and DDRAM address.
+	 * Bit 7 (BF) is the busy flag; 0 = not busy, 1 = busy.
+	 * Bits 0 to 7 (AC) are the DDRAM address.
+	 */
+	uint8_t getBusyFlagAddress();
 
-/*
- * Writes sz bytes to either CGRAM or DDRAM.  The starting address must be set in advance and no bounds checking is done.
- */
-void hd44780_set_c(char* text, uint8_t sz) {
-	for (uint8_t i = 0; i < sz; i++) {
-		hd44780_set(text[i]);
-		_delay_us(37);
-	}
-}
-/*
- * Writes sz bytes to either CGRAM or DDRAM.  The starting address must be set in advance and no bounds checking is done.
- */
-void hd44780_set_b(uint8_t* bytes, uint8_t sz) {
-	for (uint8_t i = 0; i < sz; i++) {
-		hd44780_set(bytes[i]);
-		_delay_us(37);
-	}
+	/*
+	 * Reads a byte of data to either DDRAM or CGRAM.
+	 * The address may be automatically incremented/decremented according to the entry mode.
+	 */
+	uint8_t getByte();
+
+	/*
+	 * Writes sz bytes to either CGRAM or DDRAM.  The starting address must be set in advance and no bounds checking is done.
+	 */
+	void setText(char* text, uint8_t sz);
+
+	/*
+	 * Writes sz bytes to either CGRAM or DDRAM.  The starting address must be set in advance and no bounds checking is done.
+	 */
+	void setBytes(uint8_t* bytes, uint8_t sz);
 }
