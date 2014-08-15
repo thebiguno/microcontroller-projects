d0934ce32179f1c1848e2f802d80103acfcbd6db
Warren Janssens <janssensw@richer.ca>
2014-08-15 13:58:35 -0600

    working on shift register library

--- lib/avr/ShiftRegister/ShiftRegister.cpp
+++ lib/avr/ShiftRegister/ShiftRegister.cpp
@@ -4,65 +4,47 @@
 
 #include "ShiftRegister.h"
 #include <stdlib.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
 
-
-// TODO make this conditional on the chip
-#define DD_SS DDB2
-#define DD_MOSI DDB3
-#define DD_SCK DDB5
-#define DDR_SPI DDRB
+using namespace digitalcave;
 
 // non-class variables for the ISR
 static volatile uint8_t _i;
+static volatile uint8_t *_bytes;
 static uint8_t _size;
-static volatile uint8_t *_buf;
 static volatile uint8_t *_latch_port;
-static uint8_t _latch_pin;
+static uint8_t _latch_bv;
 static void (*_callback)();
 static uint8_t _cts = 1;
 
-ShiftRegister::ShiftRegister(uint8_t size) {
-	_size = size - 1;
-	_buf = (uint8_t*) malloc(_size);
-	
-	// configure MOSI, SCK, SS as output pins
-	DDR_SPI |= _BV(DD_SCK) | _BV(DD_MOSI) | _BV(DD_SS);
-	
-	// setup SPI (enable, master, interrupts)
+ShiftRegister::ShiftRegister(volatile uint8_t *spi_port, uint8_t mosi_pin, uint8_t sclk_pin) {
+	this->spi_port = spi_port;
+	this->mosi_bv = _BV(mosi_pin);
+	this->sclk_bv = _BV(sclk_pin);
+}
+
+void ShiftRegister::initSpi() {
+	*(this->spi_port - 0x1) |= (this->mosi_bv | this->sclk_bv);
 	SPCR = _BV(SPE) | _BV(MSTR) | _BV(SPIE);
-
-	// clear SPI interrupt flag by reading SPSR and SPDR
-	// uint8_t x = SPSR;
-	// x = SPDR;
-	
-	// set SS high
-	PORTB |= _BV(DD_SS);
-
-	//Enable global interrupts if the NO_INTERRUPT_ENABLE define is not set.  If it is, you need to call sei() elsewhere.
-#ifndef NO_INTERRUPT_ENABLE
-	sei();
-#endif	
 }
 
 void ShiftRegister::setCallback(void (*callback)()) {
 	_callback = callback;
 }
 
-void ShiftRegister::setLatch(volatile uint8_t *port, uint8_t pin) {
+void ShiftRegister::initLatch(volatile uint8_t *port, uint8_t pin) {
 	_latch_port = port;
-	_latch_pin = pin;
+	_latch_bv = _BV(pin);
 	
 	if (_latch_port != 0) {
-		*(port - 0x1) |= _BV(pin);
-		*port |= _BV(pin); // set HIGH
+		*(port - 0x1) |= _latch_bv;
+		*port |= _latch_bv; // set HIGH
 	}
 }
 	
-void ShiftRegister::setEnable(volatile uint8_t *port, uint8_t pin) {
+void ShiftRegister::initEnable(volatile uint8_t *port, uint8_t pin) {
 	this->enable_port = port;
-	this->enable_pin = pin;
+	this->enable_bv = _BV(pin);
 	
 	if (this->enable_port != 0) {
 		*(port - 0x1) |= _BV(pin);
@@ -70,9 +52,9 @@ void ShiftRegister::setEnable(volatile uint8_t *port, uint8_t pin) {
 	}
 }
 	
-void ShiftRegister::setClear(volatile uint8_t *port, uint8_t pin) {
+void ShiftRegister::initClear(volatile uint8_t *port, uint8_t pin) {
 	this->clear_port = port;
-	this->clear_pin = pin;
+	this->clear_bv = _BV(pin);
 	
 	if (this->clear_port != 0) {
 		*(port - 0x1) |= _BV(pin);
@@ -80,16 +62,10 @@ void ShiftRegister::setClear(volatile uint8_t *port, uint8_t pin) {
 	}	
 }
 
-void ShiftRegister::shift(uint8_t b[]) {
+void ShiftRegister::shift(uint8_t b[], uint8_t sz) {
 	if (_cts) {
-		//SPCR |= _BV(MSTR); // re-enable master mode; disbled when SS is set low
-		_cts = 0;
-		
-		for (int i = 0; i < _size; i++) {
-			_buf[i] = b[i+1];
-		}
-	
-		_i = 0;
+		_bytes = b;
+		_i = 1;
 		SPDR = b[0];
 	}
 }
@@ -99,16 +75,16 @@ uint8_t ShiftRegister::cts() {
 }
 
 void ShiftRegister::clear() {
-	*this->clear_port &= ~_BV(this->clear_pin);
-	*this->clear_port |= _BV(this->clear_pin);	
+	*this->clear_port &= ~this->clear_bv;
+	*this->clear_port |= this->clear_bv;
 }
 
 void ShiftRegister::enable() {
-	*this->enable_port &= ~_BV(this->enable_pin);
+	*this->enable_port &= ~this->enable_bv;
 }
 
 void ShiftRegister::disable() {
-	*this->enable_port |= _BV(this->enable_pin);
+	*this->enable_port |= this->enable_bv;
 }
 
 /////////// interrupts ///////////////
@@ -116,12 +92,11 @@ void ShiftRegister::disable() {
 ISR(SPI_STC_vect) {
 	if (_i < _size) {
 		// copy next byte to the SPDR register
-		SPDR = _buf[_i++];
+		SPDR = _bytes[_i++];
 	} else if (_i == _size) {
 		if (_latch_port != 0) {
-			*_latch_port &= ~_BV(_latch_pin);
-			// _delay_us(5);
-			*_latch_port |= _BV(_latch_pin);
+			*_latch_port &= ~_latch_bv;
+			*_latch_port |= _latch_bv;
 		}
 		_cts = 1;
 		if (_callback != 0) {
--- samples/avr/atmega168/shiftcpp/main.cpp
+++ samples/avr/atmega168/shiftcpp/main.cpp
@@ -7,26 +7,25 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#include "lib/shiftcpp/shift.h"
+#include "lib/ShiftRegister/ShiftRegister.h"
 
+using namespace digitalcave;
+	
 int main (void){
 	//Do setup here
 	
 	// DDRC = _BV(PORTC5);
-	Shift s(3);
-	s.setLatch(&PORTB,PINB2);
-	
-	// &PORTD, PORTD2, &PORTD, PORTD3, &PORTD, PORTD4
+	ShiftRegister s(&PORTB, 3, 5);
+	s.initLatch(&PORTB,PINB2);
 	
 	uint8_t data[3];
 	data[0] = 0;
 	data[1] = 0;
 	data[2] = 0;
 	
-	
 	//Main program loop
 	while (1){
-		s.shift(data);
+		s.shift(data, 3);
 		_delay_ms(500);
 		data[2]++;
 	}
