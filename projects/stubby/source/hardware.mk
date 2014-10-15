a0a91398df3129011ef67116afca754979f4e89f
Wyatt <wyatt@digitalcave.ca>
2014-10-15 08:22:21 -0600

    Updated hardware.mk with proper default values

--- projects/stubby/source/hardware.mk
+++ projects/stubby/source/hardware.mk
@@ -17,8 +17,10 @@
 
 #Manually set the F_CPU speed.  Only do this if you are using a non-standard value
 # (i.e. something other than 20MHz for Rev2 boards, or 12MHz for Rev1 boards).
+#Defaults to the nominal value for the specified board revision.
 #F_CPU=20000000
 
 #Manually set the TWI_FREQ speed.  Only do this if you are having problems with the default
 # (most likely happening if you don't have high enough pull-ups on SDA / SCL).
+#Defaults to 400000L
 #TWI_FREQ=400000L
