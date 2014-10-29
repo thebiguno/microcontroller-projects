1e1ef9ebb9fc87a2d7fcfd37259a51a018a2f54e
Wyatt <wyatt@digitalcave.ca>
2014-10-29 08:19:44 -0600

    Added a semi-working python pyrawhid sample.
    Writing works (and can flash the UBBB's light on and off), but reading
    does not work for some reason (the returned value is 0 length).  Writing
    is also a bit strange as I can't figure out which byte ends up as the
    first byte in the UBBB's buffer.  Still, getting close enough to warrant
    a commit.

---
+++ samples/python/usb/usb_rawhid.py
@@ -0,0 +1,31 @@
+# Sample program demonstrating USB Raw HID access using the pyhidapi library
+# (a ctypes wrapper around hidapi).  Sends alternating arrays containing
+# 0xFF and 0x00, and prints the byte values of any recieved data.
+#
+# Designed to interface with usb_rawhid sample on the atmega32u2
+#############################################################################
+import hid, traceback, time
+
+print(hid.enumerate())
+
+dev = hid.Device(vid=5824, pid=1152)
+try:
+	dev.nonblocking = 0
+	print(dev.manufacturer)
+	print(dev.product)
+	print(dev.serial)
+	b = '\x00'
+
+	while True:
+		print("Write: " + str(dev.write(b * 64)))
+		if b == '\xFF':
+			b = '\x00'
+		else:
+			b = '\xFF'
+		arr = dev.read(64)
+		print("Read: " + str(len(arr)))
+		print(''.join([chr(x) for x in arr]) + "\n")
+		time.sleep(1)
+except Exception, e:
+	traceback.print_exc()
+dev.close()
