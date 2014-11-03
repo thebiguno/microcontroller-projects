1b423f9a5eceff66f6cb9786d302a17258ffa1c9
Wyatt <wyatt@digitalcave.ca>
2014-11-03 13:26:53 -0700

    Resource monitor AVR program now accepts a 32 byte raw hid report, and prints the raw bytes directly to the LCD buffer.  Includes support for custom characters 0 - 7 to show bar graphs (use space and 0xFF to show the 0% and 100% options).

--- projects/resource_monitor/avr/main.c
+++ projects/resource_monitor/avr/main.c
@@ -1,19 +1,14 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#include "usb_rawhid.h"
 #include "lib/Hd44780/Hd44780_Direct.h"
-
-//#define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
+#include "lib/usb/rawhid/usb_rawhid.h"
 
 using namespace digitalcave;
 
 uint8_t rx_buffer[64];
 
 int main (void){
-	// set for 16 MHz clock
-//	CPU_PRESCALE(0);
-
 	// Initialize the USB, and then wait for the host to set configuration.
 	// If the AVR is powered without a PC connected to the USB port,
 	// this will wait forever.
@@ -26,36 +21,30 @@ int main (void){
 	
 	Hd44780_Direct display(display.FUNCTION_LINE_2 | display.FUNCTION_SIZE_5x8);
 
-	uint8_t c1[8] = {31,31,0,0,0,0,31,31};
-	uint8_t c2[8] = {0,0,0,0,0,0,31,31};
-	uint8_t c3[8] = {31,31,0,0,0,0,0,0};
+	uint8_t custom[64] = {
+		0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,			// 0 (Use 0xFE)
+		0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x1f,			// 1
+		0x0,0x0,0x0,0x0,0x0,0x0,0x1f,0x1f,			// 2
+		0x0,0x0,0x0,0x0,0x0,0x1f,0x1f,0x1f,			// 3
+		0x0,0x0,0x0,0x0,0x1f,0x1f,0x1f,0x1f,		// 4
+		0x0,0x0,0x0,0x1f,0x1f,0x1f,0x1f,0x1f,		// 5
+		0x0,0x0,0x1f,0x1f,0x1f,0x1f,0x1f,0x1f,		// 6
+		0x0,0x1f,0x1f,0x1f,0x1f,0x1f,0x1f,0x1f		// 7
+													// 8 (Use 0xFF)
+	};
 	display.setCgramAddress(0x00);
 	_delay_ms(64);
-	display.setBytes(c1, 8);
-	display.setBytes(c2, 8);
-	display.setBytes(c3, 8);
+	display.setBytes(custom, 64);
 
 	while (1) {
 		// if received data, do something with it
-		if (usb_rawhid_recv(rx_buffer, 0) == 64) {
+		if (usb_rawhid_recv(rx_buffer, 0) >= 32) {
 			display.setDdramAddress(0x00);
-			_delay_ms(64);        //"1234567890123456789012345678901234567890"
-			display.setByte(0xff);
-			_delay_ms(64);
-			display.setByte(0x00);
-			_delay_ms(64);
-			display.setByte(0x00);
 			_delay_ms(64);
 			display.setText((char*) &rx_buffer[0], 16);
 			display.setDdramAddress(0x40);
-			_delay_ms(64);
-			display.setByte(0xff);
-			_delay_ms(64);
-			display.setByte(0x01);
-			_delay_ms(64);
-			display.setByte(0xff);
-			_delay_ms(64);
 			display.setText((char*) &rx_buffer[16], 16);
+
 		}
 	}
 }
