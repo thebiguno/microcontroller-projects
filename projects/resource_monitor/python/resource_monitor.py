a6cffc127c51ab84ba9b901a26a9d8ac33d74a07
Wyatt <wyatt@digitalcave.ca>
2014-11-02 20:46:53 -0700

    resource monitor WIP

--- projects/resource_monitor/python/resource_monitor.py
+++ projects/resource_monitor/python/resource_monitor.py
@@ -3,9 +3,6 @@
 # Uses psutil to monitor system resources, and updates the data over USB using raw HID packets
 # to the hardware monitor.
 #
-# All packets are sent within 64 bytes.  The first byte is the type of packet; the remaining
-# 63 bytes are packet dependent.  All unused bytes in a packet are set to zero.
-#
 ########################################################
 
 import psutil
@@ -14,8 +11,8 @@ import sys
 from time import sleep
 from collections import namedtuple
 
-line1 = ['c','9','9','%',' ','w','9','9','9','k',' ','u','9','9','9','k']
-line2 = ['m','9','9','%',' ','r','9','9','9','k',' ','d','9','9','9','k']
+line1 = ['c','X','m','X',' ','D','r','X','w','X',' ','N','d','X','u','X']
+line2 = ['/','X',' ','H','X',' ','D','X',' ','B','X',' ','S','X',' ',' ']
 
 def main():
 	while True:
@@ -31,15 +28,11 @@ def printData():
 	sys.stdout.flush()
 
 def updateCpuUsage():
-	cpu = int(psutil.cpu_percent())
-	line1[1] = str(cpu / 10)
-	line1[2] = str(cpu % 10)
+	line1[1] = str(int(psutil.cpu_percent() / 12))
 	return
 
 def updateMemoryUsage():
-	mem = int(psutil.virtual_memory().percent)
-	line2[1] = str(mem / 10)
-	line2[2] = str(mem % 10)
+	line1[3] = str(int(psutil.virtual_memory().percent / 12))
 	return
 
 def updateNetworkUsage():
@@ -48,13 +41,13 @@ def updateNetworkUsage():
 	if not hasattr(updateNetworkUsage, 'last'):
 		updateNetworkUsage.last = {"sent": current.bytes_sent, "recv": current.bytes_recv}
 
-	sent = formatBytes(current.bytes_sent - updateNetworkUsage.last["sent"]).rjust(4)
-	recv = formatBytes(current.bytes_recv - updateNetworkUsage.last["recv"]).rjust(4)
+	sent = current.bytes_sent - updateNetworkUsage.last["sent"]
+	recv = current.bytes_recv - updateNetworkUsage.last["recv"]
+
 	updateNetworkUsage.last = {"sent": current.bytes_sent, "recv": current.bytes_recv}
 
-	for x in range(4):
-		line1[x + 12] = sent[x]
-		line2[x + 12] = recv[x]
+	line1[13] = str(len(str(recv)))
+	line1[15] = str(len(str(sent)))
 
 	return
 
@@ -64,32 +57,21 @@ def updateDiskUsage():
 	if not hasattr(updateDiskUsage, 'last'):
 		updateDiskUsage.last = {"read": current.read_bytes, "write": current.write_bytes}
 
-	read = formatBytes(current.read_bytes - updateDiskUsage.last["read"]).rjust(4)
-	write = formatBytes(current.write_bytes - updateDiskUsage.last["write"]).rjust(4)
+	read = current.read_bytes - updateDiskUsage.last["read"]
+	write = current.write_bytes - updateDiskUsage.last["write"]
+
 	updateDiskUsage.last = {"read": current.read_bytes, "write": current.write_bytes}
 
-	for x in range(4):
-		line1[x + 6] = write[x]
-		line2[x + 6] = read[x]
+	line1[7] = str(len(str(read)))
+	line1[9] = str(len(str(write)))
+	
+	line2[1] = str(int(psutil.disk_usage("/").percent / 12))
+	line2[4] = str(int(psutil.disk_usage("/home").percent / 12))
+	line2[7] = str(int(psutil.disk_usage("/mnt/data").percent / 12))
+	line2[10] = str(int(psutil.disk_usage("/mnt/backup").percent / 12))
+	line2[13] = str(int(psutil.swap_memory().percent / 12))
 
 	return
-
-def updatePacket(packet):
-	while len(packet) < 64:
-		packet.append(0)
-	print(packet)
-
-def formatBytes(n):
-	format="%(value)i%(symbol)s"
-	symbols = ('B', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
-	prefix = {}
-	for i, s in enumerate(symbols[1:]):
-		prefix[s] = 1 << (i+1)*10
-	for symbol in reversed(symbols[1:]):
-		if n >= prefix[symbol]:
-			value = float(n) / prefix[symbol]
-			return format % locals()
-	return format % dict(symbol=symbols[0], value=n)
 
 ########## Main startup hooks here ##########
 if (__name__=="__main__"):
