fa296dcf1c1a39d3a6496fb51024c0c248d1e716
Wyatt <wyatt@digitalcave.ca>
2015-01-10 13:31:33 -0700

    Changed chip to u4

--- samples/avr/atmega32u4/bootloader_ubbb32u4/program.sh
+++ samples/avr/atmega32u4/bootloader_ubbb32u4/program.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-avrdude -V -p atmega32u2 -c usbtiny -e
-avrdude -V -p atmega32u2 -c usbtiny -U flash:w:BootloaderDFU.hex
-avrdude -V -p atmega32u2 -c usbtiny -U efuse:w:0xF3:m -U hfuse:w:0xD9:m -U lfuse:w:0xDE:m
+avrdude -V -p atmega32u4 -c usbtiny -e
+avrdude -V -p atmega32u4 -c usbtiny -U flash:w:BootloaderDFU.hex
+avrdude -V -p atmega32u4 -c usbtiny -U efuse:w:0xF3:m -U hfuse:w:0xD9:m -U lfuse:w:0xDE:m
 
