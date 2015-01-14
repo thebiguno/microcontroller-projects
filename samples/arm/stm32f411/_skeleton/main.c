0074cf800bdd5169378e71a732ffa57b7d7c0f30
Wyatt <wyatt@digitalcave.ca>
2015-01-13 19:39:16 -0700

    More WIP; not yet working

--- samples/arm/stm32f411/_skeleton/main.c
+++ samples/arm/stm32f411/_skeleton/main.c
@@ -19,22 +19,31 @@
 #include <stm32f4xx.h>
  
 #define LED_PIN 5
-#define LED_ON() GPIOA->BSRRL |= (1 << 5)
-#define LED_OFF() GPIOA->BSRRH |= (1 << 5)
+#define LED_ON() GPIOA->BSRRL |= (1 << LED_PIN)
+#define LED_OFF() GPIOA->BSRRH |= (1 << LED_PIN)
  
 int main() {
-	/* Enbale GPIOA clock */
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	/* Configure GPIOA pin 5 as output */
-	GPIOA->MODER |= (1 << (LED_PIN << 1));
-	/* Configure GPIOA pin 5 in max speed */
-	GPIOA->OSPEEDR |= (3 << (LED_PIN << 1));
- 
-	/* Turn on the LED */
-	while(1){
-		LED_ON();
-		LED_OFF();
-	}
 	
-	return 0;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;	// Enable GPIOA clock
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;		// enable TIM2 clock
+	
+	GPIOA->MODER |= (1 << (LED_PIN << 1)); //Configure GPIOA pin 5 as output
+	
+    NVIC->ISER[0] |= 1<< (TIM2_IRQn); // enable the TIM2 IRQ
+
+	//Timer init stuff
+	TIM2->PSC = 0x0; // no prescaler, timer counts up in sync with the peripheral clock
+	TIM2->DIER |= TIM_DIER_UIE; // enable update interrupt
+	TIM2->ARR = 0x01; // count to 1 (autoreload value 1)
+	TIM2->CR1 |= TIM_CR1_ARPE | TIM_CR1_CEN; // autoreload on, counter enabled
+	TIM2->EGR = 1; // trigger update event to reload timer registers
+
+    while (1);
+}
+
+void TIM2_IRQHandler(void) {
+	// flash on update event
+	if (TIM2->SR & TIM_SR_UIF) GPIOA->ODR ^= (1 << LED_PIN);
+
+	TIM2->SR = 0x0; // reset the status register
 }
