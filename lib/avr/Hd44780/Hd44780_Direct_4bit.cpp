e3e542f03fe564fb9b002d66995b3e86892e7ad7
Wyatt <wyatt@digitalcave.ca>
2015-01-14 21:34:56 -0700

    Got 4 bit direct drive mode working on hd44780

--- lib/avr/Hd44780/Hd44780_Direct_4bit.cpp
+++ lib/avr/Hd44780/Hd44780_Direct_4bit.cpp
@@ -11,50 +11,71 @@
 
 using namespace digitalcave;
 
+static void write_nibble(uint8_t b, uint8_t mode);
+
 Hd44780_Direct::Hd44780_Direct(uint8_t function) {
 	DDRD = 0xFF;
 	DDRB |= _BV(PORTB0) | _BV(PORTB1) | _BV(PORTB2) | _BV(PORTB3) | _BV(PORTB4) | _BV(PORTB5);
 	
+	_delay_ms(100);
+	write_nibble(0x03, 0);
+	_delay_ms(10);
+	write_nibble(0x03, 0);
+	_delay_ms(1);
+	write_nibble(0x03, 0);
+	_delay_ms(1);
+	write_nibble(0x02, 0);
+	_delay_ms(1);
 	
-	
-	this->cmd((function & 0x0c) | 0x20);	//4 bit, plus function (char size + lines)
-	_delay_us(0x3f);
-	this->cmd((function & 0x0c) | 0x20);	//The second time (need this for 4 bit mode)
-	_delay_us(0x3f);
+	this->cmd((function & 0x0c) | 0x20);	//Function set (char size + lines)
+	_delay_us(60);
 
+	this->cmd(0x08);	//Display off
+	_delay_us(60);
+	this->cmd(0x01);	//Clear display
+	_delay_ms(5);
+	this->cmd(0x04);	//Entry mode set
+	_delay_ms(60);
 	this->cmd(0x0C);	//Display on, cursor off, cursor blink off
 	this->clear();		//Clear display
 }
 
-static void write(uint8_t b, uint8_t mode){
+/*
+ * Writes the nibble.  Only the bottom 4 bits of b are used.
+ */
+static void write_nibble(uint8_t b, uint8_t mode){
 	if (mode)
 		PORTB |= _BV(PORTB0);	//Set RS high for data
 	else 
 		PORTB &= ~_BV(PORTB0);	//Set RS low for command
-
-	PORTB &= 0xC3;		//Set bits 2..5 low
-	PORTB |= (b & 0x0F) << 2;
 	
+	//Set data lines
+	PORTB &= 0xC3;		//Set bits 2..5 low
+	PORTB |= ((b & 0x0F) << 2);	//Set bits 2..5 to specified nibble
+
+	//Toggle E
 	_delay_us(64);
-	PORTB |= _BV(PORTB2);	//Set E high to clock in data
+	PORTB |= _BV(PORTB1);	//Set E high to clock in data
 	_delay_us(450);
-	PORTB &= ~_BV(PORTB2);	//Re-set E back low
+	PORTB &= ~_BV(PORTB1);	//Re-set E back low
 	_delay_us(40);
 	
-	PORTB &= 0xC3;		//Set bits 2..5 low
-	PORTB |= (b & 0xF0) >> 2;
-	
-	_delay_us(64);
-	PORTB |= _BV(PORTB2);	//Set E high to clock in data
-	_delay_us(450);
-	PORTB &= ~_BV(PORTB2);	//Re-set E back low
-	_delay_us(40);
 
+}
+
+/*
+ * Writes the entire byte, MSB nibble first, then LSB nibble
+ */
+static void write_byte(uint8_t b, uint8_t mode){
+	//Write the MSB nibble of the byte first...
+	write_nibble(((b & 0xF0) >> 4), mode);
+	//... then LSB nibble
+	write_nibble((b & 0x0F), mode);
 }
 
 void Hd44780_Direct::setByte(uint8_t b) {
-	write(b, 1);
+	write_byte(b, 1);
 }
 void Hd44780_Direct::cmd(uint8_t b) {
-	write(b, 0);
+	write_byte(b, 0);
 }
