427543d37cf1089ef2cd59a1de0b52a6c2c860cd
Warren Janssens <janssensw@richer.ca>
2015-02-01 19:36:22 -0700

    working on the layout of the tuning application

--- projects/cranky/java/src/cranky/Frame.java
+++ projects/cranky/java/src/cranky/Frame.java
@@ -1,13 +1,38 @@
 package cranky;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.GridLayout;
-import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
 
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.JToolBar;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import com.codeminders.hidapi.ClassPathLibraryLoader;
+import com.codeminders.hidapi.HIDDevice;
+import com.codeminders.hidapi.HIDDeviceNotFoundException;
+import com.codeminders.hidapi.HIDManager;
+import com.jgoodies.binding.value.ValueHolder;
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.LayoutMap;
 
 import eu.hansolo.steelseries.gauges.Radial;
 import eu.hansolo.steelseries.tools.BackgroundColor;
@@ -25,12 +50,46 @@ public class Frame extends JFrame {
 	private static final Color ORANGE = new Color(204,122,41);
 	private static final Color AZURE = new Color(41,122,204);
 
+	private final SpinnerNumberModel[][] sparkAdvance = new SpinnerNumberModel[8][8];
+	private final SpinnerNumberModel[][] injectorDuration = new SpinnerNumberModel[8][8];
+	private final JSpinner[][] spinners = new JSpinner[8][8];
+	private final SpinnerNumberModel[] loadZones = new SpinnerNumberModel[8];
+	private final SpinnerNumberModel[] rpmZones = new SpinnerNumberModel[8];
+	private final ValueHolder rpmZone = new ValueHolder();
+	private final ValueHolder loadZone = new ValueHolder();
+	private final HIDDevice[] device = new HIDDevice[2];
+
+	// simulator
+	final SpinnerNumberModel rpmModel = new SpinnerNumberModel(300, 300, 10000, 10);
+	final SpinnerNumberModel tpModel = new SpinnerNumberModel(0, 0, 100, 10);
+
+
 	public Frame() {
+		LayoutMap.getRoot().columnPut("label", "l:m");
+		
+		ClassPathLibraryLoader.loadNativeHIDLibrary();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Cranky");
-		setSize(1000, 500);
+		setSize(640, 800);
 		setBackground(BLACK);
 		
+		final JToolBar toolbar = new JToolBar();
+		toolbar.setFloatable(false);
+		toolbar.add(connectEcmAction);
+		toolbar.add(connectSimulatorAction);
+		
+		final JPanel panel = new JPanel(new BorderLayout());
+		panel.add(toolbar, BorderLayout.PAGE_START);
+		panel.add(layoutTuning(), BorderLayout.PAGE_END);
+		panel.add(layoutGuages(), BorderLayout.CENTER);
+		panel.add(layoutSimulator(), BorderLayout.LINE_END);
+
+		getContentPane().add(panel);
+		setLocationByPlatform(true);
+	}
+	
+	private JPanel layoutGuages() {
 		final Radial rpm = new Radial();
 		rpm.setTitle("Engine Speed");
 		rpm.setUnitString("RPM x 1000");
@@ -47,50 +106,6 @@ public class Frame extends JFrame {
 		tp.setSectionsVisible(true);
 		customize(tp);
 		
-		final Radial map = new Radial();
-		map.setTitle("MAP");
-		map.setUnitString("kPa");
-		map.setMaxValue(300);
-		map.setSections(new Section(200,250,ORANGE), new Section(250,300,RED));
-		map.setSectionsVisible(true);
-		customize(map);
-		
-		final Radial batt = new Radial();
-		batt.setTitle("Battery");
-		batt.setUnitString("V");
-		batt.setMinValue(8);
-		batt.setMaxValue(16);
-		batt.setSections(new Section(8,10,RED), new Section(14, 16, RED));
-		batt.setSectionsVisible(true);
-		customize(batt);
-		
-		final Radial mat = new Radial();
-		mat.setTitle("Manifold Air");
-		mat.setUnitString("\u2103");
-		mat.setMinValue(50);
-		mat.setMaxValue(130);
-		mat.setSections(new Section(100,120,ORANGE), new Section(120,130,RED));
-		mat.setSectionsVisible(true);
-		customize(mat);
-
-		final Radial clt = new Radial();
-		clt.setTitle("Coolant");
-		clt.setUnitString("\u2103");
-		clt.setMinValue(50);
-		clt.setMaxValue(130);
-		clt.setSections(new Section(100,120,ORANGE), new Section(120,130,RED));
-		clt.setSectionsVisible(true);
-		customize(clt);
-		
-		final Radial afr = new Radial();
-		afr.setTitle("Air/Fuel");
-		afr.setUnitString("1:1");
-		afr.setMinValue(10);
-		afr.setMaxValue(18);
-		afr.setSections(new Section(10,13,ORANGE), new Section(15,18,ORANGE));
-		afr.setSectionsVisible(true);
-		customize(afr);
-		
 		final Radial dwell = new Radial();
 		dwell.setTitle("Dwell");
 		dwell.setUnitString("ms");
@@ -106,27 +121,127 @@ public class Frame extends JFrame {
 		adv.setMaxValue(50);
 		customize(adv);
 		
-		final JPanel guages = new JPanel(new GridLayout(2, 4, 5, 5));
-		guages.setBackground(BLACK);
-		guages.add(rpm);
-		guages.add(tp);
-		guages.add(batt);
-		guages.add(afr);
-		guages.add(map);
-		guages.add(mat);
-		guages.add(clt);
-		guages.add(dwell);
-		guages.add(adv);
+		final JPanel result = new JPanel(new GridLayout(2, 4, 5, 5));
+		result.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+//		result.setBackground(BLACK);
+		result.add(rpm);
+		result.add(tp);
+		result.add(dwell);
+		result.add(adv);
 
-		final JPanel advance = new JPanel(new GridLayout(8,8,5,5));
-		for (int i = 0; i < 64; i++) {
-			advance.add(new JTextField());
+		return result;
+	}
+	
+	private JPanel layoutSimulator() {
+		final byte[] bytes = new byte[3];
+		
+		final ChangeListener listener = new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent evt) {
+				if (device[1] != null) {
+					try {
+						final int rpm = (Integer) rpmModel.getValue();
+						final int tp = (Integer) tpModel.getValue();
+						
+						// first byte is OCR0A; throttle position
+						final byte ocr0a = (byte) (2.55f * tp);
+						
+						// second and third bytes are OCR1AH and OCR1AL; RPM
+						// 500 rpm / 60 = 8.3 Hz = 120 ms / 72 = 1666 us/event = timer1 value of 3332 (120 ms period)
+						// 10000 rpm / 60 = 166.6 Hz = 6 ms / 72 = 83 us/event = timer1 value of 166 (6 ms period)
+						final double hz = (double) rpm / 60.0d;
+						final double ms = 1.0d / hz;
+						final double s = ms * 1000000;
+						final int ocr1a = (int) s / 36;
+						final byte ocr1al = (byte) ocr1a;
+						final byte ocr1ah = (byte) (ocr1a >> 8);
+						
+						bytes[0] = ocr0a;
+						bytes[1] = ocr1ah;
+						bytes[2] = ocr1al;
+						device[1].write(bytes);
+					} catch (IOException e) {
+						JOptionPane.showMessageDialog(getContentPane(), e.getMessage());
+					}
+				}
+			}
+		};
+		
+		rpmModel.addChangeListener(listener);
+		tpModel.addChangeListener(listener);
+		
+		final DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout("$label, $lcg, f:p:g"));
+		builder.append("RPM", new JSpinner(rpmModel));
+		builder.append("Throttle", new JSpinner(tpModel));
+		
+		return builder.getPanel();
+	}
+	
+	private JPanel layoutTuning() {
+		final JPanel grid = new JPanel(new GridLayout(9,9,5,5));
+		
+		grid.add(new JLabel("TP%\\RPM"));
+		
+		for (int i = 0; i < 8; i++) {
+			final SpinnerNumberModel rpmZone = new SpinnerNumberModel(300, 300, 9000, 10);
+			rpmZones[i] = rpmZone;
+			grid.add(new JSpinner(rpmZone));
 		}
 		
-		getContentPane().add(guages);
-		setLocationByPlatform(true);
-	}
+		for (int i = 0; i < 8; i++) {
+			final SpinnerNumberModel loadZone = new SpinnerNumberModel(0, 0, 100, 10);
+			loadZones[i] = loadZone;
+			grid.add(new JSpinner(loadZone));
+			
+			for (int j = 0; j < 8; j++) {
+				final SpinnerNumberModel advance = new SpinnerNumberModel(0, 0, 59, 1);
+				final SpinnerNumberModel duration = new SpinnerNumberModel(0, 0, 59, 1);
+				final JSpinner spinner = new JSpinner(advance);
+				sparkAdvance[i][j] = advance;
+				injectorDuration[i][j] = duration;
+				spinners[i][j] = spinner;
+				grid.add(spinner);
+			}
+		}
+		
+		final PropertyChangeListener l = new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				for (int i = 0; i < 8; i++) {
+					for (int j = 0; j < 8; j++) {
+						spinners[i][j].setBackground(Color.WHITE);
+					}
+				}
+				int i = (Integer) rpmZone.getValue();
+				int j = (Integer) loadZone.getValue();
+				spinners[i][j].setBackground(ORANGE);
+			}
+		};
+		rpmZone.addValueChangeListener(l);
+		loadZone.addValueChangeListener(l);
+		
+		final JComboBox<String> display = new JComboBox<String>(new String[] { "advance", "duration" });
+		display.setRenderer(new DefaultListCellRenderer() {
+			private static final long serialVersionUID = 1L;
 
+			@Override
+			public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+				final JLabel result = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+				if ("advance".equals(value)) {
+					result.setText("Spark Advance (degrees)");
+				} else if ("duration".equals(value)) {
+					result.setText("Ignition Duration (ms)");
+				}
+				return result;
+			}
+		});
+
+		final JPanel result = new JPanel(new BorderLayout());
+		result.add(grid, BorderLayout.CENTER);
+		result.add(display, BorderLayout.PAGE_END);
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -143,6 +258,56 @@ public class Frame extends JFrame {
 		r.setPointerType(PointerType.TYPE2);
 		r.setKnobType(KnobType.BIG_STD_KNOB);
 		r.setKnobStyle(KnobStyle.BLACK);
-		r.setFrameVisible(false);
+		r.setFrameVisible(true);
 	}
+	
+	private AbstractAction connectEcmAction = new AbstractAction() {
+		private static final long serialVersionUID = 1L;
+		{
+			putValue(NAME, "Connect ECM");
+		}
+		@Override
+		public void actionPerformed(ActionEvent evt) {
+			try {
+				if (device[0] == null) {
+					device[0] = HIDManager.getInstance().openById(0x574a, 0x6372, null);
+					putValue(NAME, "Disconnect ECM");
+				} else {
+					device[0].close();
+					putValue(NAME, "Connect ECM");
+				}
+			} catch (HIDDeviceNotFoundException e) {
+				JOptionPane.showMessageDialog(getContentPane(), "ECM not found");
+			} catch (IOException e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(getContentPane(), e.getMessage());
+			}
+		}
+	};
+	
+	private AbstractAction connectSimulatorAction = new AbstractAction() {
+		private static final long serialVersionUID = 1L;
+		{
+			putValue(NAME, "Connect Simulator");
+		}
+		@Override
+		public void actionPerformed(ActionEvent evt) {
+			try {
+				if (device[1] == null) {
+					device[1] = HIDManager.getInstance().openById(0x574a, 0x6373, null);
+					rpmModel.setValue(300);
+					tpModel.setValue(0);
+					putValue(NAME, "Disconnect Simulator");
+				} else {
+					device[1].close();
+					putValue(NAME, "Connect Simulator");
+				}
+			} catch (HIDDeviceNotFoundException e) {
+				JOptionPane.showMessageDialog(getContentPane(), "Simulator not found");
+			} catch (IOException e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(getContentPane(), e.getMessage());
+			}
+		}
+	};
 }
