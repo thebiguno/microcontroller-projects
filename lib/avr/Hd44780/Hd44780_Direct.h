4cc7c219ab2ea79a18fa7bb163f909d57b54a6bb
Wyatt <wyatt@digitalcave.ca>
2015-02-16 10:16:05 -0700

    Renaming Hd44780_Direct_4bit to Hd44780_Direct since there is no longer an 8 bit direct version

--- lib/avr/Hd44780/Hd44780_Direct.h
+++ lib/avr/Hd44780/Hd44780_Direct.h
@@ -1,5 +1,12 @@
-#ifndef HD44780_DIRECT_H
-#define HD44780_DIRECT_H
+/*
+ * Driver for HD44780 in 4 bit mode using direct pin access.
+ * Defaults to use PORTB0 for RS, and PORTB1 for E,PORTB2..5 
+ * for data pins.  Use the alternate constructor to define
+ * each pin individually.
+ */
+
+#ifndef HD44780_DIRECT_4BIT_H
+#define HD44780_DIRECT_4BIT_H
 
 #include <avr/io.h>
 #include <stdlib.h>
@@ -14,14 +21,6 @@ namespace digitalcave {
 			uint8_t pin_rs;
 			volatile uint8_t *port_e; 
 			uint8_t pin_e;
-			volatile uint8_t *port_d0; 
-			uint8_t pin_d0;
-			volatile uint8_t *port_d1; 
-			uint8_t pin_d1;
-			volatile uint8_t *port_d2; 
-			uint8_t pin_d2;
-			volatile uint8_t *port_d3; 
-			uint8_t pin_d3;
 			volatile uint8_t *port_d4; 
 			uint8_t pin_d4;
 			volatile uint8_t *port_d5; 
@@ -32,25 +31,20 @@ namespace digitalcave {
 			uint8_t pin_d7;
 			
 			void cmd(uint8_t cmd);
-
+			void write_byte(uint8_t b, uint8_t mode);
+			void write_nibble(uint8_t b, uint8_t mode);
+			
 		public:
-			/*
-			 * Creates a new HD44780 module in 8 bit direct drive mode.
-			 */
 			Hd44780_Direct(uint8_t function);
-			/* TODO add support for specific pins
+			
 			Hd44780_Direct(uint8_t function, 
 								volatile uint8_t *port_rs, uint8_t pin_rs,
 								volatile uint8_t *port_e, uint8_t pin_e,
-								volatile uint8_t *port_d0, uint8_t pin_d0,
-								volatile uint8_t *port_d1, uint8_t pin_d1,
-								volatile uint8_t *port_d2, uint8_t pin_d2,
-								volatile uint8_t *port_d3, uint8_t pin_d3,
 								volatile uint8_t *port_d4, uint8_t pin_d4,
 								volatile uint8_t *port_d5, uint8_t pin_d5,
 								volatile uint8_t *port_d6, uint8_t pin_d6,
 								volatile uint8_t *port_d7, uint8_t pin_d7);
-			*/
+
 			virtual void setByte(uint8_t b);
 	};
 }
