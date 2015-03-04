f7a27950c6a7050f2c85e5c0e75011bc2b4c3358
Wyatt <wyatt@digitalcave.ca>
2015-03-03 18:38:02 -0700

    Adding WIP python client for USB communications

---
+++ projects/power_supply/python/usb_rawhid.py
@@ -0,0 +1,27 @@
+# Sample program demonstrating USB Raw HID access using the pyhidapi library
+# (a ctypes wrapper around hidapi).  Sends alternating arrays containing
+# 0xFF and 0x00, and prints the byte values of any recieved data.
+#
+# Designed to interface with usb_rawhid sample on the atmega32u2
+#############################################################################
+import hid, traceback, time
+
+print(hid.enumerate())
+
+dev = hid.Device(vid=0x4200, pid=0xFF01)
+try:
+	dev.nonblocking = 0
+	print(dev.manufacturer)
+	print(dev.product)
+	print(dev.serial)
+	b = '\x00'
+
+	while True:
+		arr = dev.read(6)
+		voltage = (ord(arr[2]) << 8) + ord(arr[3]);
+		current = (ord(arr[4]) << 8) + ord(arr[5]);
+		print("Read channel " + str(ord(arr[1])) + ", voltage=" + str(voltage) + ", current=" + str(current) + "\n")
+		time.sleep(1)
+except Exception, e:
+	traceback.print_exc()
+dev.close()
