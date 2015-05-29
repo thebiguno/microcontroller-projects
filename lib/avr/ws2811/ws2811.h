ba43bb006a939c6532194099cd463ca1d68f5715
Warren Janssens <warren.janssens@gmail.com>
2015-05-29 00:41:38 -0600

    changes to ws2811/ws2812 lib

--- lib/avr/ws2811/ws2811.h
+++ lib/avr/ws2811/ws2811.h
@@ -1,20 +1,14 @@
-#ifndef ws2811_h
-#define ws2811_h
+#ifndef ws281x_h
+#define ws281x_h
 
 #include <stdint.h>
 
-typedef struct ws2811_t {
-	// the order of the struct members must by g, r, b
+typedef struct ws281x_t {
 	uint8_t red;
 	uint8_t green;
 	uint8_t blue;
 } ws2811_t;
 
-/*
- * Sends GRB values to a chain of WS2811 leds.
- * v a pointer to the first GRB tuple
- * s the number of GRB tuples
- */
-void ws2811_set(struct ws2811_t *values, uint8_t s);
-
+void ws281x_set(const void *values);
+	
 #endif
