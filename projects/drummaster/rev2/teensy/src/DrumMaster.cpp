f6c47ca8f95bc4e17bbc55063e1ef7e893837fc0
Wyatt <wyatt@digitalcave.ca>
2015-07-03 19:43:48 -0600

    WIP on POC multi sample player.  Compiles, but does not yet play any sounds.  Debugging...

--- projects/drummaster/rev2/teensy/src/DrumMaster.cpp
+++ projects/drummaster/rev2/teensy/src/DrumMaster.cpp
@@ -1,75 +1,73 @@
-#include <SerialFlash.h>
+#include <Audio.h>
+#include <Bounce.h>
+#include <SD.h>
 #include <SPI.h>
 
-const int FlashChipSelect = 6;
+AudioPlaySdWav samples[16];
+AudioMixer4 mixers[3];
+AudioMixer4 masterMixer;
+AudioOutputI2S           i2s1;
+AudioConnection          patchCord0(samples[0], 0, mixers[0], 0);
+AudioConnection          patchCord1(samples[1], 0, mixers[0], 1);
+AudioConnection          patchCord2(samples[2], 0, mixers[0], 2);
+AudioConnection          patchCord3(samples[3], 0, mixers[0], 3);
+AudioConnection          patchCord4(samples[4], 0, mixers[1], 0);
+AudioConnection          patchCord5(samples[5], 0, mixers[1], 1);
+AudioConnection          patchCord6(samples[6], 0, mixers[1], 2);
+AudioConnection          patchCord7(samples[7], 0, mixers[1], 3);
+AudioConnection          patchCord8(samples[8], 0, mixers[2], 0);
+AudioConnection          patchCord9(samples[9], 0, mixers[2], 1);
+AudioConnection          patchCord10(samples[10], 0, mixers[2], 2);
+AudioConnection          patchCord11(samples[11], 0, mixers[2], 3);
+AudioConnection          patchCord12(samples[12], 0, mixers[3], 0);
+AudioConnection          patchCord13(samples[13], 0, mixers[3], 1);
+AudioConnection          patchCord14(samples[14], 0, mixers[3], 2);
+AudioConnection          patchCord15(samples[15], 0, mixers[3], 3);
+AudioConnection          patchCord16(mixers[0], 0, masterMixer, 0);
+AudioConnection          patchCord17(mixers[1], 0, masterMixer, 1);
+AudioConnection          patchCord18(mixers[2], 0, masterMixer, 2);
+AudioConnection          patchCord19(mixers[3], 0, masterMixer, 3);
+AudioConnection          patchCord20(masterMixer, 0, i2s1, 0);
+AudioConnection          patchCord21(masterMixer, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000;
 
-void error(const char *message) {
-  while (1) {
-    Serial.println(message);
-    delay(2500);
-  }
-}
+//Index into samples array
+uint8_t sampleCounter = 0;
 
-void spaces(int num) {
-  for (int i=0; i < num; i++) {
-    Serial.print(" ");
-  }
-}
+Bounce button0 = Bounce(15, 25);
 
-void setup() {
-  //uncomment these if using Teensy audio shield
-  //SPI.setSCK(14);  // Audio shield has SCK on pin 14
-  //SPI.setMOSI(7);  // Audio shield has MOSI on pin 7
+int main(){
+	delay(1000);
+	Serial.println("Starting...");
+	
+	//Audio shield SPI setup
+	SPI.setMOSI(7);
+	SPI.setMISO(12);
+	SPI.setSCK(14);
+	
+	sgtl5000.enable();
+	sgtl5000.volume(0.5);
+	
+	pinMode(15, INPUT_PULLUP);
 
-  // wait for Arduino Serial Monitor
-  while (!Serial) ;
-  delay(100);
-  Serial.println("All Files on SPI Flash chip:");
-
-  if (!SerialFlash.begin()) {
-    error("Unable to access SPI Flash chip");
-  }
-
-  SerialFlash.opendir();
-  unsigned int count = 0;
-  while (1) {
-    char filename[64];
-    unsigned long filesize;
-
-    if (SerialFlash.readdir(filename, sizeof(filename), filesize)) {
-      Serial.print("  ");
-      Serial.print(filename);
-      spaces(20 - strlen(filename));
-      Serial.print("  ");
-      Serial.print(filesize);
-      Serial.print(" bytes");
-      Serial.println();
-    } else {
-      break; // no more files
-    }
-  }
-}
-
-void loop() {
-}
-
-
-
-
-extern "C" int main(void){
-	/*
-	pinMode(13, OUTPUT);
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(500);
-		digitalWriteFast(13, LOW);
-		delay(500);
+	if (!(SD.begin(10))) {
+		// stop here, but print a message repetitively
+		while (1) {
+			Serial.println("Unable to access the SD card");
+			delay(500);
+		}
 	}
-	*/
-	setup();
 	
 	while (1){
-		loop();
+		button0.update();
+		
+		if (button0.fallingEdge()) {
+			Serial.println("Button push");
+			samples[sampleCounter].play("SDTEST1.WAV");
+			sampleCounter = (sampleCounter + 1) & 0x0F;
+		}
+		
+		delay(5);
 	}
 }
 
