fc5c01dbb1cbd9388428faccd90dcf5157a65904
Wyatt <wyatt@digitalcave.ca>
2015-07-05 20:28:55 -0600

    More work on the drums.  Extracting sample logic into a separate class

--- projects/drummaster/rev2/teensy/src/DrumMaster.cpp
+++ projects/drummaster/rev2/teensy/src/DrumMaster.cpp
@@ -1,8 +1,11 @@
 #include <ADC.h>
-#include <Audio.h>
 #include <Bounce.h>
 #include <SD.h>
 #include <SPI.h>
+
+#include "Samples.h"
+
+using namespace digitalcave;
 
 //Pin assignments
 #define MUX0						0
@@ -18,36 +21,9 @@
 
 //Constants
 #define MIN_VALUE					10
-#define SAMPLE_COUNT				16
 
-AudioPlaySdRaw samples[SAMPLE_COUNT];
-AudioMixer4 mixers[4];
-AudioMixer4 masterMixer;
-AudioOutputI2S output;
-AudioConnection patchCord00(samples[0],  0, mixers[0], 0);
-AudioConnection patchCord01(samples[1],  0, mixers[0], 1);
-AudioConnection patchCord02(samples[2],  0, mixers[0], 2);
-AudioConnection patchCord03(samples[3],  0, mixers[0], 3);
-AudioConnection patchCord04(samples[4],  0, mixers[1], 0);
-AudioConnection patchCord05(samples[5],  0, mixers[1], 1);
-AudioConnection patchCord06(samples[6],  0, mixers[1], 2);
-AudioConnection patchCord07(samples[7],  0, mixers[1], 3);
-AudioConnection patchCord08(samples[8],  0, mixers[2], 0);
-AudioConnection patchCord09(samples[9],  0, mixers[2], 1);
-AudioConnection patchCord10(samples[10], 0, mixers[2], 2);
-AudioConnection patchCord11(samples[11], 0, mixers[2], 3);
-AudioConnection patchCord12(samples[12], 0, mixers[3], 0);
-AudioConnection patchCord13(samples[13], 0, mixers[3], 1);
-AudioConnection patchCord14(samples[14], 0, mixers[3], 2);
-AudioConnection patchCord15(samples[15], 0, mixers[3], 3);
-
-AudioConnection patchCord16(mixers[0], 0, masterMixer, 0);
-AudioConnection patchCord17(mixers[1], 0, masterMixer, 1);
-AudioConnection patchCord18(mixers[2], 0, masterMixer, 2);
-AudioConnection patchCord19(mixers[3], 0, masterMixer, 3);
-AudioConnection patchCord20(masterMixer, 0, output, 0);
-AudioConnection patchCord21(masterMixer, 0, output, 1);
-AudioControlSGTL5000     sgtl5000;
+Samples samples;
+AudioControlSGTL5000 control;
 
 ADC adc;
 
@@ -84,99 +60,15 @@ uint8_t readDrum(uint8_t channel){
 	return result;
 }
 
-/*
- * Find the first available sample that is not currently playing; if all samples
- * are playing, then find the oldest one to stop it.
- */
-uint8_t findAvailableSample(){
-	uint8_t oldestSample = 0;
-	uint16_t oldestSamplePosition = 0;
-	for (uint8_t i = 0; i < 4; i++){	//TODO Change 4 to SAMPLE_COUNT
-		if (samples[i].isPlaying() == 0){
-			return i;
-		}
-		else if (samples[i].positionMillis() > oldestSamplePosition){
-			oldestSamplePosition = samples[i].positionMillis();
-			oldestSample = i;
-		}
-	}
-	samples[oldestSample].stop();
-	return oldestSample;
-}
-
-void playSample(uint8_t channel, uint8_t value){
-	char sample[12];
-	double volume = value / 256.0;
-	switch(channel){
-		case 0:
-			snprintf(sample, sizeof(sample), "DX%02d.RAW", 0);
-			break;
-		case 1:
-			snprintf(sample, sizeof(sample), "SN%02d.RAW", 0);
-			break;
-		case 2:
-			snprintf(sample, sizeof(sample), "BS%02d.RAW", 0);
-			break;
-		case 3:
-			snprintf(sample, sizeof(sample), "T1%02d.RAW", 0);
-			break;
-		case 4:
-			snprintf(sample, sizeof(sample), "T2%02d.RAW", 0);
-			break;
-		case 5:
-			snprintf(sample, sizeof(sample), "T3%02d.RAW", 0);
-			break;
-		case 6:
-			snprintf(sample, sizeof(sample), "HH%02d.RAW", 0);
-			break;
-		case 7:
-			snprintf(sample, sizeof(sample), "SP%02d.RAW", 0);
-			break;
-		case 8:
-			snprintf(sample, sizeof(sample), "CR%02d.RAW", 0);
-			break;
-		case 9:
-			snprintf(sample, sizeof(sample), "RD%02d.RAW", 0);
-			break;
-		case 10:
-			snprintf(sample, sizeof(sample), "CX%02d.RAW", 0);
-			break;
-	}
-	
-	uint8_t c = findAvailableSample();
-	mixers[c >> 2].gain(c & 0x03, volume / 2);
-	samples[c].play("00.RAW");
-	
-	Serial.print("Playing sample index ");
-	Serial.print(c);
-	Serial.print(" at volume ");
-	Serial.print(volume);
-	Serial.println();
-	
-	Serial.print("CPU: ");
-	Serial.print(AudioProcessorUsage());
-	Serial.print(",");
-	Serial.print(AudioProcessorUsageMax());
-	Serial.print("    ");
-	Serial.print("Memory: ");
-	Serial.print(AudioMemoryUsage());
-	Serial.print(",");
-	Serial.print(AudioMemoryUsageMax());
-	Serial.println();
-}
-
 int main(){
 	//Audio shield SPI setup
 	SPI.setMOSI(7);
 	SPI.setMISO(12);
 	SPI.setSCK(14);
-
-	//Allocate enough memory for audio
-	AudioMemory(16);
 	
 	//Turn on the audio chip
-	sgtl5000.enable();
-	sgtl5000.volume(0.5);
+	control.enable();
+	control.volume(0.5);
 	
 	//Enable pins
 	pinMode(MUX0, OUTPUT);
@@ -206,7 +98,7 @@ int main(){
 		
 		uint8_t value = readDrum(channel);
 		if (value > MIN_VALUE){
-			playSample(channel, value);
+			samples.play(channel, value);
 		}
 		channel = (channel + 1) & 0x0F;
 /*		
---
+++ projects/drummaster/rev2/teensy/src/Samples.cpp
@@ -0,0 +1,127 @@
+#include "Samples.h"
+
+using namespace digitalcave;
+
+Samples::Samples():
+	patchCord00(samples[0],  0, mixers[0], 0),
+	patchCord01(samples[1],  0, mixers[0], 1),
+	patchCord02(samples[2],  0, mixers[0], 2),
+	patchCord03(samples[3],  0, mixers[0], 3),
+	patchCord04(samples[4],  0, mixers[1], 0),
+	patchCord05(samples[5],  0, mixers[1], 1),
+	patchCord06(samples[6],  0, mixers[1], 2),
+	patchCord07(samples[7],  0, mixers[1], 3),
+	patchCord08(samples[8],  0, mixers[2], 0),
+	patchCord09(samples[9],  0, mixers[2], 1),
+	patchCord10(samples[10], 0, mixers[2], 2),
+	patchCord11(samples[11], 0, mixers[2], 3),
+	patchCord12(samples[12], 0, mixers[3], 0),
+	patchCord13(samples[13], 0, mixers[3], 1),
+	patchCord14(samples[14], 0, mixers[3], 2),
+	patchCord15(samples[15], 0, mixers[3], 3),
+	patchCord16(mixers[0], 0, masterMixer, 0),
+	patchCord17(mixers[1], 0, masterMixer, 1),
+	patchCord18(mixers[2], 0, masterMixer, 2),
+	patchCord19(mixers[3], 0, masterMixer, 3),
+	patchCord20(masterMixer, 0, output, 0),
+	patchCord21(masterMixer, 0, output, 1)
+{	
+	//Allocate enough memory for audio
+	AudioMemory(16);
+	
+	//Initialize maps
+	for (uint8_t i = 0; i < CHANNEL_COUNT; i++){
+		this->sampleToChannelMap[i] = 0xFF;
+	}
+	for (uint8_t i = 0; i < 11; i++){
+		this->channelToSampleMap[i] = 0xFF;
+	}
+}
+
+
+uint8_t Samples::findAvailableSample(){
+	uint8_t oldestSample = 0;
+	uint16_t oldestSamplePosition = 0;
+	for (uint8_t i = 0; i < 4; i++){	//TODO Change 4 to SAMPLE_COUNT
+		if (this->samples[i].isPlaying() == 0){
+			return i;
+		}
+		else if (this->samples[i].positionMillis() > oldestSamplePosition){
+			oldestSamplePosition = this->samples[i].positionMillis();
+			oldestSample = i;
+		}
+	}
+	this->samples[oldestSample].stop();
+	return oldestSample;
+}
+
+void Samples::play(uint8_t channel, uint8_t value){
+	char sample[12];
+	double volume = value / 256.0;
+	switch(channel){
+		case 0:
+			snprintf(sample, sizeof(sample), "DX%02d.RAW", 0);
+			break;
+		case 1:
+			snprintf(sample, sizeof(sample), "SN%02d.RAW", 0);
+			break;
+		case 2:
+			snprintf(sample, sizeof(sample), "BS%02d.RAW", 0);
+			break;
+		case 3:
+			snprintf(sample, sizeof(sample), "T1%02d.RAW", 0);
+			break;
+		case 4:
+			snprintf(sample, sizeof(sample), "T2%02d.RAW", 0);
+			break;
+		case 5:
+			snprintf(sample, sizeof(sample), "T3%02d.RAW", 0);
+			break;
+		case 6:
+			snprintf(sample, sizeof(sample), "HH%02d.RAW", 0);
+			break;
+		case 7:
+			snprintf(sample, sizeof(sample), "SP%02d.RAW", 0);
+			break;
+		case 8:
+			snprintf(sample, sizeof(sample), "CR%02d.RAW", 0);
+			break;
+		case 9:
+			snprintf(sample, sizeof(sample), "RD%02d.RAW", 0);
+			break;
+		case 10:
+			snprintf(sample, sizeof(sample), "CX%02d.RAW", 0);
+			break;
+	}
+	
+	//Check if this channel was recently played.  If it was, check the value; we will increase
+	// the gain for higher values, but will not change it for lower values.
+	if (millis() - this->lastPlayedChannel[channel] < DOUBLE_HIT_THRESHOLD){
+		
+	}
+	
+	uint8_t c = this->findAvailableSample();
+	this->mixers[c >> 2].gain(c & 0x03, volume / 2);
+	this->samples[c].play("00.RAW");
+	
+	this->sampleToChannelMap[c] = channel;
+	this->channelToSampleMap[channel] = c;
+	this->lastPlayedChannel[channel] = millis();
+	
+	Serial.print("Playing sample index ");
+	Serial.print(c);
+	Serial.print(" at volume ");
+	Serial.print(volume);
+	Serial.println();
+	
+	Serial.print("CPU: ");
+	Serial.print(AudioProcessorUsage());
+	Serial.print(",");
+	Serial.print(AudioProcessorUsageMax());
+	Serial.print("    ");
+	Serial.print("Memory: ");
+	Serial.print(AudioMemoryUsage());
+	Serial.print(",");
+	Serial.print(AudioMemoryUsageMax());
+	Serial.println();
+}
