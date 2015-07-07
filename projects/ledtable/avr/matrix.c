aafba0142c42ddf017b69d861f28c690dd8496aa
Warren Janssens <janssensw@richer.ca>
2015-07-07 09:03:10 -0600

    adding buffer to matrix

--- projects/ledtable/avr/matrix.c
+++ projects/ledtable/avr/matrix.c
@@ -1,11 +1,17 @@
 #include "matrix.h"
 
+uint8_t changed;
 pixel_t buffer[144];
 
 void set_pixel(int16_t x, int16_t y, pixel_t rgb, uint8_t overlay) {
 	uint8_t i = ((x & 0x01) == 0x00) ? (x * 12 + y) : (x * 23 - y);
 	
 	if (i >= 144 || i < 0) return;
+	
+	pixel_t current;
+	current.red = buffer[i].red;
+	current.green = buffer[i].green;
+	current.blue = buffer[i].blue;
 	
 	if (overlay == OVERLAY_REPLACE){
 		buffer[i].red   = rgb.red;
@@ -27,6 +33,8 @@ void set_pixel(int16_t x, int16_t y, pixel_t rgb, uint8_t overlay) {
 		buffer[i].green ^= rgb.green;
 		buffer[i].blue  ^= rgb.blue;
 	}
+	
+	if (buffer[i].red != current.red || buffer[i].green != current.green || buffer[i].blue != current.blue) changed = 1;
 }
 
 pixel_t get_pixel(uint8_t x, uint8_t y) {
@@ -41,5 +49,8 @@ pixel_t get_pixel(uint8_t x, uint8_t y) {
 }
 
 void matrix_write_buffer(){
-	ws281x_set(buffer);
+	if (changed) {
+		ws281x_set(buffer);
+		changed = 0;
+	}
 }
