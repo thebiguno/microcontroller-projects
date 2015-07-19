0ce788e47d3ada7eaf3790935cbb65fc0f41beb8
Warren Janssens <warren.janssens@gmail.com>
2015-07-19 10:22:43 -0600

    wip

---
+++ projects/ledtable/avr/Animated.h
@@ -0,0 +1,19 @@
+#ifndef Animated_H
+#define Animated_H
+
+#include <Module.h>
+#include <stdint.h>
+
+namespace digitalcave {
+	class Animated : public Module {
+	private:
+	
+	public:
+		Animated();
+		~Animated();
+		void run();
+
+	};
+}
+
+#endif
