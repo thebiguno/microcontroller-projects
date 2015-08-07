75a1dfe85ea2eef4d40b8700c13f0062f949c3c0
Wyatt <wyatt@digitalcave.ca>
2015-08-06 23:40:20 -0600

    i hate computers

--- projects/drummaster/rev2/src/Sample.cpp
+++ projects/drummaster/rev2/src/Sample.cpp
@@ -4,37 +4,9 @@ using namespace digitalcave;
 
 //All the audio junk.  I can't seem to get it into the class structure...
 
-//Inputs
-static AudioPlaySerialRaw samples[SAMPLE_COUNT];
-static AudioInputI2Sslave input;
+extern AudioPlaySerialRaw samples[SAMPLE_COUNT];
+extern AudioMixer16 mixer;
 
-//Mixer
-static AudioMixer16 mixer;
-
-//Output
-static AudioOutputI2S output;
-
-//Samples to mixer
-static AudioConnection sample0ToMixer(samples[0], 0, mixer, 0);
-static AudioConnection sample1ToMixer(samples[1], 0, mixer, 1);
-static AudioConnection sample2ToMixer(samples[2], 0, mixer, 2);
-static AudioConnection sample3ToMixer(samples[3], 0, mixer, 3);
-static AudioConnection sample4ToMixer(samples[4], 0, mixer, 4);
-static AudioConnection sample5ToMixer(samples[5], 0, mixer, 5);
-static AudioConnection sample6ToMixer(samples[6], 0, mixer, 6);
-static AudioConnection sample7ToMixer(samples[7], 0, mixer, 7);
-static AudioConnection sample8ToMixer(samples[8], 0, mixer, 8);
-static AudioConnection sample9ToMixer(samples[9], 0, mixer, 9);
-static AudioConnection sample10ToMixer(samples[10], 0, mixer, 10);
-static AudioConnection sample11ToMixer(samples[11], 0, mixer, 11);
-
-//Input passthrough to mixer
-static AudioConnection inputToMixer0(input, 0, mixer, 12);
-static AudioConnection inputToMixer1(input, 1, mixer, 13);
-
-//Mixer to output
-static AudioConnection mixerToOutput0(mixer, 0, output, 0);
-static AudioConnection mixerToOutput1(mixer, 0, output, 1);
 
 Sample::Sample(uint8_t index): index(index) {
 	//Nothing to see here...
@@ -51,11 +23,6 @@ uint8_t Sample::isPlaying(){
 }
 
 uint32_t Sample::positionMillis(){
-	Serial.print("lengthMillis: ");
-	Serial.println(samples[index].lengthMillis());
-
-	Serial.print("positionMillis: ");
-	Serial.println(samples[index].positionMillis());
 	return samples[index].positionMillis();
 }
 
@@ -64,5 +31,5 @@ void Sample::stop(){
 }
 
 void Sample::setGain(uint8_t volume){
-	mixer.gain(index, volume / VOLUME_DIVISOR);
+	mixer.gain(index, 0.5);//volume / VOLUME_DIVISOR);
 }
