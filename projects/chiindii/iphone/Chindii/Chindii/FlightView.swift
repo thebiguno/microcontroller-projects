0bb4662b66c46217cb4d3b7e02fef1afc22bf111
Warren Janssens <warren.janssens@gmail.com>
2015-09-08 22:35:42 -0600

    pitch roll mostly working something is wrong with roll still though

--- projects/chiindii/iphone/Chindii/Chindii/FlightView.swift
+++ projects/chiindii/iphone/Chindii/Chindii/FlightView.swift
@@ -40,48 +40,6 @@ class FlightView: UIView {
 		let centerX = width / 2
 		let centerY = height / 2
 		
-		//		CGContextSetBlendMode(context, kCGBlendModeNormal)
-		
-		
-		buttons.removeAll(keepCapacity: true)
-
-		// draw the artifical horizon main circle (same in both orientations)
-		let r = CGRect(x: centerX - radius, y: centerY - radius, width: radius * 2, height: radius * 2)
-		CGContextAddEllipseInRect(context, r)
-		CGContextStrokePath(context)
-		
-		// draw the artifical horizon
-		// TODO, this has to move with the pitch / roll telemetry
-		CGContextAddEllipseInRect(context, CGRect(x: centerX - 10, y: centerY - 10, width: 20, height: 20))
-		CGContextMoveToPoint(context, centerX - 120, centerY);
-		CGContextAddLineToPoint(context, centerX - 20, centerY)
-		CGContextMoveToPoint(context, centerX + 20, centerY)
-		CGContextAddLineToPoint(context, centerX + 120, centerY)
-		CGContextStrokePath(context)
-		
-		CGContextSetLineDash(context, 0, [0,10], 2)
-		CGContextMoveToPoint(context, centerX - 20, centerY - 20)
-		CGContextAddLineToPoint(context, centerX + 21, centerY - 20)
-		CGContextMoveToPoint(context, centerX - 20, centerY + 20)
-		CGContextAddLineToPoint(context, centerX + 21, centerY + 20)
-		CGContextMoveToPoint(context, centerX - 40, centerY - 40)
-		CGContextAddLineToPoint(context, centerX + 41, centerY - 40)
-		CGContextMoveToPoint(context, centerX - 40, centerY + 40)
-		CGContextAddLineToPoint(context, centerX + 41, centerY + 40)
-		CGContextMoveToPoint(context, centerX - 60, centerY - 60)
-		CGContextAddLineToPoint(context, centerX + 61, centerY - 60)
-		CGContextMoveToPoint(context, centerX - 60, centerY + 60)
-		CGContextAddLineToPoint(context, centerX + 61, centerY + 60)
-		CGContextMoveToPoint(context, centerX - 80, centerY - 80)
-		CGContextAddLineToPoint(context, centerX + 81, centerY - 80)
-		CGContextMoveToPoint(context, centerX - 80, centerY + 80)
-		CGContextAddLineToPoint(context, centerX + 81, centerY + 80)
-		CGContextStrokePath(context)
-		
-		CGContextSetLineDash(context, 0, [], 0)
-		
-		let points = [CGPoint]();
-
 		// draw the throttle position
 		for i in -6..<7 {
 			if (i == g) {
@@ -106,13 +64,14 @@ class FlightView: UIView {
 		}
 		
 		// draw the buttons
+		buttons.removeAll(keepCapacity: true)
 		for i in 0..<2 {
 			let r = i == 0 ?
-			CGRect(x: rect.width - 100, y: 20, width: 80, height: 80) :
-			CGRect(x: rect.width - 100, y: rect.height - 100, width: 80, height: 80)
+				CGRect(x: rect.width - 100, y: 20, width: 80, height: 80) :
+				CGRect(x: rect.width - 100, y: rect.height - 100, width: 80, height: 80)
 			
 			buttons.append(r)
-
+			
 			CGContextAddEllipseInRect(context, r)
 			if (pressed[i]) {
 				CGContextSetRGBFillColor(context, 1.0, 1.0, 0.8, 1.0)
@@ -133,69 +92,128 @@ class FlightView: UIView {
 		CGContextAddLineToPoint(context, rect.width - 80, 60)
 		CGContextStrokePath(context)
 		
-		// draw the menu button
-		CGContextSetRGBStrokeColor(context, 0.5, 0.7, 0.8, 1.0)
-		CGContextMoveToPoint(context, rect.width - 40, rect.height - 40)
-		CGContextAddLineToPoint(context, rect.width - 80, rect.height - 40)
-		CGContextMoveToPoint(context, rect.width - 40, rect.height - 60)
-		CGContextAddLineToPoint(context, rect.width - 80, rect.height - 60)
-		CGContextMoveToPoint(context, rect.width - 40, rect.height - 80)
-		CGContextAddLineToPoint(context, rect.width - 80, rect.height - 80)
-		CGContextStrokePath(context)
-		
 		// draw the battery indicator
 		//CGContextSetLineWidth(context, 3.0);
 		//CGContextMoveToPoint(context, rect.width - 60, 20)
 		//CGContextAddCurveToPoint(context, rect.width - 7, 20, rect.width - 7, 100, rect.width - 60, 100)
 		//CGContextStrokePath(context)
+		
+		
+		//		CGContextSetBlendMode(context, kCGBlendModeNormal)
+		
+		
+		// translate to the centre to draw the horizon
+		CGContextTranslateCTM(context, centerX, centerY)
+
+		// draw the artifical horizon main circle (same in both orientations)
+		let r = CGRect(x: -radius, y: -radius, width: radius * 2, height: radius * 2)
+		CGContextAddEllipseInRect(context, r)
+		CGContextStrokePath(context)
+		
+		// draw the horizon lines in 5 degree increments
+		CGContextSetLineDash(context, 0, [0,5], 2)
+		// 0
+		CGContextMoveToPoint(context, -140, 0)
+		CGContextAddLineToPoint(context, +141, 0)
+		// +/- 5
+		CGContextMoveToPoint(context, -10, -20)
+		CGContextAddLineToPoint(context, +11, -20)
+		CGContextMoveToPoint(context, -10, +20)
+		CGContextAddLineToPoint(context, +11, +20)
+		// +/- 10
+		CGContextMoveToPoint(context, -40, -40)
+		CGContextAddLineToPoint(context, +41, -40)
+		CGContextMoveToPoint(context, -40, +40)
+		CGContextAddLineToPoint(context, +41, +40)
+		// +/- 15
+		CGContextMoveToPoint(context, -20, -60)
+		CGContextAddLineToPoint(context, +21,  -60)
+		CGContextMoveToPoint(context, -20, +60)
+		CGContextAddLineToPoint(context, +21, +60)
+		// +/- 20
+		CGContextMoveToPoint(context, -40, -80)
+		CGContextAddLineToPoint(context, +41, -80)
+		CGContextMoveToPoint(context, -40, +80)
+		CGContextAddLineToPoint(context, +41, +80)
+		// +/- 35
+		CGContextMoveToPoint(context, -30, -100)
+		CGContextAddLineToPoint(context, +31, -100)
+		CGContextMoveToPoint(context, -30, +100)
+		CGContextAddLineToPoint(context, +31, +100)
+		// +/- 40
+		CGContextMoveToPoint(context, -60, -120)
+		CGContextAddLineToPoint(context, +61, -120)
+		CGContextMoveToPoint(context, -60, +120)
+		CGContextAddLineToPoint(context, +61, +120)
+
+		CGContextStrokePath(context)
+		
+		CGContextSetLineDash(context, 0, [], 0)
+		
+		// draw the artifical horizon
+		// TODO, this has to move with the pitch / roll telemetry
+		
+		CGContextTranslateCTM(context, 0, CGFloat(-pitch*20))
+		CGContextRotateCTM(context, CGFloat(M_PI / 6 / 6) * CGFloat(-roll));
+		
+		CGContextSetRGBStrokeColor(context, 1.0, 1.0, 0.8, 1.0)
+		CGContextAddEllipseInRect(context, CGRect(x: -10, y: -10, width: 20, height: 20))
+		CGContextMoveToPoint(context, -60, 0);
+		CGContextAddLineToPoint(context, -10, 0)
+		CGContextMoveToPoint(context, +10, 0)
+		CGContextAddLineToPoint(context, +60, 0)
+		CGContextStrokePath(context)
+
 	}
 	
 	override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-		let touch = touches.first as! UITouch
-		let point = touch.locationInView(self)
-
-		for i in 0..<2 {
-			let r = buttons[i]
-			pressed[i] = r.contains(point)
+		for touch in touches {
+			let point = (touch as! UITouch).locationInView(self)
+			
+			for i in 0..<2 {
+				let r = buttons[i]
+				pressed[i] = r.contains(point)
+			}
+			gTouch(touch as! UITouch);
+			pitchRollTouch(touch as! UITouch)
 		}
-		
-		// throttle position
-		for i in -6..<7 {
-			if (i <= 0) {
-				let rect = CGRect(x: 20, y: bounds.height / 2 - CGFloat(i*20), width: 40, height: 20);
-				if (rect.contains(point)) {
-					g = i;
-				}
-			}
-			if (i >= 0) {
-				let rect = CGRect(x: 20, y: bounds.height / 2 - CGFloat(i*20) - 20, width: 40, height: 20);
-				if (rect.contains(point)) {
-					g = i;
-				}
-			}
+
+		setNeedsDisplay()
+	}
+	override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+		for touch in touches {
+			gTouch(touch as! UITouch);
+			pitchRollTouch(touch as! UITouch)
 		}
 		
 		setNeedsDisplay()
 	}
-	override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
-		let touch = touches.first as! UITouch
+	override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+		for i in 0..<2 {
+			pressed[i] = false
+		}
+		setNeedsDisplay()
+	}
+	
+	func gTouch(touch: UITouch) {
 		let point = touch.locationInView(self)
 
-		// throttle position
 		for i in -6..<7 {
-			
 			let by = bounds.height / 2 - CGFloat(i*20) - (i >= 0 ? 20 : 0)
 			let h = CGFloat(i == 0 ? 40 : 20)
-
-			let rect = CGRect(x: 20, y: by, width: 40, height: 20);
+			
+			let rect = CGRect(x: 20, y: by, width: 40, height: h);
 			if (rect.contains(point)) {
 				g = i;
 			}
 		}
-		
-		// pitch (y) / roll (x)
+	}
+	
+	func pitchRollTouch(touch: UITouch) {
+		let point = touch.locationInView(self)
+
 		for x in -6..<7 {
-			let bx = bounds.width / 2 - CGFloat(x*20) - (x >= 0 ? 20 : 0)
+			let bx = bounds.width / 2 - CGFloat(x*20) - (x > 0 ? 0 : 20)
 			let h = CGFloat(x == 0 ? 40 : 20)
 			
 			for y in -6..<7 {
@@ -209,13 +227,5 @@ class FlightView: UIView {
 				}
 			}
 		}
-
-		setNeedsDisplay()
-	}
-	override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
-		for i in 0..<2 {
-			pressed[i] = false
-		}
-		setNeedsDisplay()
 	}
 }
