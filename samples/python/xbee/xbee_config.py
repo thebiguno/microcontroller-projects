fec61815d6759a353839fb460dc05a4c7a6f9965
Wyatt <wyatt@digitalcave.ca>
2015-09-17 19:20:35 -0600

    Some more work on the xbee configuration script.  Getting a bit more clear and extensible.

--- samples/python/xbee/xbee_config.py
+++ samples/python/xbee/xbee_config.py
@@ -59,117 +59,103 @@ def exitCommandMode(ser):
 		print("Cannot exit command mode")
 		return False
 
-def read(ser):
-	if (not(enterCommandMode(ser))):
-		return False
-	
-	
-	for i in range(0, len(AT_READ)):
-		VALUES[i] = getReplyFor(ser, AT_READ[i])
-		print(AT_READ[i] + ": " + VALUES[i])
-		
-	if (not(exitCommandMode(ser))):
-		return False
-	
-	print("Read successful\n")
-	
-	#We could check for minimum firmware here... but I know that my modules are all fine.
-	
-	return True
+MINIMUM_FIRMWARE = 0x10A5
 
-def change(ser):
-	#Choose a baud rate
+def writeGeneric(atCommand):
+	if (atCommand["newValue"] == None):
+		return None
+	return atCommand["at"] + str(atCommand["newValue"])
+
+def writePan(atCommand):
+	if (atCommand["newValue"] == None):
+		return None
+	return atCommand["at"] + format(atCommand["newValue"], 'x')
+
+def readBaud(atCommand):
 	for i in range(0, len(BAUD_RATES)):
 		print(" (" + str(i) + ") " + str(BAUD_RATES[i]))
-	baudIndex = raw_input("Choose a baud rate [" + VALUES[2] + "]: ")
+	baudIndex = raw_input("Choose a baud rate [" + str(atCommand["oldValue"]) + "]: ")
 	if (baudIndex == ""):
-		baudIndex = VALUES[2]
+		baudIndex = atCommand["oldValue"]
 	bd = int(baudIndex)
 	if (bd < 0 or bd >= len(BAUD_RATES)):
 		print("Invalid input\n")
-		return False
+		return None
+	else:
+		return bd
 
-	#Choose a channel
-	channel = raw_input("Choose a Channel (B: XBee) (C: XBee Pro) [" + VALUES[3] + "]: ")
+def readPan(atCommand):
+	panId = raw_input("Choose a PAN ID (0 - FFFF) [" + str(atCommand["oldValue"]) + "]:")
+	if (panId == ""):
+		panId = atCommand["oldValue"]
+	id = int("0x" + panId, 16)
+	if (id < 0 or id > 0xffff):
+		print("Invalid input\n")
+		return None
+	else:
+		return id;
+
+def readChannel(atCommand):
+	channel = raw_input("Choose a Channel (B: XBee) (C: XBee Pro) [" + str(atCommand["oldValue"]) + "]: ")
 	if (channel == ""):
-		channel = VALUES[3]
+		channel = atCommand["oldValue"]
 	if (channel == 'B' or channel == 'C'):
 		ch = channel
+		return ch
 	else:
 		print("Invalid input\n")
-		return False
+		return None
 
-	#Choose the PAN ID
-	panId = raw_input("Choose a PAN ID (0 - FFFE) [" + VALUES[4] + "]:")
-	if (panId == ""):
-		panId = VALUES[4]
-	id = int("0x" + panId, 16)
-	if (id < 0 or id > 0xfffe):
-		print("Invalid input\n")
-		return False
-	
-	#Is this a coordinator?
-	coordinator = raw_input("Choose end device (0) or coordinator (1) [" + VALUES[5] + "]: ")
+
+def readCoordinator(atCommand):
+	coordinator = raw_input("Choose end device (0) or coordinator (1) [" + str(atCommand["oldValue"]) + "]: ")
 	if (coordinator == ""):
-		coordinator = VALUES[5]
+		coordinator = atCommand["oldValue"]
 	ce = int(coordinator)
 	if (ce < 0 or ce > 1):
 		print("Invalid input\n")
-
-	#TODO things we may want to think about
-	# * MM -- probably want to use MM1
-	# * RR -- ignored for MM1?
-	# * RO -- probably want to use RO1
-	
-	if (ce == 0):
-		my = 0
-		dl = 0
+		return None;
 	else:
-		my = 1
-		dl = 1
-		
-	atCommandString = "ATBD" + str(bd) + ",CE" + str(ce) + ",ID" + format(id, 'x') + ",MY" + str(my) + ",DL" + str(dl) + ",WR"
+		return ce;
 	
-	confirm = raw_input(atCommandString + "\nOK to write? (y/n): ")
-	if (confirm == "y" or confirm == "Y"):
-		if (not(enterCommandMode(ser))):
-			return False
-		ser.write("ATRE\r")
-		ser.flush()
-		if(not(isOkay(ser))):
-			print("Can't reset\n");
-			return False;
+def readMyId(atCommand):
+	for x in supportedATCommands:
+		if x["at"] == "CE":
+			return x["newValue"]
+	return None
 
-		if getReplyFor(ser, atCommandString) != "OK":
-			print("Cannot write configuration\n")
-			return False
-		
-		if (not(exitCommandMode(ser))):
-			return False
-		
-		print("Configuration update successful\n")
-	else:
-		print("Cancelled configuration update\n")
-	
+def readDestinationHigh(atCommand):
+	return "0"
+
+def readDestinationLow(atCommand):
+	for x in supportedATCommands:
+		if x["at"] == "CE":
+			return x["newValue"]
+	return None
+
+
+BAUD_RATES = [1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200]
 
 ########## Main program starts here ##########
-
-MINIMUM_FIRMWARE = 0x10A5
-BAUD_RATES = [1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200]
-AT_READ = [
-		"SH", # serial number high 
-		"SL", # serial number low 
-		"BD", # baud rate
-		"CH", # channel
-		"ID", # pan id
-		"CE", # coordinator enable
-		"RR", # retries
-		"RO", # packetization timeout
-		"MY", # my id 
-		"DL", # destination address low 
-		"VR", # firmware version
+#Supported AT commands, along with the following info:
+#	at: the AT command
+#	writable: boolean indicating whether it is to be written (True) or read only (False)
+#	read: a function to read the user's input into a variable of the proper type, populating newValue
+#	write: a function to format the saved value into an AT command for writing.
+supportedATCommands = [
+		{"at": "SH", "writable": False}, # serial number high 
+		{"at": "SL", "writable": False}, # serial number low 
+		{"at": "BD", "writable": True, "read": readBaud, "write": writeGeneric}, # baud rate
+		{"at": "CH", "writable": False}, # channel
+		{"at": "ID", "writable": True, "read": readPan, "write": writePan}, # pan id
+		{"at": "CE", "writable": True, "read": readCoordinator, "write": writeGeneric}, # coordinator enable
+		{"at": "RR", "writable": False}, # retries
+		{"at": "RO", "writable": False}, # packetization timeout
+		{"at": "MY", "writable": True, "read": readMyId, "write": writeGeneric}, # my id 
+		{"at": "DH", "writable": True, "read": readDestinationHigh, "write": writeGeneric}, # destination address high
+		{"at": "DL", "writable": True, "read": readDestinationLow, "write": writeGeneric}, # destination address low 
+		{"at": "VR", "writable": False}, # firmware version
 ]
-VALUES = [None] * len(AT_READ)
 
 if (len(sys.argv) != 2):
 	print("Usage: " + sys.argv[0] + " <port>")
@@ -181,9 +167,57 @@ for i in range(0, len(BAUD_RATES)):
 baudIndex = input("Choose a baud rate: ")
 
 ser = serial.Serial(sys.argv[1], BAUD_RATES[baudIndex], timeout=2)
-if (not(read(ser))):
-	sys.exit(0)
+
+if (not(enterCommandMode(ser))):
+	sys.exit(-1);
+
+print("\nCurrent configuration:")
+for x in supportedATCommands:
+	x["oldValue"] = getReplyFor(ser, x["at"])
+	print("  " + x["at"] + ": " + x["oldValue"])
+
+print("\n")
+	
+if (not(exitCommandMode(ser))):
+	sys.exit(-1);
+
+print("Read successful\n")
 
 changeConfig = raw_input("Change configuration? (y/n): ")
-if (changeConfig == "Y" or changeConfig == "y"):
-	change(ser)
+if (changeConfig != "Y" and changeConfig != "y"):
+	sys.exit(0)
+
+for x in supportedATCommands:
+	if x["writable"]:
+		x["newValue"] = x["read"](x)
+
+atCommandString = ""
+for x in supportedATCommands:
+	if x["writable"]:
+		atCommand = x["write"](x)
+		if atCommand != None:
+			atCommandString = atCommandString + atCommand + ","
+
+atCommandString = atCommandString + "WR"
+
+confirm = raw_input(atCommandString + "\nOK to write? (y/n): ")
+if (confirm == "y" or confirm == "Y"):
+	if (not(enterCommandMode(ser))):
+		sys.exit(-1);
+	ser.write("ATRE\r")
+	ser.flush()
+	if(not(isOkay(ser))):
+		print("Can't reset\n");
+		sys.exit(-1);
+
+	if getReplyFor(ser, atCommandString) != "OK":
+		print("Cannot write configuration\n")
+		sys.exit(-1);
+	
+	if (not(exitCommandMode(ser))):
+		sys.exit(-1);
+	
+	print("Configuration update successful\n")
+else:
+	print("Cancelled configuration update\n")
+
