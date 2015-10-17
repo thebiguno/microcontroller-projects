2414b910fdea6e383e3a3007d5c48c7ed0578857
Warren Janssens <warren.janssens@gmail.com>
2015-10-16 21:04:30 -0600

    fixed math and other bugs

--- projects/ledcubicle/avr/Matrix.cpp
+++ projects/ledcubicle/avr/Matrix.cpp
@@ -3,7 +3,7 @@
 using namespace digitalcave;
 
 Matrix::Matrix() {
-	twi_init();
+//	twi_init();
 }
 
 Matrix::~Matrix() {
@@ -13,17 +13,15 @@ void Matrix::setColor(uint8_t gr) {
 	color = gr;
 }
 void Matrix::setColor(uint8_t red, uint8_t green) {
-	red /= 16;
-	green /= 16;
-	color = (green << 4) | red;
+	color = ((green / 16) << 4) | (red / 16);
 }
 
 void Matrix::setPixel(int16_t x, int16_t y) {
-	//changed = 1;
+	changed = 1;
 	
 	if (x >= MATRIX_WIDTH || y >= MATRIX_HEIGHT || x < 0 || y < 0) return;  //Bounds check
 
-	uint8_t index = x * MATRIX_WIDTH + y + 1;
+	uint16_t index = x * MATRIX_HEIGHT + y + 1;
 	if (overlay == DRAW_OVERLAY_REPLACE){
 		buffer[index] = color;
 	}
@@ -39,8 +37,8 @@ void Matrix::setPixel(int16_t x, int16_t y) {
 }
 
 void Matrix::flush() {
-	//if (changed) {
+	if (changed) {
 		twi_write_to(MATRIX_DRIVER_SLAVE_ADDRESS, buffer, MATRIX_BUFFER_LENGTH, TWI_BLOCK, TWI_STOP);
-		this->changed = 0;
-	//}
+		changed = 0;
+	}
 }
