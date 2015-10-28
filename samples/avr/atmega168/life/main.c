5336104c8a3f66b55c2a976c405dae6917841a92
Wyatt <wyatt@digitalcave.ca>
2015-10-28 14:18:35 -0600

    Change life sample to draw nameplate for about 10 seconds then play game of life.  Changed life to play on a wrapping plane.

--- samples/avr/atmega168/life/main.c
+++ samples/avr/atmega168/life/main.c
@@ -9,8 +9,8 @@
 #include "lib/draw/draw.h"
 #include "lib/draw/matrix/matrix.h"
 #include "lib/timer/timer.h"
-#include "lib/draw/fonts/cp_ascii_caps.h"
-#include "lib/draw/fonts/f_3x5.h"
+//#include "lib/draw/fonts/cp_ascii_caps.h"
+//#include "lib/draw/fonts/f_3x5.h"
 #include <util/delay.h>
 #include <stdlib.h>
 
@@ -21,8 +21,10 @@
 #define RED_2	0x02
 #define RED_1	0x01
 
-#define RECENT_HASH_COUNT			20
-#define RECENT_HASH_MATCH_COUNT			20
+#define RECENT_HASH_COUNT			50
+#define RECENT_HASH_MATCH_COUNT			50
+
+uint8_t nameplate[] PROGMEM = {0x88, 0x1, 0x10, 0x88, 0x1, 0x10, 0x88, 0x3, 0xb8, 0x8a, 0x99, 0x10, 0xaa, 0xa9, 0x10, 0xa9, 0xa9, 0x10, 0x50, 0x99, 0x10, 0x3, 0x0, 0x0, 0xe, 0x40, 0x0, 0x11, 0x40, 0x0, 0x11, 0x4c, 0xce, 0x11, 0x51, 0x29, 0x11, 0x59, 0x29, 0x11, 0x4d, 0x29, 0x11, 0x45, 0x29, 0xe, 0x58, 0xc9};
 
 //We write to the scratch buffer, and then flush to the matrix buffer.  We have to
 // do this so that we can check for the current neighborhood, even as we are scanning
@@ -38,16 +40,24 @@ void set_scratch(uint8_t x, uint8_t y, uint8_t value){
 	scratch_buffer[x][y] = value;
 }
 
+uint8_t get_wrapped_pixel(int8_t x, int8_t y){
+	while (x < 0) x += MATRIX_WIDTH;
+	while (x >= MATRIX_WIDTH) x -= MATRIX_WIDTH;
+	while (y < 0) y += MATRIX_HEIGHT;
+	while (y >= MATRIX_WIDTH) y -= MATRIX_HEIGHT;
+	return get_pixel(x, y);
+}
+
 uint8_t get_neighbor_count(uint8_t x, uint8_t y){
 	uint8_t count = 0;
-	if (get_pixel(x - 1, y - 1)) count++;
-	if (get_pixel(x - 1, y)) count++;
-	if (get_pixel(x - 1, y + 1)) count++;
-	if (get_pixel(x, y - 1)) count++;
-	if (get_pixel(x, y + 1)) count++;
-	if (get_pixel(x + 1, y - 1)) count++;
-	if (get_pixel(x + 1, y)) count++;
-	if (get_pixel(x + 1, y + 1)) count++;
+	if (get_wrapped_pixel(x - 1, y - 1)) count++;
+	if (get_wrapped_pixel(x - 1, y)) count++;
+	if (get_wrapped_pixel(x - 1, y + 1)) count++;
+	if (get_wrapped_pixel(x, y - 1)) count++;
+	if (get_wrapped_pixel(x, y + 1)) count++;
+	if (get_wrapped_pixel(x + 1, y - 1)) count++;
+	if (get_wrapped_pixel(x + 1, y)) count++;
+	if (get_wrapped_pixel(x + 1, y + 1)) count++;
 	return count;
 }
 
@@ -63,42 +73,47 @@ uint32_t get_board_hash(){
 }
 
 void clear_scratch(){
-    for (uint8_t x = 0; x < MATRIX_WIDTH; x++){
-	for (uint8_t y = 0; y < MATRIX_HEIGHT; y++){
-		set_scratch(x, y, 0);
-	}
-    }	
+	for (uint8_t x = 0; x < MATRIX_WIDTH; x++){
+		for (uint8_t y = 0; y < MATRIX_HEIGHT; y++){
+			set_scratch(x, y, 0);
+		}
+	}	
 }
 
 void flush(){
-    for (uint8_t x = 0; x < MATRIX_WIDTH; x++){
-	for (uint8_t y = 0; y < MATRIX_HEIGHT; y++){
-		set_pixel(x, y, scratch_buffer[x][y], OVERLAY_REPLACE);
+	for (uint8_t x = 0; x < MATRIX_WIDTH; x++){
+		for (uint8_t y = 0; y < MATRIX_HEIGHT; y++){
+			set_pixel(x, y, scratch_buffer[x][y], OVERLAY_REPLACE);
+		}
 	}
-    }
 }
 
-void flash_palette(uint8_t value){
-	draw_rectangle(0, 0, MATRIX_WIDTH - 1, MATRIX_HEIGHT - 1, DRAW_FILLED, value, OVERLAY_REPLACE);
-	draw_text(2, 5, (char*) "START", 3, 5, ORIENTATION_NORMAL, font_3x5, codepage_ascii_caps, GRN_3, OVERLAY_REPLACE);
+void show_name(uint8_t value){
+	draw_rectangle(0, 0, MATRIX_WIDTH - 1, MATRIX_HEIGHT - 1, DRAW_FILLED, 0, OVERLAY_REPLACE);
+	draw_bitmap(0, 0, 24, 16, ORIENTATION_NORMAL, nameplate, value, OVERLAY_REPLACE);
 	matrix_write_buffer();
 }
 
 void setup(){
-	srandom(analog_read_p(0) + timer_micros() + timer_millis());
-	
-	//Flash palette
-	flash_palette(GRN_1); _delay_ms(100);
-	flash_palette(GRN_2); _delay_ms(100);
-	flash_palette(GRN_3); _delay_ms(100);
-	flash_palette(RED_1 | GRN_3); _delay_ms(100);
-	flash_palette(RED_2 | GRN_3); _delay_ms(100);
-	flash_palette(RED_3 | GRN_3); _delay_ms(100);
-	flash_palette(RED_3 | GRN_2); _delay_ms(100);
-	flash_palette(RED_3 | GRN_1); _delay_ms(100);
-	flash_palette(RED_3); _delay_ms(100);
-	flash_palette(RED_2); _delay_ms(100);
-	flash_palette(RED_1); _delay_ms(100);
+	//Show name plate
+	for(uint8_t i = 0; i < 10; i++){
+		show_name(GRN_2); _delay_ms(100);
+		show_name(GRN_3); _delay_ms(100);
+		show_name(RED_1 | GRN_3); _delay_ms(100);
+		show_name(RED_2 | GRN_3); _delay_ms(100);
+		show_name(RED_3 | GRN_3); _delay_ms(100);
+		show_name(RED_3 | GRN_2); _delay_ms(100);
+		show_name(RED_3 | GRN_1); _delay_ms(100);
+		show_name(RED_3); _delay_ms(100);
+		show_name(RED_2); _delay_ms(100);
+		show_name(RED_3); _delay_ms(100);
+		show_name(RED_3 | GRN_1); _delay_ms(100);
+		show_name(RED_3 | GRN_2); _delay_ms(100);
+		show_name(RED_3 | GRN_3); _delay_ms(100);
+		show_name(RED_2 | GRN_3); _delay_ms(100);
+		show_name(RED_1 | GRN_3); _delay_ms(100);
+		show_name(GRN_3); _delay_ms(100);
+	}
 	
 	//Clear board
 	clear_scratch();
@@ -129,6 +144,8 @@ int main (void){
 	analog_init(analog_pins, 1, ANALOG_INTERNAL);
 	matrix_init();
 	matrix_set_mode(MATRIX_MODE_2BIT);
+	
+	srandom(analog_read_p(0));
 	
 	setup();
 		
