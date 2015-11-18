404476cbe9be1732dbaff4bd040b53eaf47a6a5a
Wyatt <wyatt@digitalcave.ca>
2015-11-17 21:31:19 -0700

    Skeleton hardware test program for Chiindii.  Turns on LEDs and spins up motor1 once per second.

--- projects/chiindii/src/Chiindii.cpp
+++ projects/chiindii/src/Chiindii.cpp
@@ -1,9 +1,31 @@
+#include <avr/io.h>
+#include <avr/power.h>
+#include <util/delay.h>
+
 #include "Chiindii.h"
 
 int main(){
-
+	//Set clock to run at full speed
+	clock_prescale_set(clock_div_1);
+	// DISABLE JTAG - take control of F port
+	MCUCR = _BV(JTD);
+	MCUCR = _BV(JTD);
+	
+	DDRF |= _BV(5) | _BV(6) | _BV(7);
+	DDRB |= _BV(0) | _BV(1);
+	
+	//Set motor pins as output
+	*(&MOTOR1_PORT - 0x01) |= _BV(MOTOR1_PIN);
+	*(&MOTOR2_PORT - 0x01) |= _BV(MOTOR2_PIN);
+	*(&MOTOR3_PORT - 0x01) |= _BV(MOTOR3_PIN);
+	*(&MOTOR4_PORT - 0x01) |= _BV(MOTOR4_PIN);
+	
 	//Main program loop
 	while (1) {
-
+		MOTOR1_PORT ^= _BV(MOTOR1_PIN);
+		
+		PORTB ^= _BV(0) | _BV(1);
+		PORTF ^= _BV(5) | _BV(6) | _BV(7);
+		_delay_ms(1000);
 	}
 }
--- projects/chiindii/src/Chiindii.h
+++ projects/chiindii/src/Chiindii.h
@@ -4,4 +4,14 @@
 #include <util/delay.h>
 #include <avr/io.h>
 
+#define MOTOR1_PORT		PORTE
+#define MOTOR1_PIN		PINE6
+#define MOTOR2_PORT		PORTD
+#define MOTOR2_PIN		PIND5
+#define MOTOR3_PORT		PORTC
+#define MOTOR3_PIN		PINC7
+#define MOTOR4_PORT		PORTB
+#define MOTOR4_PIN		PINB7
+
+
 #endif
