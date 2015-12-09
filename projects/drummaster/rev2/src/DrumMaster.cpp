582c5d378db1881e5f3bc570db571d8741687049
Wyatt <wyatt@digitalcave.ca>
2015-12-09 00:52:16 -0700

    Oops, forgot a debugging statement last time...

--- projects/drummaster/rev2/src/DrumMaster.cpp
+++ projects/drummaster/rev2/src/DrumMaster.cpp
@@ -21,7 +21,7 @@ int main(){
 	Menu::display->write_text(0, 0, "Loading...          ", 20);
 	Menu::display->refresh();
 	
-	while(!Serial);		//Wait for a serial console before continuing.  Only needed for debugging at startup.
+	//while(!Serial);		//Wait for a serial console before continuing.  Only needed for debugging at startup.
 	Serial.begin(9600);
 	Wire.begin();
 	
