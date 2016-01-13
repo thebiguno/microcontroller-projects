1ac3f5d5dfbe7c233db645cc83ed6f2697d560c8
Warren Janssens <janssensw@richer.ca>
2016-01-13 12:53:34 -0700

    more WIP on calibration program

--- projects/chiindii/python/calibration.py
+++ projects/chiindii/python/calibration.py
@@ -29,20 +29,47 @@ MODE_CALIBRATION_ANGLE_PID = 0x03
 ########## Main program here ##########
 
 def main(ser):
+	axis = 0
+	
 	while True:
 		writeMessage(ser, 0x00, [0x43])
 		print(
 """
 Chiindii Calibration: Please selection an option below:
-	R) Rate PID Calibration
-	C) Complementary Filter Calibration
-	A) Attitude PID Calibration
+	X) Choose which axis to calibrate
+	R) Change Rate PID calibration
+	C) Change Complementary filter calibration
+	A) Change Angle PID calibration
 	L) Load all values from EEPROM (revert changes for this session)
 	S) Save all values to EEPROM
 	Q) Quit (Any unsaved changes will be lost)
 """)
 		choice = raw_input("Selected Option: ")
-	
+		
+		if (choice == "X" or choice == "x"):
+			print("""
+Each axis must be tuned independently since Chiindii must be placed into the jig
+in the proper orientation for that axis.
+For Pitch and Roll, attach the arms to the jig such that the craft can freely pitch or roll.
+For Yaw, suspend the craft from the top so that it can freely yaw.
+""")
+			print("\nPlease select a axis to calibrate")
+			print("	P) Pitch")
+			print("	R) Roll")
+			print("	Y) Yaw")
+			print("	Q) Return to the main menu")
+
+			axis = raw_input("Select axis: ")
+		
+			if (axis == "Q" or axis == "q"):
+				break
+			elif (axis == "P" or axis == "p" or axis == "R" or axis == "r" or axis == "Y" or axis == "y"):
+				## translate axis into number for indexing into tuning array
+				if (axis == "P" or axis == "p"): axis = 0
+				elif (axis == "R" or axis == "r"): axis = 1
+				else: axis = 2
+			else:
+				print("Invalid parameter, please try again\n")
 		if (choice == "R" or choice == "r"):
 			doRatePidCalibration(ser)
 		elif (choice == "C" or choice == "c"):
@@ -89,159 +116,179 @@ Place Chiindii into the jig, and press enter.
 		return
 	d = response["data"]
 	while True:
-		print("\nPlease select a axis to modify")
-		print("	P) Pitch")
-		print("	R) Roll")
-		print("	Y) Yaw")
-		print("	Q) Return to the main menu")
+		print("\nPlease select a parameter to modify")
+		print("	S) Rate Set Point (deg/sec)")
+		print("	P) Proportional")
+		print("	I) Integral")
+		print("	D) Derivitive")
+		print("	Q) Return to axis selection")
 		
-		axis = raw_input("Select axis: ")
+		param = raw_input("Select parameter: ")
 		
-		if (axis == "Q" or axis == "q"):
+		if (param == "Q" or param == "q"):
 			break
-		elif (axis == "P" or axis == "p" or axis == "R" or axis == "r" or axis == "Y" or axis == "y"):
-			## translate axis into number for indexing into tuning array
-			if (axis == "P" or axis == "p"): axis = 0
-			elif (axis == "R" or axis == "r"): axis = 1
-			else: axis = 2
+		elif (param == "R" or param == "r"):
+			print("\nEnter a valid number, or 'Q' to return to parameter selection.")
+			while True:
+				value = raw_input("Rate: ")
+				if (float.match(value)):
+					rate_sp = [0,0,0,0]
+					struct.pack_info("!f", rate_sp, 0, radians(float(value)))
+					writeMessage(ser, MESSAGE_SEND_ANGLE_SP, rate_sp)
+				elif (value == "Q" or value == "q"):
+					break;
+				else:
+					print("Invalid value, please try again\n")
+		elif (param == "P" or param == "p" or param == "I" or param == "i" or param == "D" or param == "d"):
+			## translate param into number for indexing into tuning array
+			if (param == "P" or param == "p"): 
+				param = 0
+			elif (param == "I" or param == "i"): 
+				param = 1
+			else: 
+				param = 2
 			
-			print("\nPlease select a parameter to modify")
-			print("	S) Rate Set Point (deg/sec)")
-			print("	P) Proportional")
-			print("	I) Integral")
-			print("	D) Derivitive")
-			print("	Q) Return to axis selection")
-			
-			param = raw_input("Select parameter: ")
-			
-			if (param == "Q" or param == "q"):
-				break
-			elif (param == "R" or param == "r"):
-				print("\nEnter a valid number, or 'Q' to return to parameter selection.")
-				while True:
-					value = raw_input("value (" + str(to_float_t(rate_sp) + "): ")
-					if (float.match(value)):
-						rate_sp = to_float_t(float(value))
-						writeMessage(ser, MESSAGE_SEND_RATE_SP, rate_sp)
-					elif (value == "Q" or value == "q"):
-						break;
-					else:
-						print("Invalid value, please try again\n")
-			elif (param == "P" or param == "p" or param == "I" or param == "i" or param == "D" or param == "d"):
-				## translate param into number for indexing into tuning array
-				if (param == "P" or param == "p"): param = 0
-				elif (param == "I" or param == "i"): param = 1
-				else: param = 2
-				
-				print("\nEnter a valid number, or 'Q' to return to parameter selection.")
-				while True:
-					value = raw_input("value (" + str(to_float_t(d[axis * 3 + param])) + "): ")
-					if (float.match(value)):
-						d[axis * 3 + param] = to_float_t(float(value))
-						writeMessage(ser, MESSAGE_SEND_CALIBRATION_RATE_PID, d)
-					elif (value == "Q" or value == "q"):
-						break;
-					else:
-						print("Invalid value, please try again\n")
-			else
-				print("Invalid parameter, please try again\n")
+			print("\nEnter a valid number, or 'Q' to return to parameter selection.")
+			while True:
+				value = raw_input("Value (" + str(to_float_t(d[axis * 3 + param])) + "): ")
+				if (float.match(value)):
+					d[axis * 3 + param] = to_float_t(float(value))
+					writeMessage(ser, MESSAGE_SEND_CALIBRATION_RATE_PID, d)
+				elif (value == "Q" or value == "q"):
+					break;
+				else:
+					print("Invalid value, please try again\n")
 		else:
 			print("Invalid axis, please try again\n")
 
-def doMagnetometerCalibration(ser):
-	raw_input("""
-Magnetometer calibration determines what offset values are required for the magnetometer
-to be properly centered.  Each magnetometer will be different, and depending on the 
-magnetic fields surrounding the servos, each frame / servo combination will likely
-be different as well.
-The best way to calibrate the magnetometer is to use the automatic routine.  Place
-Stubby on the floor or a table, far from the edge.  When calibration starts, Stubby
-will slowly turn in place for a minute or two, completing about 3 - 5 full rotations.
-While rotating, Stubby repeatedly reads the magnetometer, and sends the data to the 
-calibration program.  When completed, the calibration program analyzes the data, and
-computes the required calibration offsets.
-The calibration units for this mode are offsets on the raw magnetometer readings.
-See http://stubby.digitalcave.ca/stubby/calibration.jsp for additional instructions on 
-calibration, including pictures.
-Please place Stubby on the floor, and press enter.
+def doRatePidCalibration(ser, mode):
+	requestMessage = 0x00
+	sendMessage = 0x00
+		raw_input("""
+Angle PID calibration allows Chiindii to quickly an accurately achieve the requested absolute
+angle.  Each axis (pitch, roll, yaw) has three parameters (proportional, integral, 
+derivitive) for a total of nine parameters.
+Tune each axis in turn by requesting a fixed angle (for example 30 degree)
+and increasing the proportional parameter until the the observed angle starts to
+occilate around the angle.  At that point begin to increase the integral parameter
+until the observed angle stops occilating.  Continue tuning until the observed angle
+is stable and matches the requested angle for a variety of requested angles.
+Place Chiindii into the jig, and press enter.
 """)
-
-	while True:
-		writeMessage(ser, 0x00, [0x43])
-		writeMessage(ser, MESSAGE_REQUEST_MAGNETOMETER_CALIBRATION, [])
-		response = readMessage(ser)
-		if (response == False):
-			print("Communication failure")
-			return
-		elif (response["command"] != MESSAGE_SEND_MAGNETOMETER_CALIBRATION):
-			print("Invalid response detected")
-			return
-		
-		d = response["data"]
 	
-		print("\nPlease select an option")
-		print("	C) Show current magnetometer offsets")
-		print("	S) Start auto magnetometer offset calibration")
-		print("	M) Set manual magnetometer offsets")
-		print("	Q) Return to main menu")
+	writeMessage(ser, 0x00, [0x43])
+	writeMessage(ser, MESSAGE_REQUEST_CALIBRATION_RATE_PID, [])
+	response = readMessage(ser)
+	if (response == False):
+		print("Communication failure")
+		return
+	elif (response["command"] != MESSAGE_REQUEST_CALIBRATION_RATE_PID):
+		print("Invalid response detected")
+		return
+	d = response["data"]
+	while True:
+		print("\nPlease select a parameter to modify")
+		print("	S) Angle Set Point (deg)")
+		print("	P) Proportional")
+		print("	I) Integral")
+		print("	D) Derivitive")
+		print("	Q) Return to axis selection")
 		
-		option = raw_input("Selected Option: ")
+		param = raw_input("Select parameter: ")
+		
+		if (param == "Q" or param == "q"):
+			break
+		elif (param == "R" or param == "r"):
+			print("\nEnter a valid number, or 'Q' to return to parameter selection.")
+			while True:
+				value = raw_input("Angle: ")
+				if (float.match(value)):
+					angle_sp = [0,0,0,0]
+					struct.pack_info("!f", angle_sp, 0, radians(float(value)))
+					writeMessage(ser, MESSAGE_SEND_ANGLE_SP, angle_sp)
+				elif (value == "Q" or value == "q"):
+					break;
+				else:
+					print("Invalid value, please try again\n")
+		elif (param == "P" or param == "p" or param == "I" or param == "i" or param == "D" or param == "d"):
+			## translate param into number for indexing into tuning array
+			if (param == "P" or param == "p"): 
+				param = 0
+			elif (param == "I" or param == "i"): 
+				param = 1
+			else: 
+				param = 2
+			
+			print("\nEnter a valid number, or 'Q' to return to parameter selection.")
+			while True:
+				value = raw_input("Value (" + str(struct.unpack_from("!f", d, axis * 12 + param * 4)) + "): ")
+				if (float.match(value)):
+					struct.pack_into("!f", d, axis * 12 + param * 4, value)
+					writeMessage(ser, MESSAGE_SEND_CALIBRATION_ANGLE_PID, d)
+				elif (value == "Q" or value == "q"):
+					break;
+				else:
+					print("Invalid value, please try again\n")
+		else:
+			print("Invalid axis, please try again\n")
 
-		if (option == "C" or option == "c"):
-			x = to_int16_t((d[0] << 8) + d[1])
-			y = to_int16_t((d[2] << 8) + d[3])
-			print("Current offsets: x=" + str(x) + ", y=" + str(y))
-		elif (option == "S" or option == "s"):
-			xOffset = to_int16_t((d[0] << 8) + d[1])
-			yOffset = to_int16_t((d[2] << 8) + d[3])
 
-			print("Stubby will now slowly turn in place, for about 3 - 5 rotations.  Please stand by...")
-			writeMessage(ser, MESSAGE_START_MAGNETOMETER_CALIBRATION, [])
-			xValues = []
-			yValues = []
-			print("Reading raw values and showing adjusted values with current offsets (" + str(xOffset) + ", " + str(yOffset) + "):\nX\tY\tadj. X\tadj. Y")
+def doComplementaryFilterCalibration(ser, mode):
+	requestMessage = 0x00
+	sendMessage = 0x00
+		raw_input("""
+Complemenentary calibration allows Chiindii to integrate Gyro and Accelerometer
+data to reduce Gyro drift.  There is one paramater that called Tau which is the response
+time of integration.  This allows a trade off between drift elimination and responsiveness.
+The tuning allows you to collect raw and integrated data which can be graphed.
+""")
+	
+	writeMessage(ser, 0x00, [0x43])
+	writeMessage(ser, MESSAGE_REQUEST_CALIBRATION_RATE_PID, [])
+	response = readMessage(ser)
+	if (response == False):
+		print("Communication failure")
+		return
+	elif (response["command"] != MESSAGE_REQUEST_CALIBRATION_COMPLEMENTARY):
+		print("Invalid response detected")
+		return
+	d = response["data"]
+	while True:
+		print("\nPlease select a parameter to modify")
+		print("	T) Tau")
+		print("	O) Output raw data")
+		print("	Q) Return to axis selection")
+		
+		param = raw_input("Select parameter: ")
+		
+		if (param == "Q" or param == "q"):
+			break
+		elif (param == "T" or param == "t"):
+			print("\nEnter a valid number, or 'Q' to return to parameter selection.")
+			while True:
+				value = raw_input("Tau (" + struct.unpack_from("!f", d, axis * 4) + "): ")
+				if (float.match(value)):
+					struct.pack_into("!f", d, axis * 4, float(value))
+					writeMessage(ser, MESSAGE_SEND_CALIBRATION_COMPLEMENTARY, d)
+				elif (value == "Q" or value == "q"):
+					break;
+				else:
+					print("Invalid value, please try again\n")
+		else if (param == "O" or param == "o"):
+			writeMessage(ser, MESSAGE_SEND_COMPLEMENTARY_OUTPUT, [axis])
 			while True:
 				rawValue = readMessage(ser)
 				if (rawValue == False):
 					break
 				rawData = rawValue["data"]
-				xValue = to_int16_t((rawData[0] << 8) + rawData[1]);
-				yValue = to_int16_t((rawData[2] << 8) + rawData[3]);
-				xValues.append(xValue)
-				yValues.append(yValue)
-				print(str(xValue) + "\t" + str(yValue) + "\t" + str(xValue + xOffset) + "\t" + str(yValue + yOffset))
-			xAverage = -1 * sum(xValues) / len(xValues)
-			yAverage = -1 * sum(yValues) / len(yValues)
-			print("Current offsets: " + str(xOffset) + ", " + str(yOffset))
-			print("New (auto calculated) offsets: " + str(xAverage) + ", " + str(yAverage))
-			useNewOffsets = raw_input("Do you want to use these new offsets? (Y/N) ")
-			if (useNewOffsets == "Y" or useNewOffsets == "y"):
-				#Send new calibration values
-				data = []
-				data.append((to_uint16_t(xAverage) >> 8) & 0xFF)
-				data.append((to_uint16_t(xAverage)) & 0xFF)
-				data.append((to_uint16_t(yAverage) >> 8) & 0xFF)
-				data.append((to_uint16_t(yAverage)) & 0xFF)
-				writeMessage(ser, MESSAGE_SEND_MAGNETOMETER_CALIBRATION, data)
-			
-		elif (option == "M" or option == "m"):
-			xOffset = raw_input("X Offset: ")
-			yOffset = raw_input("Y Offset: ")
-			if (not(integer.match(xOffset)) or not(integer.match(yOffset))):
-				print("Invalid offset specified.  Please enter a valid integer.")
-			else:
-				data = []
-				data.append((to_uint16_t(int(xOffset)) >> 8) & 0xFF)
-				data.append((to_uint16_t(int(xOffset))) & 0xFF)
-				data.append((to_uint16_t(int(yOffset)) >> 8) & 0xFF)
-				data.append((to_uint16_t(int(yOffset))) & 0xFF)
-				writeMessage(ser, MESSAGE_SEND_MAGNETOMETER_CALIBRATION, data)
 				
-		elif (option == "Q" or option == "q"):
-			print("Exiting to main menu")
-			return
+				accel = struct.unpack_from("!f", rawData, 0);
+				gyro = struct.unpack_from("!f", rawData, 4);
+				comp = struct.unpack_from("!f", rawData, 8);
+				print(str(accel) + "\t" + str(gyro) + "\t" + str(comp))
 		else:
-			print("Invalid option selected.")
+			print("Invalid axis, please try again\n")
+
 
 
 ########## Helper functions here ##########
@@ -334,41 +381,6 @@ def readMessage(ser):
 				else:
 					buf[pos - 3] = b
 					pos = pos + 1
-
-#These next four functions are the equivalent of casting to the specified c types.
-def to_int8_t(b):
-	if (b > 0xFF):
-		b = 0xFF
-	elif (b < 0x00):
-		b = 0x00
-		
-	if (b > 0x7F):
-		return (b - 0xFF)
-	else:
-		return b
-
-def to_uint8_t(b):
-	if (b < 0x00):
-		return (0xFF + b) & 0xFF
-	else:
-		return b & 0xFF
-	
-def to_int16_t(i):
-	if (i > 0xFFFF):
-		i = 0xFFFF
-	elif (i < 0x0000):
-		i = 0x0000
-		
-	if (i > 0x7FFF):
-		return (i - 0xFFFF)
-	else:
-		return i
-
-def to_uint16_t(i):
-	if (i < 0x0000):
-		return (0xFFFF + i) & 0xFFFF
-	else:
-		return i & 0xFFFF
 
 
 ########## Main startup hooks here ##########
