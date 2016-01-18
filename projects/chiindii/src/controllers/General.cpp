37cd0300937d85f7a76f7f777a5966593def5690
Warren Janssens <janssensw@richer.ca>
2016-01-18 10:52:09 -0700

    adding general controller to dispatch

--- projects/chiindii/src/controllers/General.cpp
+++ projects/chiindii/src/controllers/General.cpp
@@ -10,7 +10,17 @@ General::General(Chiindii *chiindii) {
 
 void General::dispatch(FramedSerialMessage* request) {
 	uint8_t cmd = request->getCommand();
-	if (cmd == MESSAGE_REQUEST_BATTERY){
+	if (cmd == MESSAGE_REQUEST_ENABLE_DEBUG) {
+		chiindii->setDebug(0x01);
+		FramedSerialMessage response(MESSAGE_REQUEST_ENABLE_DEBUG, 0);
+		chiindii->sendMessage(&response);
+	}
+	else if (cmd == MESSAGE_REQUEST_DISABLE_DEBUG) {
+		chiindii->setDebug(0x00);
+		FramedSerialMessage response(MESSAGE_REQUEST_DISABLE_DEBUG, 0);
+		chiindii->sendMessage(&response);
+	}
+	else if (cmd == MESSAGE_REQUEST_BATTERY){
 		uint8_t data[] = { chiindii->getBatteryLevel() };
 		FramedSerialMessage response(MESSAGE_SEND_BATTERY, data, 1);
 		chiindii->sendMessage(&response);
