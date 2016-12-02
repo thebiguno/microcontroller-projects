b0005d0d2e780527c746ce512f91a473d389511f
Wyatt <wyatt@digitalcave.ca>
2016-12-02 09:55:02 -0700

    Serial read works if you don't use interrupt mode... so the hardware is good.

--- inc/stm32f4/Serial/SerialHAL.cpp
+++ inc/stm32f4/Serial/SerialHAL.cpp
@@ -7,18 +7,16 @@ SerialHAL::SerialHAL(UART_HandleTypeDef* huart, uint8_t bufferSize):
 	rxBuffer(bufferSize),
 	huart(huart)
 {
-	HAL_StatusTypeDef ret = HAL_UART_Receive_IT(huart, &incomingByte, 1);	//Start listening
-	while (ret != HAL_OK) {
-		delay_ms(100);
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_2);
-	}
+	// HAL_StatusTypeDef ret = HAL_UART_Receive_IT(huart, &incomingByte, 1);	//Start listening
+	// while (ret != HAL_OK) {
+	// 	delay_ms(100);
+	// 	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_2);
+	// }
 }
 
 uint8_t SerialHAL::read(uint8_t *c){
-	if (!rxBuffer.isEmpty()){
-		if (rxBuffer.read(c)){
-			return 1;
-		}
+	if (HAL_OK == HAL_UART_Receive(huart, c, 1, 1)){
+		return 1;
 	}
 	return 0;
 }
