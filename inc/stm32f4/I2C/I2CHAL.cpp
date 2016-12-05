3426163eeaeef5b8143034a95dda942124c83552
Wyatt <wyatt@digitalcave.ca>
2016-12-04 19:44:51 -0700

    Change to I2CHAL to use 7 bit addresses as documented.
    HAL requires 8 bit addresses, so this translates.

--- inc/stm32f4/I2C/I2CHAL.cpp
+++ inc/stm32f4/I2C/I2CHAL.cpp
@@ -11,9 +11,9 @@ I2CHAL::I2CHAL(I2C_HandleTypeDef* hi2c) :
 }
 
 void I2CHAL::read(uint8_t address, I2CMessage* m, uint8_t length, uint8_t i2cStop){
-	HAL_I2C_Master_Receive(hi2c, address, m->getData(), m->getLength(), 100);
+	HAL_I2C_Master_Receive(hi2c, address << 1, m->getData(), m->getLength(), 100);
 }
 
 void I2CHAL::write(uint8_t address, I2CMessage* m, uint8_t i2cBlock, uint8_t i2cStop){
-	HAL_I2C_Master_Transmit(hi2c, address, m->getData(), m->getLength(), 100);
+	HAL_I2C_Master_Transmit(hi2c, address << 1, m->getData(), m->getLength(), 100);
 }
