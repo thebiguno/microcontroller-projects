5afdefa77d620eda843f5ca2cf80c33a7789b5fd
Wyatt <wyatt@digitalcave.ca>
2016-12-12 22:32:04 -0700

    Flash persistence is working, albeit not in the best way possible.  Need to implement wear leveling, and figure out how to erase a small section without needing to erase the entire sector (which may not be possible, given that a section is huge and may contain code)

--- inc/stm32f4/dcutil/persist.c
+++ inc/stm32f4/dcutil/persist.c
@@ -1,19 +1,53 @@
-#include <dcutil/persist.h>
+/*
+ * WARNING:
+ * This persistence layer implementation just writes bytes at the end of the flash section
+ * for the currently selected chip.  This means that if your program takes up the
+ * full space, PARTS OF IT WILL BE OVERWRITTEN WITH RANDOM DATA.  THIS IS A BAD THING!
+ * You MUST BE SURE that you are safe to write to this area before writing.
+ *
+ * TODO: Wear leveling, cross chip capabilities, etc.
+ */
 
+#include <dcutil/persist.h>
+#include "stm32f4xx_hal.h"
+
+#define FLASH_SIZE (1024)
+#define FLASH_BASE_ADDRESS (0x8010000)
 
 void persist_write(uint32_t address, uint8_t* data, uint16_t length){
-	uint8_t flash_status = FLASH_COMPLETE;
+	//Bounds checking
+	if ((address + length) > FLASH_SIZE) return;
+
+	//Verify that data has actually changed, and update persistance array if it has
+	uint8_t changed = 0;
+	uint8_t updatedData[FLASH_SIZE];
+	for (uint16_t i = 0; i < FLASH_SIZE; i++){
+		updatedData[i] = *(__IO uint8_t*) (FLASH_BASE_ADDRESS + i);
+	}
+	for (uint16_t i = address; i < length; i++){
+		if (updatedData[i] != data[i]){
+			updatedData[i] = data[i];
+			changed = 1;
+		}
+	}
+	if (!changed) return;
 
 	HAL_FLASH_Unlock();
-	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR
-		| FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERRFLASH_FLAG_EOP
-		| FLASH_FLAG_OPERR |FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR
-	 	| FLASH_FLAG_PGSERR);
+	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR);
+	FLASH_Erase_Sector(FLASH_SECTOR_4, VOLTAGE_RANGE_3);
+	for (uint16_t i = 0; i < FLASH_SIZE; i++){
+		HAL_FLASH_Program(
+				TYPEPROGRAM_BYTE,
+				(uint32_t) FLASH_BASE_ADDRESS + i,
+				updatedData[i]
+		);
+	}
 
-		FLASH_SECTOR_0
-	//eeprom_update_block(data, (void*) (uint16_t) address, length);
+	HAL_FLASH_Lock();
 }
 
 void persist_read(uint32_t address, uint8_t* data, uint16_t length){
-	//eeprom_read_block(data, (void*) (uint16_t) address, length);
+	for (uint16_t i = 0; i < length; i++){
+		data[i] = *(__IO uint8_t*) (FLASH_BASE_ADDRESS + address + i);
+	}
 }
---
+++ samples/arm/stm32f410/flash_persist/main.cpp
@@ -0,0 +1,65 @@
+#include "stm32f4xx_hal.h"
+
+#include <persist.h>
+#include <dctypes.h>
+#include <dcutil/delay.h>
+#include <SerialHAL.h>
+
+using namespace digitalcave;
+
+extern UART_HandleTypeDef huart6;
+
+extern "C" {
+	void dc_main();
+}
+
+
+void dc_main(){
+	//We flash the green light briefly.
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);
+	for (uint8_t i = 0; i < 10; i++){
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
+		delay_ms(200);
+	}
+
+	SerialHAL serial(&huart6, 64);
+
+	//We check whether the persisted flash is valid
+	uint8_t data[8];
+	persist_read(0, data, 8);
+
+	char buffer[32];
+	snprintf(buffer, sizeof(buffer), "%02x %02x %02x %02x %02x %02x %02x %02x \n", data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
+	serial.write(buffer);
+
+	if (data[0] == 0x43 && data[1] == 0x55){
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+
+		serial.write("Flash verified\n");
+	}
+	else {
+		//If not, we show a red light and write it now
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET);
+		data[0] = 0x43;
+		data[1] = 0x55;
+		data[2] = 0x01;
+		data[3] = 0x02;
+		data[4] = 0x03;
+		data[5] = 0x04;
+		data[6] = 0x05;
+		data[7] = 0x06;
+		persist_write(0, data, 8);
+		serial.write("Flash updated\n");
+
+		persist_read(0, data, 8);
+
+		char buffer[32];
+		snprintf(buffer, sizeof(buffer), "%02x %02x %02x %02x %02x %02x %02x %02x \n", data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
+		serial.write(buffer);
+	}
+
+	while (1){
+	}
+}
