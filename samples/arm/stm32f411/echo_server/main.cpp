b0d62de55f1836c6f64b172aaa1dd7c69331708d
Warren <warren.janssens@gmail.com>
2017-10-18 10:47:30 -0600

    starting work on a test sample project for esp8266

---
+++ samples/arm/stm32f411/echo_server/main.cpp
@@ -0,0 +1,40 @@
+#include "stm32f4xx_hal.h"
+
+#include <SerialHAL.h>
+#include <ESP8266.h>
+#include <TimerHAL.h>
+
+using namespace digitalcave;
+
+extern UART_HandleTypeDef huart1;
+
+extern "C" {
+	void dc_main();
+}
+
+SerialHAL* serial;
+
+void dc_main(){
+	SerialHAL s(&huart1, 64);
+	serial = &s;
+
+	ESP8266 wifi(serial);
+	wifi.join("sloth","password");
+	wifi.start_server(1234);
+
+	while (1) {
+		uint8_t b = 0;
+		ESP8266Socket* socket = wifi.accept();
+		if (socket != NULL && !socket->is_closed()) {
+			socket->read(&b);
+			socket->write(b);
+		}
+	}
+
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+	if (serial){
+		serial->isr();
+	}
+}
