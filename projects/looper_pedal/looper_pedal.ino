79ed32a8a86dea2a45de2299ca8055a7c7f5927b
Wyatt <olsonw@richer.ca>
2021-07-28 14:02:06 -0600

    sharing

--- projects/looper_pedal/looper_pedal.ino
+++ projects/looper_pedal/looper_pedal.ino
@@ -29,9 +29,6 @@ AudioControlSGTL5000     control;        //xy=344,466
 // GUItool: end automatically generated code
 
 
-
-
-
 //Looper state
 Encoder beatsPerMinuteKnob(34, 33);
 const uint8_t timeSignatureButtonPin = 35;
@@ -39,13 +36,20 @@ Bounce timeSignatureButton = Bounce(timeSignatureButtonPin, 5); //5ms debounce t
 
 const uint8_t looperButtonPin = 41;
 Bounce looperButton = Bounce(looperButtonPin, 5); //5ms debounce time
+
 enum record_state {
-  play,       //Playing back the buffer repeatedly
+  idle,       //Not recording
   record,     //Actively recording
   start_record,       //Ready to start recording as soon as the next metronome click happens.  Only applicable when timeSignatureCount != 0.
   stop_record         //Ready to stop recording as soon as the next metronome click happens.  Only applicable when timeSignatureCount != 0.
 };
-record_state state = play;
+record_state recordState = idle;
+
+enum play_state {
+  play,     //Actively playing
+  start_play    //Ready to start playback as soon as the next metronome click happens.  Only applicable when timeSignatureCount != 0.
+};
+play_state playState = play;
 
 //Recording / playback buffers
 extern "C" uint8_t external_psram_size;
@@ -143,17 +147,23 @@ void playMetronome(){
   if (timeSignatureCount > 0){  //Set time signature to 0 to disable metronome
     uint16_t beatPeriod = (1 / (beatsPerMinute / 60.0) * 1000);
     if (timeSinceLastBeat >= beatPeriod){
-      if (state == stop_record){
+      if (recordState == stop_record){
         stopRecord();
       }
-      else if (state == start_record){
+      else if (recordState == start_record){
         startRecord();
       }
+
+      if (playState == start_play){
+        playState = play;
+        Serial.println("Starting playback");
+      }
+      
       timeSinceLastBeat -= beatPeriod;
       metronome.frequency(beatCounter == 0 ? 400 : 200);
       metronome.noteOn();
       beatCounter = (beatCounter + 1) % timeSignatureCount;
-      if (state == record){
+      if (recordState == record){
         Serial.print("Recording ");
         Serial.print(recordBuffer.index);
         Serial.println(" bytes");
@@ -165,17 +175,17 @@ void playMetronome(){
 void adjustRecordingState(){
   if (looperButton.update()){
     if (looperButton.fallingEdge()){
-      if (state == record){
+      if (recordState == record){
         if (timeSignatureCount){
-          state = stop_record;  //Queue the recording to stop at the next metronome click;
+          recordState = stop_record;  //Queue the recording to stop at the next metronome click;
         }
         else {
           stopRecord();
         }
       }
-      else if (state == play){
+      else if (recordState == idle){
         if (timeSignatureCount){
-          state = start_record;   //Queue the recording to start at the next metronome click;
+          recordState = start_record;   //Queue the recording to start at the next metronome click;
         }
         else {
           startRecord();
@@ -189,44 +199,46 @@ void startRecord(){
   Serial.println("Recording");
   queueRecord.begin();
   recordBuffer.index = 0;
-  state = record;
+  recordState = record;
 }
 
 void doRecord(){
-  doRecord(0);  //Not in cleanup mode.
-}
-void doRecord(uint8_t cleanup){
-  while(state == record && queueRecord.available() && recordBuffer.index < bufferSize){
+  while((recordState == record || recordState == stop_record) && queueRecord.available() && recordBuffer.index < bufferSize){
     memcpy(recordBuffer.buffer + recordBuffer.index, queueRecord.readBuffer(), 256);
     recordBuffer.index += 256;
     queueRecord.freeBuffer();
   }
 
-  if (!cleanup && recordBuffer.index >= bufferSize){
+  if (recordBuffer.index >= bufferSize){
     stopRecord();
   }
 }
 
 void stopRecord(){
   queueRecord.end();
-  state = record;
-  doRecord(1);   //Finish getting whatever samples are left over
-  state = play;
+  recordState = idle;
+  playState = start_play;
   playBuffer.length = max(playBuffer.length, recordBuffer.index);
+  uint32_t start = millis();
   memcpy(playBuffer.buffer, recordBuffer.buffer, recordBuffer.index);
-  Serial.print("Finished recording; Playing back ");
+  uint32_t end = millis();
+  Serial.print("Finished recording; ");
+  Serial.print(end - start);
+  Serial.print("ms for buffer copy.  Playing back ");
   Serial.print(playBuffer.length);
   Serial.println(" bytes");
 }
 
 void doPlayback(){
-  for (uint8_t i = 0; i < 8 && playBuffer.length > 0 && queuePlay.available(); i++){
-    memcpy(queuePlay.getBuffer(), playBuffer.buffer + playBuffer.index, 256);
-    queuePlay.playBuffer();
-    playBuffer.index += 256;
-    if (playBuffer.index >= playBuffer.length){
-      playBuffer.index = 0;
-      Serial.println("Playing back buffer");
+  if (playState == play || timeSignatureCount == 0){
+    for (uint8_t i = 0; i < 8 && playBuffer.length > 0 && queuePlay.available(); i++){
+      memcpy(queuePlay.getBuffer(), playBuffer.buffer + playBuffer.index, 256);
+      queuePlay.playBuffer();
+      playBuffer.index += 256;
+      if (playBuffer.index >= playBuffer.length){
+        playBuffer.index = 0;
+        playState = start_play;
+      }
     }
   }
 }
